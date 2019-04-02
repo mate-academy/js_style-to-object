33f488e7d2efeeb85d780ee46f3a062fbbe7422d
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-02 16:31:47 +0300

    added tests and task description

---
+++ src/stylesString.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const stylesString = `
+  background-color: #fff;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear: both;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height: 40px;
+  outline: 0;
+  padding-left: 18px;
+  padding-right: 30px;
+  position: relative;
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  white-space: nowrap;
+  width: auto;
+`;
+
+module.exports = stylesString;

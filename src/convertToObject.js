ad24c53e2912fca1e5c14a3bc363b7435e601ac0
Alexandr <alexandrmail32@gmail.com>
2019-04-09 21:09:18 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let resultObj = {};
+  let startArr = sourceString.split(';');
+  let arr = startArr.map((item) => {
+    item = item.trim();
+    return item;
+  });
+
+  arr.pop();
+
+  arr.forEach((item) => {
+    let smallArr = item.split(': ');
+    resultObj[`${smallArr[0]}`] = smallArr[1];
+  });
+  return resultObj;
 }
 
 module.exports = convertToObject;

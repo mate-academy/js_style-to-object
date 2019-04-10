d4ca5b9b0170fdf488ab09a802b8d4eb67cf44ea
Olegus <gulldan1996@gmail.com>
2019-04-10 10:00:10 +0300

    change style coding

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,12 @@
  */
 
 function convertToObject(sourceString) {
-  let styles = sourceString.trim().split('\n').join('').split(';').slice(0, -1);
+  let styles = sourceString
+    .trim()
+    .split('\n')
+    .join('')
+    .split(';')
+    .slice(0, -1);
   let obj = {};
   for (let i = 0; i < styles.length; i++) {
     let temp = styles[i].trim().split(': ');

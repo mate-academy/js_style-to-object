3b9eae4956ff8dfb758f2bfd57cbbe3515269ec7
ghj ghj <StasGostev>
2019-04-12 01:31:35 +0300

    'Solution_of_Convert_to_object_by_Stas_Gostev'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,17 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  let strSplited = sourceString.split(';');
+  let cssStyle = {};
+
+  for (let propertie of strSplited) {
+    let key = propertie.split(':')[0];
+    let val = propertie.split(':')[1];
+    if (val !== undefined) {
+      cssStyle[key.trim()] = val.trim();
+    }
+  }
+  return cssStyle;
 }
 
 module.exports = convertToObject;

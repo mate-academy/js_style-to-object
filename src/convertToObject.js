3a90ae79fccfafb6d8b5f10d2faed338da903574
Alexei Belous <belous.aleksei18@gmail.com>
2019-04-11 15:25:59 +0300

    2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,16 +13,12 @@
  */
 function convertToObject(sourceString) {
   let result = {};
-  let properties = sourceString.split(';').slice(0, -1);
-  let keys = [];
-  let values = [];
+  let properties = sourceString.split(';');
   for (let property of properties) {
-    let parts = property.split(':');
-    keys.push(parts[0].trim());
-    values.push(parts[1].trim());
-  }
-  for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
+    let [key, value] = property.split(':');
+    if (value !== undefined) {
+      result[key.trim()] = value.trim();
+    }
   }
   return result;
 };

5055b8a8e5eecb50d05459a72f509478b0e545ce
ghj ghj <StasGostev>
2019-04-12 11:05:12 +0300

    'Fixed_Style_to_object'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,12 +13,11 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  let strSplited = sourceString.split(';');
+  let arrayOfString = sourceString.split(';');
   let cssStyle = {};
 
-  for (let propertie of strSplited) {
-    let key = propertie.split(':')[0];
-    let val = propertie.split(':')[1];
+  for (let propertie of arrayOfString) {
+    let [key, val] = propertie.split(':');
     if (val !== undefined) {
       cssStyle[key.trim()] = val.trim();
     }

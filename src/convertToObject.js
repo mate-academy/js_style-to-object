918c67c4e479d6077c531f544bc46e9aefd7327a
DimaKashyrin <kashyrindima@gmail.com>
2019-04-12 12:11:21 +0300

    convertObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * Implement convertToObject function:
  *
@@ -12,7 +11,14 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let arr = sourceString.split(';');
+  let obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].split(':')[0] && arr[ i ].split(':')[1]) {
+      obj[arr[ i ].split(':')[0].trim()] = arr[ i ].split(':')[ 1 ].trim();
+    }
+  }
+  return obj;
 }
 
 module.exports = convertToObject;

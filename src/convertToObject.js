ac47f648b55ea134cfdd5eeea3253e0774413335
Sorumond <sorumond@gmail.com>
2019-05-12 20:31:53 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,17 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const arr = sourceString.split('\n  ');
+  for (let i = 1; i < arr.length; i++) {
+    let withOutSemiColon = arr[i].replace(';', '');
+    withOutSemiColon = withOutSemiColon.replace('\n', '');
+    const objKey = withOutSemiColon.split(': ');
+    obj[objKey[0]] = objKey[1];
+  }
+  return obj;
 }
 
 module.exports = convertToObject;

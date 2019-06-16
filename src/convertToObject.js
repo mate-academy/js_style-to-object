d1a445557a2791749941eb8088915398e637cc2e
h2ash <hun22ash@gmail.com>
2019-06-16 22:38:43 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,14 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const string = sourceString.replace(/;/g, ':').replace(/\n/g, '').split(':');
+
+  const object = {};
+  for (let i = 0; i < string.length - 1; i += 2) {
+    object[string[i].trim()] = string[i + 1].trim();
+  }
+
+  return object;
 }
 
 module.exports = convertToObject;

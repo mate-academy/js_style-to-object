3a562d622a4cba3a5bd0d121dfe9db3f86ae6dbe
bratyslav <beginner.altavista@gmail.com>
2019-06-17 11:59:29 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let cssString = sourceString.slice(1);
+  const cssObj = {};
+  let cssProperty = '';
+  let semicolonsIndex = 0;
+  let colonIndex = 0;
+  let key = '';
+  let value = '';
+  while (cssString.length !== 0) {
+    semicolonsIndex = cssString.indexOf(';');
+    cssProperty = cssString.slice(2, semicolonsIndex);
+    colonIndex = cssProperty.indexOf(':');
+    key = cssProperty.slice(0, colonIndex);
+    value = cssProperty.slice(colonIndex + 2);
+    cssObj[key] = value;
+    cssString = cssString.slice(semicolonsIndex + 2);
+  }
+  return cssObj;
 }
 
 module.exports = convertToObject;

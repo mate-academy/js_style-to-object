ba3366e86a527ce9a6de842481c8fde53708798d
Taisiia <taisiia.pomazan@gmail.com>
2019-06-18 17:21:55 +0300

    sol

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,12 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const str = sourceString.replace(/;/g, ':').replace(/\n/g, '').split(':');
+  const obj = {};
+  for (let i = 0; i < str.length - 1; i += 2) {
+    obj[str[i].trim()] = str[i + 1].trim();
+  }
+  return obj;
 }
 
 module.exports = convertToObject;

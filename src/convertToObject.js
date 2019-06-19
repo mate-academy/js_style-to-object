6f4974fdade56ef772d9c9a2c03aaf8067fc1cdf
bratyslav <beginner.altavista@gmail.com>
2019-06-19 11:24:55 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -17,7 +17,7 @@ function convertToObject(sourceString) {
   let keyValueArr = [];
   for (let i = 0; i < stylesArray.length; i++) {
     keyValueArr = stylesArray[i].split(': ');
-    keyValueArr[1] = keyValueArr[1].slice(0, keyValueArr[1].length - 1);
+    keyValueArr[1] = keyValueArr[1].replace(';', '');
     stylesObject[ keyValueArr[0] ] = keyValueArr[1];
   }
   return stylesObject;

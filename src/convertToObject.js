fe5c3fa692e8592007a607305f77a2f3b1ae1296
Serhii <davydko.serhiy@gmail.com>
2019-06-20 08:44:28 +0300

    delete console log, add for each loop

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,13 +15,12 @@ function convertToObject(sourceString) {
   const properties = sourceString.split(';')
     .map(item => item.trim().split(':'));
   const result = {};
-  for (const i in properties) {
-    if (properties[i][1]) {
-      result[properties[i][0].trim()] = properties[i][1].trim();
+  properties.forEach(elem => {
+    if (elem[1]) {
+      result[elem[0].trim()] = elem[1].trim();
     }
-  }
+  });
   return result;
   // write your code here
 }
-console.log(convertToObject('Свойство: значение ; свойство:значение'));
 module.exports = convertToObject;

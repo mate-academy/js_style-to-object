93476cbf7d3c1de46862fd104b2a1a372f0978dc
vladyslav223 <vladyslav223@gmail.com>
2019-08-16 18:28:21 +0300

    ok

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,15 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const sourceStringNned = sourceString.replace(/\n/g, '');
+  const obj = {};
+  const arr = sourceStringNned.split(';');
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      const item = arr[i].split(':');
+      obj[item[0]] = item[1];
+    }
+  }
+  return obj;
 }
-
 module.exports = convertToObject;

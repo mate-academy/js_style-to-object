c4aff19d9cfd4ff0a288a46524637893853eacbf
Vyacheslav Piskun <webdev.pv@gmail.com>
2019-08-16 17:31:14 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,28 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let sourceInLine = '';
+  for (let i = 0; i < sourceString.length; i++) {
+    if (sourceString[i] !== '\n') {
+      sourceInLine += sourceString[i];
+    }
+  }
+
+  const arrFromSourse = sourceInLine.split(';');
+  const arrayOfProperties = [];
+  for (const item of arrFromSourse) {
+    if (item !== '') {
+      arrayOfProperties.push(item);
+    }
+  }
+
+  const ListOfProperties = {};
+  for (const item of arrayOfProperties) {
+    const [key, value] = item.split(':');
+    ListOfProperties[key.trim()] = value.trim();
+  }
+
+  return ListOfProperties;
 }
 
 module.exports = convertToObject;

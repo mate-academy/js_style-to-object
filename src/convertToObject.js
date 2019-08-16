58d58f864f0f10281638543ef4ecee3b3e36a63e
KseniiaMazan <ksusamazan@gmail.com>
2019-08-16 16:32:58 +0300

    fixed comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,11 +13,11 @@
  */
 function convertToObject(sourceString) {
   const sourceObj = {};
-  const sourceArr = sourceString.split('\n  ').join('').split(';');
+  const sourceArr = sourceString.split(';');
   sourceArr.length = sourceArr.length - 1;
 
   for (let i = 0; i < sourceArr.length; i++) {
-    const intermidiateArr = sourceArr[i].split(': ');
+    const intermidiateArr = sourceArr[i].trim().split(': ');
     sourceObj[intermidiateArr[0]] = intermidiateArr[1];
   }
 

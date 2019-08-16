b66aed0c3889e584b37668eb2bc99375a8b5e3d3
aleks-mos <mossssss@ukr.net>
2019-08-16 19:16:06 +0300

    change doc

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,10 +15,12 @@ function convertToObject(sourceString) {
   const stylePropsObject = {};
   const styleString = sourceString.split(';');
   let styleElement;
+
   for (let i = 0; i < styleString.length; i++) {
     styleElement = styleString[i].trim().split(': ');
     stylePropsObject[styleElement[0]] = styleElement[1];
   }
   return stylePropsObject;
 }
+
 module.exports = convertToObject;

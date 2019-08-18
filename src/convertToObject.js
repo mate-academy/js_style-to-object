cca3b55a3d9b44e80208b23806abee4e1bcb128b
Alina Kizilova <alinakzilova@gmail.com>
2019-08-18 10:38:41 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,7 +15,7 @@ function convertToObject(sourceString) {
   const stylesArray = sourceString.split(';');
   const stylesObject = {};
 
-  for (let i = 0; i < stylesArray.length; i++) {
+  for (let i = 0; i < stylesArray.length - 1; i++) {
     const styleDeclaration = stylesArray[i].trim().split(': ');
     stylesObject[styleDeclaration[0]] = styleDeclaration[1];
   }

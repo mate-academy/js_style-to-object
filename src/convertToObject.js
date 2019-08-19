91acb804f2b731d0401c5aff6aeb350ac8f3b718
Volodsher <volodsher@gmail.com>
2019-08-19 09:28:10 +0300

    after refactoring

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,8 +3,9 @@
 function convertToObject(sourceString) {
   const newObj = {};
   let property;
-  const newArr = sourceString.split(';\n');
+  const newArr = sourceString.split(';');
   newArr.pop();
+
   for (let i = 0; i < newArr.length; i++) {
     property = newArr[i].trim().split(': ');
     newObj[property[0]] = property[1];

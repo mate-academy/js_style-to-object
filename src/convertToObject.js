a94490969ab88aa0f7d7cf4e1a6d62d243a34103
Vladimir <koretskyi@gmail.com>
2019-08-20 03:01:56 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,10 +14,12 @@
 function convertToObject(sourceString) {
   const strToObj = {};
   const strToArr = sourceString.split(';');
-  for (let i = 0; i < strToArr.length; i++) {
+
+  for (let i = 0; i < strToArr.length - 1; i++) {
     const tempArr = strToArr[i].trim().split(': ');
     strToObj[tempArr[0]] = tempArr[1];
   }
+
   return strToObj;
 }
 

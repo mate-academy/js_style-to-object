58adb4436262f707c717160ec596f20a548a5077
Serhii-P <serhii.pyskun@gmail.com>
2019-08-20 08:16:46 +0300

    'Solution'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,14 +13,11 @@
  */
 function convertToObject(sourceString) {
   const obj = {};
-
   const arr = sourceString.split(';');
   for (let i = 0; i < arr.length; i++) {
     const elements = arr[i].trim().split(': ');
-
     obj[elements[0]] = elements[1];
   }
-
   return obj;
 }
 

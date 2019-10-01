e31c551f69453f9245359c1e80954119bbee3fb6
Veronika D <donets.veronika@yahoo.com>
2019-10-01 15:06:05 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const splitPattern = /;\n/g;
+  const splitStr = sourceString.split(splitPattern);
+  splitStr.pop();
+  let point;
+
+  for (let i = 0; i < splitStr.length; i++) {
+    point = splitStr[i].trim().indexOf(': ');
+    const key = splitStr[i].trim().slice(0, point);
+    result[key] = splitStr[i].trim().slice((point + 2), (splitStr[i].length));
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

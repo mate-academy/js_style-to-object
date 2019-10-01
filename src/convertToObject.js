990fd39e9dc173bf5617de00b933855dbbdae628
ITStan <s.stanislavskaya@gmail.com>
2019-10-01 19:58:25 +0300

    Correction

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,19 +11,30 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
+  /* Разбиваем строку со стилями на массив объектов arr,
+  в каждом элементе которого записано одно свойство из стилей. */
   const arr = sourceString.split(';');
+
   let arr2 = [];
   const obj = {};
 
-  for (const item of arr) {
-    arr2 = item.split(': ');
-  }
+  // Удаляем последний элемент массива arr - перенос строки.
+  arr.pop();
 
-  for (let i = 0; i < arr.length - 1; i++) {
+  // Элементы массива arr разбиваем по : на две части и записываем в объект.
+  arr.forEach(item => {
+    arr2 = item.split(':');
+    obj[arr2[0].trim()] = arr2[1].trim();
+  });
+
+  /* Вариант 2:
+  for (let i = 0; i < arr.length; i++) {
     arr2 = arr[i].trim().split(': ');
     obj[arr2[0]] = arr2[1];
-  }
+  } */
+
   return obj;
 }
 

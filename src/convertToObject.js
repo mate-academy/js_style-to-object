f6b5d589ab00140201d1b925d28f54c6a142195f
Sijey <popov040391@gmail.com>
2019-10-01 13:06:18 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,15 +12,15 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const obj = {};
+  const styles = {};
   const styleArr = sourceString.split(';');
 
   for (let i = 0; i < styleArr.length - 1; i++) {
-    const item = styleArr[i].trim().split(': ');
-    obj[item[0]] = item[1];
+    const item = styleArr[i].trim().split(':');
+    styles[item[0]] = item[1].trim();
   }
 
-  return obj;
+  return styles;
 }
 
 module.exports = convertToObject;

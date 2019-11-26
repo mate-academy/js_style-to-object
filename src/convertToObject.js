9c3dd752a7386bb34086bb8af609217702a0f755
Vivych Oksana <vivichokcana@gmail.com>
2019-11-27 00:30:23 +0200

    'Solution'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObj = {};
+  sourceString.replace(/\n/g, ' ')
+    .replace(/\s+/g, ' ')
+    .split(';')
+    .map(style => style.trim()
+      .replace(/ :/, ':'))
+    .filter(i => i !== '')
+    .map(style => style.split(':'))
+    .forEach(function(elem) {
+      styleObj[elem[0]] = elem[1].trim();
+    });
+
+  return styleObj;
 }
 
 module.exports = convertToObject;

074a39ce32254e0ac5d16a90bb0781df88934ea7
Senya Volkov <formobileprima@gmail.com>
2020-01-30 20:03:13 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const splited = sourceString.split(',');
+
+  for (let i = 0; i < splited.length; i++) {
+    const splited2 = splited[i].split(':');
+    let [property, value] = [...splited2];
+
+    if (property === undefined || value === undefined) {
+      continue;
+    }
+    property = property.trim();
+    value = value.trim();
+    obj[property] = value;
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

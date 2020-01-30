22461628b3a3d76942e46f5f6eb6081d67443e38
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-30 15:36:18 +0200

    used reduce

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,57 +12,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const styles = {};
-  const arrFromStr = sourceString.split(';');
+  const result = sourceString.split(';').reduce((acc, keyValue) => {
+    const style = keyValue.split(':');
 
-  arrFromStr.forEach(item => {
-    const subArr = item.split(':');
+    if (style.length === 2) {
+      let [key, value] = style;
 
-    if (subArr.length === 2) {
-      const key = subArr[0].toString().trim();
-      const value = subArr[1].toString().trim();
+      key = key.toString().trim();
+      value = value.toString().trim();
 
-      styles[key] = value;
+      return {
+        ...acc,
+        [key]: value,
+      };
+    } else {
+      return acc;
     }
-  });
+  }, {});
 
-  return styles;
+  return result;
 }
-
-convertToObject(`
-background-color:      #fff;
--webkit-border-radius: 5px;
-border-radius     : 5px;
-border: 1px solid #e8e8e8;
--webkit-box-sizing: border-box;
-box-sizing: border-box;
-clear   : both       ;
-cursor: pointer;
-float: left;
-font-family: inherit;
-    font-size: 14px;
-font-weight: 400;
-height: 42px;
-line-height:    40px;
-outline: 0;
-padding-left    : 18px;
-padding-right: 30px;
-;
-
-;
-position: relative;
-
-
-text-align: left !important;
--webkit-transition: all .2s ease-in-out;
-transition: all .2s ease-in-out;
--moz-user-select: none;
--ms-user-select: none;
-user-select: none;
-
-
-white-space: nowrap;
-width: auto;
-`);
 
 module.exports = convertToObject;

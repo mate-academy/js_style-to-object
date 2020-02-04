1f73f1d2f75351f231bfe86ac7c0472150f36f66
Roma <romakoder@gmail.com>
2020-02-04 17:18:24 +0200

    fixed comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,14 +14,12 @@
 function convertToObject(sourceString) {
   const stylesObject = {};
 
-  sourceString.replace(/\s+/g, ' ')
-    .replace(/\s+:/g, ':')
-    .trim()
+  sourceString.replace(/\s+:/g, ':')
     .split(';')
     .map(styleCss => styleCss.trim().split(': '))
     .forEach(propertyCss => {
       if (propertyCss[0] !== '') {
-        stylesObject[propertyCss[0]] = propertyCss[1];
+        stylesObject[propertyCss[0]] = propertyCss[1].trim();
       }
     });
 

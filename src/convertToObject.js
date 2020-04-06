6418d2089584406a85da13830245e3b2a804bfaf
crash2bash <sachagurov@gmail.com>
2020-04-06 18:00:32 +0300

    fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,18 +13,18 @@
  */
 function convertToObject(sourceString) {
   const result = {};
-  const arr = sourceString.replace(/\n/g, '')
-    .split(';');
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== '') {
-      const arrElement = arr[i].split(':');
+  sourceString.replace(/\n/g, '')
+    .split(';')
+    .map(el => {
+      if (el !== '') {
+        const arrElement = el.split(':');
 
-      if (arrElement.length > 1) {
-        result[arrElement[0].trim()] = arrElement[1].trim();
+        if (arrElement.length > 1) {
+          result[arrElement[0].trim()] = arrElement[1].trim();
+        }
       }
-    }
-  }
+    });
 
   return result;
 }

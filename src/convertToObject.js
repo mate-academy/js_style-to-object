7050f81cb09fec751fcb127be96872dc97b95e52
Andreas_Just <andreas.wq@gmail.com>
2020-04-09 00:49:43 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,20 +13,10 @@
  */
 function convertToObject(sourceString) {
   return Object.fromEntries(sourceString.split(';')
-    .map(entries => entries.split(':')
-      .map(words => words.split(' ')
-        .filter(
-          word => word !== '\n'
-          && word !== '\n\n'
-          && word !== '\n\n\n'
-          && word.length > 0
-        )
-        .map(x => x.trim().replace(/^\\n/))
-        .join(' ')
-      ))
-    .filter(
-      group => group.length !== 1
-    )
+    .map(rule => rule.trim())
+    .filter(Boolean)
+    .map(rule => rule.split(':', 2))
+    .map(parts => [parts[0].trim(), parts[1].trim()])
   );
 }
 

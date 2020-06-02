4ef42c9efb21b784da8ca32c7c79abaa37efd7c6
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-06-02 18:20:54 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let styles = sourceString.split(';');
+  const regex = /[\w-.!#]+/g;
+  let property = [];
+  const stylesObject = {};
+
+  styles = styles.filter(x => x.includes(':'));
+
+  for (let i = 0; i < styles.length; i++) {
+    property = styles[i].match(regex);
+
+    if (property.length > 2) {
+      stylesObject[property[0]] = property.slice(1).join(' ');
+    } else {
+      stylesObject[property[0]] = property[1];
+    }
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

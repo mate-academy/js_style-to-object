5ef1d9c00ffae624d9b7689f154eb914e82488ef
solaryasha <solaryasha@gmail.com>
2020-06-03 12:26:30 +0300

    Fix variable names

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -17,8 +17,10 @@ function convertToObject(sourceString) {
 
   const regex = /([^\s;`]+)\s*:\s*([#\w\d-]*\s?[!.#\w\d-]*\s?[#\w\d-]+)/g;
 
-  for (const [, value2, value3] of sourceString.matchAll(regex)) {
-    styleProperties[value2] = value3;
+  const matches = sourceString.matchAll(regex);
+
+  for (const [, property, value] of matches) {
+    styleProperties[property] = value;
   }
 
   return styleProperties;

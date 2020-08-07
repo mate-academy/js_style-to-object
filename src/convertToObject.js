b5fa0034f585c5e7f530b1b2e70f7b4033efa9a9
Dennis Priadka <dennis.priadka@gmail.com>
2020-08-07 17:03:51 +0300

    +

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const properties = sourceString.replace(/ +/g, ' ')
+    .split('\n').join('').split(';').join().split(':').join().split(',');
+
+  for (const i in properties) {
+    properties[i] = properties[i].trim();
+  }
+
+  for (let i = 0; i < properties.length; i += 2) {
+    if (properties[i]) {
+      result[properties[i]] = properties[i + 1];
+    }
+  }
+  delete result['\'\''];
+
+  return result;
 }
 
 module.exports = convertToObject;

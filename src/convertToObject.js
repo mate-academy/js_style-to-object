32030b61e323826105e0524ba877b5141fce3b74
ArtemiiKab <artemiikab@gmail.com>
2020-08-11 14:49:06 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString
+    .split(';')
+    .filter(string => string.length > 5)
+    .map(prop => {
+      const separator = prop.indexOf(':');
+
+      result[prop.slice(0, separator).trim()]
+      = prop.slice(separator + 1).trim();
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

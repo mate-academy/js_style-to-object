d8eb076b737a32a69a644fee6e02c09d3f463a90
Maxym Chernyavskiy <masyatya@gmail.com>
2020-08-12 14:39:28 +0300

    soltuion

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arr = sourceString
+    .split(';')
+    .filter(Boolean)
+    .map(elem => elem.replace(/\s+/, ''))
+    .filter(Boolean);
+
+  const func = (a, b) => {
+    const property = (b.split(':')
+      .map(elem => elem.split(' ').filter(Boolean).join(' '))
+    );
+
+    return {
+      ...a,
+      [property[0]]: property[1],
+    };
+  };
+
+  return arr.reduce(func, {});
 }
 
 module.exports = convertToObject;

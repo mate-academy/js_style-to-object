1a3a3847890fed72afab8e48311c9985ac8a601b
sobchuksasha <sobchuksasha1995@gmail.com>
2020-08-12 15:00:25 +0300

    vers

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,14 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arr = sourceString.split(';\n').filter(elem => elem.includes(':'));
+  const obj = {};
+
+  for (const elem of arr) {
+    obj[elem.split(': ')[0].trim()] = elem.split(': ')[1].trim();
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

085e40534a6e9c9d5f87eb197b92605b0cf62821
Tania Omelko <tanyaomelko39@gmail.com>
2020-08-17 12:51:25 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,15 +15,18 @@ function convertToObject(sourceString) {
   const object = {};
   const arr = sourceString.replace(/\s+/g, ' ').split(';');
 
-  for (let i of arr) {
-    i = i.trim();
+  for (let string of arr) {
+    string = string.trim();
 
-    if (!i) {
-      arr.slice(i, 1);
+    if (!string) {
+      arr.slice(0, 1);
       continue;
     }
-    object[i.split(': ')[0].trim()] = i.split(': ')[1];
-    i++;
+
+    const value = string.split(':');
+
+    object[value[0].trim()] = value[1].trim();
+    string++;
   }
 
   return object;

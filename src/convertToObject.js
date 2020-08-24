32f5fba32ab4b6e5be208f8bf914797b6858b0b6
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-25 01:58:09 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,11 +12,15 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const res = sourceString.replace(/[\r\n]/g, '').split(';');
+  const res = sourceString.split(';');
   const result = {};
 
-  res.map(x => x.split(':')).filter(x => x.length > 1).forEach(property => {
-    result[property[0].trim()] = property[1].trim();
+  res.forEach(value => {
+    const property = value.split(':');
+
+    if (property.length > 1) {
+      result[property[0].trim()] = property[1].trim();
+    }
   });
 
   return result;

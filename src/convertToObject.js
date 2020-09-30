3d83b040e8139c5ed372d8258d9e3bb4bae40eb8
andlap24 <andrew.laptev24@gmail.com>
2020-09-30 13:19:48 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  
+  const stylesArray = sourceString.split(';');
+  const stylesArrayFromated = [];
+  const stylesArraySplited = [];
+  let result = {};
+
+  for (let i = 0; i < stylesArray.length; i++) {
+    stylesArrayFromated[i] = stylesArray[i].replace(/\s+/g, ' ');
+  }
+
+  for (let j = 0; j < stylesArrayFromated.length; j++) {
+    if (stylesArrayFromated[j].length > 1) {
+      stylesArraySplited.push(stylesArrayFromated[j].split(':'));
+    }
+  }
+
+  for (const style of stylesArraySplited) {
+    const trimKey = style[0].trim();
+    const trimValue = style[1].trim();
+
+    style[0] = trimKey;
+    style[1] = trimValue;
+  }
+
+  result = Object.fromEntries(stylesArraySplited);
+
+  return result;
 }
 
 module.exports = convertToObject;

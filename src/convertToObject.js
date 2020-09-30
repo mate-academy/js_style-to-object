4767e7b13498743003cffb21eeec092aaecebc66
andlap24 <andrew.laptev24@gmail.com>
2020-09-30 15:35:00 +0300

    implement new logic

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,32 +12,13 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const stylesArray = sourceString.split(';');
-  const stylesArrayFromated = [];
-  const stylesArraySplited = [];
-  let result = {};
+  const result = sourceString
+    .split(';')
+    .map(cssRule => cssRule.split(':')
+      .map(cssRuleItem => cssRuleItem.trim()))
+    .filter(cssRule => cssRule.length > 1);
 
-  for (let i = 0; i < stylesArray.length; i++) {
-    stylesArrayFromated[i] = stylesArray[i].replace(/\s+/g, ' ');
-  }
-
-  for (let j = 0; j < stylesArrayFromated.length; j++) {
-    if (stylesArrayFromated[j].length > 1) {
-      stylesArraySplited.push(stylesArrayFromated[j].split(':'));
-    }
-  }
-
-  for (const style of stylesArraySplited) {
-    const trimKey = style[0].trim();
-    const trimValue = style[1].trim();
-
-    style[0] = trimKey;
-    style[1] = trimValue;
-  }
-
-  result = Object.fromEntries(stylesArraySplited);
-
-  return result;
+  return Object.fromEntries(result);
 }
 
 module.exports = convertToObject;

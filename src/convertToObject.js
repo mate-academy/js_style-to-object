1c4f9e7cfd1c3ad112080a2c775a49c7eb5fed36
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-01 12:49:07 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceStringPars = sourceString.split(';');
+  const cssRuls = {};
+
+  sourceStringPars
+    .forEach(rule => {
+      const rulePars = rule
+        .split(':')
+        .map(ruleItem => ruleItem.trim());
+
+      if (rulePars[0] && rulePars[1]) {
+        cssRuls[rulePars[0]] = rulePars[1];
+      }
+    });
+
+  return cssRuls;
 }
 
 module.exports = convertToObject;

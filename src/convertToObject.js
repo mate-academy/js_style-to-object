6597c1b935bf7490ded0b208715943db734c2ff8
Atteroot <yuora556@gmail.com>
2020-10-06 18:50:24 +0300

    changed returnString value name to cssRules

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -2,18 +2,18 @@
 
 function convertToObject(sourceString) {
   const splittedSourceString = sourceString.split(';');
-  const resultString = {};
+  const cssRules = {};
 
   for (const rules of splittedSourceString) {
     if (rules.trim()) {
       const key = rules.split(':')[0].trim();
       const value = rules.split(':')[1].trim();
 
-      resultString[key] = value;
+      cssRules[key] = value;
     }
   }
 
-  return resultString;
+  return cssRules;
 }
 
 module.exports = convertToObject;

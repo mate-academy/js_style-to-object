8b826efbad453c316dc496109a36fd19b351e6fa
Igor Lisogor <illisogori@gmail.com>
2020-11-12 15:07:30 +0200

    convertToObject implemented

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultObject = {};
+  let propArray = sourceString.split(';');
+
+  propArray = propArray.map(
+    prop => prop.slice(
+      prop.lastIndexOf('\n') + 1, prop.length));
+  propArray = propArray.map(prop => prop.trim());
+  propArray = propArray.filter(prop => prop !== '');
+
+  propArray.forEach(function(prop) {
+    resultObject[(prop.split(':')[0]).trim()] = (prop.split(':')[1]).trim();
+  });
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

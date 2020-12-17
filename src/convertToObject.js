460bdb811aec174f60e6719b0384e50a558bf94f
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-17 19:08:11 +0200

    Completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const pairs = sourceString.split(';');
+  let validPairs = pairs.map((n) => n.trim());
+  const validItems = [];
+  const result = {};
+
+  validPairs = validPairs.filter((n) => n !== '');
+
+  const items = validPairs.map((n) => n.split(':'));
+
+  items.map((m, index) => {
+    validItems.push([]);
+    validItems[index].push(items[index][0].trim());
+    validItems[index].push(items[index][1].trim());
+  });
+
+  validItems.forEach((item) => {
+    result[item[0]] = item[1];
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

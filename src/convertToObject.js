a1f2a61f9b3fa8b84dc1125c331db945e200a41b
Oleksii Spalylo <white.rh42@gmail.com>
2020-12-21 20:38:45 +0200

    converted string to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const objOfStyles = {};
+
+  const arrayOfStyles = sourceString.split(';');
+
+  arrayOfStyles
+    .filter(item => item.length > 5)
+    .map((item) => item.split(':'))
+    .map((item) => [item[0].trim(), item[1].trim()])
+    .forEach(item => {
+      objOfStyles[item[0]] = item[1];
+    });
+
+  return objOfStyles;
 }
 
 module.exports = convertToObject;

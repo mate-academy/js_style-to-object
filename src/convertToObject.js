28fdff53779805f41ac3d2486ee6ca93e0758797
Dubchak Marina <dubchmar@gmail.com>
2020-12-21 22:33:43 +0200

    added solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,15 +14,15 @@
 
 function convertToObject(sourceString) {
   const arrayOfStyles = {};
+  const properties = sourceString.split(';');
 
-  sourceString.split(';')
-    .map(property => property
-      .split(':')
-      .map(item => item.trim())).forEach(rule => {
-      if (rule[0].length !== 0) {
-        arrayOfStyles[rule[0]] = rule[1];
-      }
-    });
+  for (let i = 0; i < properties.length; i++) {
+    properties[i] = properties[i].split(':').map(rule => rule.trim());
+
+    if (properties[i][0].length !== 0) {
+      arrayOfStyles[properties[i][0]] = properties[i][1];
+    }
+  };
 
   return arrayOfStyles;
 }

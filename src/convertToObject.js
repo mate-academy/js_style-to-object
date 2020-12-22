79c232e458b86ce51903c77f9ae5cd2fd8027e0f
dinexpod <work.toxa@gmail.com>
2020-12-22 17:47:38 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const res = {};
+
+  sourceString
+    .split(';')
+    .map(s => s.trim())
+    .filter(e => e)
+    .map(s => s.split(':').map(props => props.trim()))
+    .forEach(e => {
+      res[e[0]] = e[1];
+    });
+
+  return res;
 }
 
 module.exports = convertToObject;

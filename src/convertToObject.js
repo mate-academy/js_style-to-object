27e6f1bbaa09b8f7caa365e5468492e0973722f2
Maksym Maksymenko <finside41@gmail.com>
2021-01-24 21:11:36 +0200

    Solution2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,16 +13,31 @@
  */
 function convertToObject(sourceString) {
   const cssOnly = {};
+  const sourceStringArr = sourceString.split(';');
+  const cssKeysAndValuesArr = sourceStringArr.map((item) => item.split(':'));
 
-  sourceString.split(';').map(item => {
-    const [key, value] = item.split(':').map(word => word.trim());
-
-    if (key && value) {
-      return (cssOnly[key] = value);
+  for (const element of cssKeysAndValuesArr) {
+    if (element[1]) {
+      cssOnly[element[0].trim()] = element[1].trim();
     }
-  });
+  }
 
   return cssOnly;
 }
 
 module.exports = convertToObject;
+
+// function convertToObject(sourceString) {
+//   // write your code here
+//   const cssOnly = {};
+
+//   sourceString.split(';').map(item => {
+//     const [key, value] = item.split(':').map(word => word.trim());
+
+//     if (key && value) {
+//       return (cssOnly[key] = value);
+//     }
+//   });
+
+//   return cssOnly;
+// }

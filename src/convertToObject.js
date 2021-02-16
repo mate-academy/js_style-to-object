ff9639557c0eb998d7fc61026ddfa660ef2aa7d8
Bohdan Dobak <chudolipton@gmail.com>
2021-02-16 12:53:56 +0200

    formatted the code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,15 +13,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const sourceArr = sourceString.split(';').map(x => x.split(': '));
-  const clearSourceArr = sourceArr.map(x => x.map(y => y.trim()));
+  const sourceArr = sourceString.split(';').map(words => words.split(': '));
+  const clearSourceArr = sourceArr
+    .map(word => word.map(letter => letter.trim())
+    );
 
-  return clearSourceArr.filter(x => x[1] !== undefined).reduce((obj, item) => {
-    return {
-      ...obj,
-      [item[0]]: item[1],
-    };
-  }, {});
+  return clearSourceArr
+    .filter(word => word[1] !== undefined).reduce((obj, item) => {
+      return {
+        ...obj,
+        [item[0]]: item[1],
+      };
+    }, {}
+    );
 }
 
 module.exports = convertToObject;

032a42d97c5bae73d7bc601f39118d29d5364cdd
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-17 10:13:56 +0200

    first commit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const entries = sourceString.split(';').map(item => item.split(':'));
+  const sourseWithoutSpace = entries.map(item => item.map(i => i.trim()));
+
+  for (let i = 0; i < sourseWithoutSpace.length; i++) {
+    if (sourseWithoutSpace[i][1] === undefined) {
+      i++;
+    } else {
+      result[sourseWithoutSpace[i][0]] = sourseWithoutSpace[i][1];
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

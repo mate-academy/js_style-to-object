0ddb7f946d4234a4537f8a4e9bdfbc31d987b1b7
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-02-19 10:29:57 +0200

    renamed the variables

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,9 +4,9 @@ function convertToObject(sourceString) {
   const result = {};
 
   Array.from(new Set(sourceString.split(';')))
-    .map(x => x.replace(['\n'], ''))
-    .map(x => x.split(':'))
-    .map(x => x.map(y => y.trim()))
+    .map(style => style.replace(['\n'], ''))
+    .map(style => style.split(':'))
+    .map(style => style.map(y => y.trim()))
     .map((style) => {
       if (style.length === 2) {
         result[style[0]] = style[1];

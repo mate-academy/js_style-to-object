fa506dd994296624a14bc333ada87ae6e24d8180
ivan Panchenko <ivanpanchenkorulit@gmail.com>
2021-02-20 12:14:39 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,18 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const prepearingForStyle = sourceString
+    .split(';').filter(element => element.includes(':'))
+    .map(styles => styles.split(':')
+      .map(partOfStyle => partOfStyle.trim()));
+
+  const readyToStyle = {};
+
+  for (const part of prepearingForStyle) {
+    readyToStyle[part[0]] = part[1];
+  }
+
+  return readyToStyle;
 }
 
 module.exports = convertToObject;

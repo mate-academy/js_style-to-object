79f0388e5aa356704ed71c9c6da8d050daa08377
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-02-22 01:26:36 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceArray = sourceString.split(';');
+
+  const trimmedArray = sourceArray.map(word => (word + '').trim());
+
+  const arrayWithValues = trimmedArray.map(string => string.split(':'));
+
+  const arrayWithoutEmptyValues = arrayWithValues.filter(value =>
+    value.length > 1);
+
+  const property = {};
+
+  function callback(array) {
+    property[array[0].trim()] = array[1].trim();
+
+    return property;
+  }
+
+  const result = arrayWithoutEmptyValues.map(callback)[0];
+
+  return result;
 }
 
 module.exports = convertToObject;

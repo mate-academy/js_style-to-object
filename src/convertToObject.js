d029b337adaab7ad479c37108c6140a93fb6cea8
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-02-22 15:28:56 +0200

    Solved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cascadingStyleSheets = {};
+  const stringConvertedToArray = sourceString.split(';')
+    .map(style => style.split(':'))
+    .map(pair => pair.map(element => element.trim()))
+    .filter(style => style.length === 2);
+
+  for (const style of stringConvertedToArray) {
+    cascadingStyleSheets[style[0]] = style[1];
+  }
+
+  return cascadingStyleSheets;
 }
 
 module.exports = convertToObject;

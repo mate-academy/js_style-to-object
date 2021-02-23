2037502ccc71dee5e82b1a721ba56d194cf2fb53
Oleg Bezriadin <muhachov86@gmail.com>
2021-02-23 18:51:39 +0200

    removed unnecessary regex

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,7 +15,7 @@ function convertToObject(sourceString) {
   const styles = {};
 
   sourceString.split(';').map(item => {
-    const declaration = item.replace(/[\n]/g, '').split(':');
+    const declaration = item.split(':');
 
     if (!!declaration[0] && !!declaration[1]) {
       styles[declaration[0].trim()] = declaration[1].trim();

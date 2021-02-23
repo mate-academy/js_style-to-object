1533e4f153cb558c5de767e27daccb1f287f82e2
Nikita Puhovskii <cheliojul@gmail.com>
2021-02-23 12:07:40 +0200

    fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,19 +13,19 @@
  */
 function convertToObject(sourceString) {
   const arrayOfProperties = sourceString.split('\n');
-  const replacedSymbols = arrayOfProperties
-    .map(string => string.replace(';', ' '))
-    .filter(string => string.trim() !== '');
-  const trimmedString = replacedSymbols.map(string => string.trim());
-  const keys = trimmedString.map(string => string.split(':')[0].trim());
-  const values = trimmedString.map(string => string.split(':')[1].trim());
-  const result = {};
+  const arrayOfValidProperties = arrayOfProperties
+    .map(property => property.split(';'))
+    .filter(property => property[0].length > 3)
+    .map(property => property.join('').split(':'))
+    .map(value => [value[0].trim(), value[1].trim()]);
 
-  for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
+  const styles = {};
+
+  for (let i = 0; i < arrayOfValidProperties.length; i++) {
+    styles[arrayOfValidProperties[i][0]] = arrayOfValidProperties[i][1];
   }
 
-  return result;
+  return styles;
 }
 
 module.exports = convertToObject;

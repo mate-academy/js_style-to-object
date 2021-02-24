a61b911e3cbe9b22cb670da87085817945fbcd33
Vasiliy Sergeev <the2producer@gmail.com>
2021-02-24 08:24:30 +0200

    stylesOfObjects

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+
+  const properties = sourceString
+    .split(';')
+    .map(property => (
+      property
+        .trimLeft()
+        .split(':')
+        .map(elementOfProperty => elementOfProperty.trim())
+    ));
+
+  for (let i = 0; i < properties.length; i++) {
+    if (properties[i].length > 1) {
+      styles[properties[i][0]] = properties[i][1];
+    }
+  }
+
+  return styles;
 }
 
 module.exports = convertToObject;

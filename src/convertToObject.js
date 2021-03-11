e1f5c76f0953df50b94a25144de73dd3b931621a
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-11 17:50:14 +0200

    Created function convertToObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,19 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const arr = sourceString.split(';').map(i => i.trim()).filter(
+    i => i.length !== 0);
+
+  for (let j = 0; j < arr.length; j++) {
+    arr[j] = arr[j].split(':').map(item => item.trim());
+    result[arr[j][0]] = arr[j][1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

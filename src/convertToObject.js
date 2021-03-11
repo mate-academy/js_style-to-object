6e517b06aeed91cfd4f2a7a5dbf7746fb1761586
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-11 15:10:32 +0200

    Created function convertToresultect

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,28 @@
 'use strict';
 
 /**
- * Implement convertToObject function:
+ * Implement convertToresultect function:
  *
  * Function takes string with styles
- * and returns object where CSS properties are keys
+ * and returns resultect where CSS properties are keys
  * and values are the values of related CSS properties
 
  * @param {string} sourceString
  *
- * @return {object}
+ * @return {resultect}
  */
-function convertToObject(sourceString) {
+function convertToresultect(sourceString) {
   // write your code here
+  const result = {};
+  const arr = sourceString.split(';').map(i => i.trim()).filter(
+    i => i.length !== 0);
+
+  for (let j = 0; j < arr.length; j++) {
+    arr[j] = arr[j].split(':').map(item => item.trim());
+    result[arr[j][0]] = arr[j][1];
+  }
+
+  return result;
 }
 
-module.exports = convertToObject;
+module.exports = convertToresultect;

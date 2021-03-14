62fa07204e7b98aa9c6d67d43f166153263ae637
Ruslan Kalyniak <krv241085krv@icloud.com>
2021-03-14 11:17:58 +0200

    test

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const result = {};
+  const arrSourceString = sourceString.split(';');
+
+  arrSourceString.filter(i => i.length > 1 && i !== ' ');
+
+  for (const element of arrSourceString) {
+    const css = element.split(':');
+
+    if (css[1] !== undefined) {
+      css[0] = css[0].trim();
+      css[1] = css[1].trim();
+      result[css[0]] = css[1];
+    }
+  }
+
+  return result;
+};
 
 module.exports = convertToObject;

ae81abd1c56d60531f7eb4166c375850b46b7513
Andriy Fesych <andriyhimself@gmail.com>
2021-04-06 00:14:08 +0300

    Solution_0.1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const a = sourceString.split('\n').join('').split(';');
+  // morfing a string by removing \n
+  // and splitting it by ';' to key/value strings
+  const cssParams = {}; // our resulting object
+
+  for (let i = 0; i < a.length; i++) {
+    a[i] = a[i].split(':'); // spliting to key/value pairs
+    a[i][0] = a[i][0].trim();
+
+    if (a[i][0].length === 0) { // check if key is invalid
+      continue; // meaning pair will be invalid as well
+    }
+    a[i][1] = a[i][1].trim(); // if pair valid triming value
+    cssParams[a[i][0]] = a[i][1]; // assigning key/value to an object
+  }
+
+  return cssParams;
 }
 
 module.exports = convertToObject;

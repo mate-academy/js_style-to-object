6fed3970ce4ef369629a2de27899c36445625edc
Dmytro Prontenko <dprontenko@gmail.com>
2021-04-16 19:42:32 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const splitArr = sourceString.split(';').map(item => item.trim());
+  const cssArr = splitArr.filter(function(item) {
+    return item.length > 0;
+  });
+
+  cssArr.forEach(x => {
+    const tempSelector = x.split(':').map(item => item.trim());
+
+    result[tempSelector[0]] = tempSelector[1];
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

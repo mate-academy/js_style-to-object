cde144c6190f61c3725846c566a0e63c5f0b3a59
Taras Konovalov <taras.konovalov2014@gmail.com>
2021-04-28 01:03:41 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,21 +12,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const obj = {};
-  const semicolon = sourceString.split(';');
-  const colon = semicolon.map(i => i.split(':'));
-  const delTrim = colon.map(del => {
-    return del.map(elem => {
-      return elem.trim();
+  const objResult = {};
+
+  const array = sourceString.split(';')
+    .map(elem => elem.split(':'))
+    .map(del => {
+      return del.map(elem => {
+        return elem.trim();
+      });
     });
-  });
-  const delEmptyArrays = delTrim.filter(item => item.length === 2);
 
-  delEmptyArrays.forEach(elem => {
-    obj[elem[0]] = elem[1];
-  });
+  array.filter(item => item.length === 2)
+    .forEach(elem => {
+      objResult[elem[0]] = elem[1];
+    });
 
-  return obj;
+  return objResult;
 }
 
 module.exports = convertToObject;

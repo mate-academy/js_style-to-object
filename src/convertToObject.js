8d7c46ff9a4723cbfd62bf3725ac1c796175f5d4
Ihor Karpyn <igor82200@gmail.com>
2021-04-28 13:55:45 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString.split('\n') // array key-value
+    .map(row => row.split(': ')) // array [key,value]
+    .filter(rule => rule.length === 2) // remove excess
+    .map(([prop, value]) => // remove space and semicolon
+      [prop.trim(), value.replace(';', '').trim()]
+    )
+    .reduce((convertList, [prop, value]) => ( // make object
+      {
+        ...convertList, ...{ [prop]: value },
+      }
+    ), {});
 }
 
 module.exports = convertToObject;

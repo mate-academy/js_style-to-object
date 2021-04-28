e15227223db9c2423f4f988a1cce868705550b2a
Ihor Karpyn <igor82200@gmail.com>
2021-04-28 16:35:56 +0300

    fix misprint

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,7 +13,7 @@
  */
 function convertToObject(sourceString) {
   return sourceString.split('\n') // array key-value
-    .map(row => row.split(': ')) // array [key,value]
+    .map(row => row.split(':')) // array [key,value]
     .filter(rule => rule.length === 2) // remove excess
     .map(([prop, value]) => // remove space and semicolon
       [prop.trim(), value.replace(';', '').trim()]

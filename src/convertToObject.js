c8a08639ddba25936126aab21bedb90876a155ff
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-30 00:41:41 +0300

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const convertStylesToArr = sourceString
+    .split('\n')
+    .filter(prop => prop.includes(':'));
+  const stylesObj = {};
+
+  convertStylesToArr.forEach(el => {
+    const style = el.slice(0, el.lastIndexOf(';'));
+    const t = style.split(':');
+    const [key, value] = [t[0].trim(), t[1].trim()];
+
+    stylesObj[key] = value;
+  });
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

aa9b31787a535454620905ecbbcad4678fefd839
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-03 18:46:03 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,36 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrFromStr = sourceString
+    .trim()
+    .split('\n')
+    .filter(item => item.includes(':'))
+    .map(str => {
+      const item = deleteSymb(str, ';');
+
+      let [prop, value] = item.split(':');
+
+      prop = prop.trim();
+      value = value.trim();
+
+      return [prop, value];
+    });
+
+  const objFromArr = {};
+
+  for (const [key, value] of arrFromStr) {
+    objFromArr[String(key)] = value;
+  }
+
+  return objFromArr;
+}
+
+function deleteSymb(str, symb) {
+  const arr = str
+    .split('')
+    .filter(char => char !== symb);
+
+  return arr.join('');
 }
 
 module.exports = convertToObject;

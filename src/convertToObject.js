7e1f667ba8320795f667a9a865af939f69432cac
Vasyl Habalevych <vasylhab@gmail.com>
2021-05-04 13:00:35 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const normalizeString = sourceString.replace(/[ \r\n]/g, '')
+    .replace('solid', ' solid ')
+    .replace('!important', ' !important')
+    .replace(/\.2s/g, ' .2s ')
+    .split(';');
+
+  const styles = {};
+
+  for (let style of normalizeString) {
+    style = style.split(':');
+
+    if (style.length > 1) {
+      styles[style[0]] = style[1];
+    }
+  }
+
+  return styles;
 }
 
 module.exports = convertToObject;

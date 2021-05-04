f217cdcf07d506cbe61f7ca3a7b5c6f997247e04
Igor Lisogor <illisogori@gmail.com>
2021-05-04 22:37:27 +0300

    style to object  implemented

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const object = {};
+  let properties = sourceString.split(';').map(property => property.trim());
+
+  properties = properties.filter(property => property.length > 0);
+
+  properties = properties.map(property =>
+    `${property.split(':')[0].trim()}:${property.split(':')[1].trim()}`);
+
+  properties.forEach(
+    property => {
+      object[property.split(':')[0]] = property.split(':')[1];
+    }
+  );
+
+  return object;
 }
 
 module.exports = convertToObject;

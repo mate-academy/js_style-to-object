df0e0aa9aada9720ac3ff4e61c92a921d5d210bb
Ivan Kolodyazhnyi <80517717+IvanKolodiazhnyi@users.noreply.github.com>
2021-05-04 11:49:52 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const transformInToArr = sourceString.split(';');
+  const sortWithMap = transformInToArr.map(element => element.split(':'));
+  const result = {};
+
+  sortWithMap.map(element => {
+    if (element[1] !== undefined) {
+      result[element[0].trim()] = element[1].trim();
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

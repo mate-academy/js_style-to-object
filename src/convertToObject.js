d540c90332b4c4df73f3db54cf9d68668cd5b92f
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-05-05 14:49:14 +0300

    was changed some logic

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,16 +12,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
+  const stylesObj = {};
   const convertStylesToArr = sourceString
     .split('\n')
-    .filter(prop => prop.includes(':'));
-  const stylesObj = {};
+    .filter(prop => prop.includes(':'))
+    .map(el => {
+      const style = el.slice(0, el.lastIndexOf(';')).split(':');
+
+      style[0] = style[0].trim();
+      style[1] = style[1].trim();
+
+      return style;
+    });
 
   convertStylesToArr.forEach(el => {
-    const style = el.slice(0, el.lastIndexOf(';')).split(':');
-    const [key, value] = [style[0].trim(), style[1].trim()];
-
-    stylesObj[key] = value;
+    stylesObj[el[0]] = el[1];
   });
 
   return stylesObj;

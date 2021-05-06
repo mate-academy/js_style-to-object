ea40f4a06e9c2f6d6beaf218095475a8ec929e33
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-06 21:04:16 +0300

    project is fixed after mentors review

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,42 +6,17 @@
  * Function takes string with styles
  * and returns object where CSS properties are keys
  * and values are the values of related CSS properties
-
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const arrFromStr = sourceString
-    .trim()
-    .split('\n')
-    .filter(item => item.includes(':'))
-    .map(str => {
-      const item = deleteSymb(str, ';');
-
-      let [prop, value] = item.split(':');
-
-      prop = prop.trim();
-      value = value.trim();
-
-      return [prop, value];
-    });
-
-  const objFromArr = {};
-
-  for (const [key, value] of arrFromStr) {
-    objFromArr[String(key)] = value;
-  }
-
-  return objFromArr;
-}
-
-function deleteSymb(str, symb) {
-  const arr = str
-    .split('')
-    .filter(char => char !== symb);
-
-  return arr.join('');
+  return Object.fromEntries(sourceString
+    .split(';')
+    .map(property => property.split(':'))
+    .filter(propertyRecord => propertyRecord.length === 2)
+    .map(([propName, propVal]) => [propName.trim(), propVal.trim()])
+  );
 }
 
 module.exports = convertToObject;

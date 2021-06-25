f802bcb62aeffdc0afcd24f49f0a3efc1e4c290e
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-25 14:46:16 +0300

    first_try

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,29 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const convertedStyles = {};
+
+  const splitedStyles = sourceString.split(';');
+
+  const keysAndValues = splitedStyles.map(value => value.split(':'));
+
+  const onlyWithNeededLength = keysAndValues.filter(
+    value => value.length === 2);
+
+  const deleteSpaces = propertie => {
+    propertie[0] = propertie[0].trim();
+    propertie[1] = propertie[1].trim();
+  };
+
+  onlyWithNeededLength.map(deleteSpaces);
+
+  const addEntries = entry => {
+    convertedStyles[entry[0]] = entry[1];
+  };
+
+  onlyWithNeededLength.forEach(addEntries);
+
+  return convertedStyles;
 }
 
 module.exports = convertToObject;

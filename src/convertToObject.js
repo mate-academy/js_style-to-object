2fdc26f37fbc1dc7d186c6dc688ebc39e1168a9e
Vitaliy Martynyuk <vitaliymartiniuk16@gmail.com>
2021-06-29 17:13:26 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const firstLevelArray = sourceString.split(';').map(element => {
+    return element.trim();
+  });
+
+  const secondLevelArray = firstLevelArray.map(element => {
+    return element.split(':').map(item => {
+      return item.trim();
+    });
+  });
+
+  const filteredArray = secondLevelArray.filter(element => {
+    return element.every(x => {
+      return x.length !== 0;
+    });
+  });
+
+  const resultingObj = {};
+
+  filteredArray.forEach(element => {
+    const key = element[0];
+
+    const value = element[1];
+
+    resultingObj[key] = value;
+  });
+
+  return resultingObj;
 }
 
 module.exports = convertToObject;

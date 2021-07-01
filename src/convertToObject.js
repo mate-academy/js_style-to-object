9a8b065dddeac6c72574a7e0746a2f2f22987d09
VadymMakevytss <vadymlat@mail.ru>
2021-07-01 14:38:26 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const propertyDataObject = {};
+
+  const transformedArray = sourceString.split(';')
+    .map(element => element.trim())
+    .map(element => element.split(':')
+      .map(item => item.trim()));
+
+  const filteredArray = transformedArray.filter(element => {
+    return element.every(item => {
+      return item.length !== 0;
+    });
+  });
+
+  filteredArray.forEach(element => {
+    propertyDataObject[element[0]] = element[1];
+  });
+
+  return propertyDataObject;
 }
 
 module.exports = convertToObject;

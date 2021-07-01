07db9597627552a2d773b5fda67799b069e471b2
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-07-01 15:21:21 +0300

    solution with promts

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,35 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssRulesList = sourceString.split(';');
+
+  for (let i = 0; i < cssRulesList.length; i++) {
+    // якщо у рядку є ":" значить цей рядок містить css правило
+    if (cssRulesList[i].includes(':')) {
+      // розбиває css правило на [властивість][значення]
+      cssRulesList[i] = cssRulesList[i].split(':');
+    } else {
+      // видаляє елемент масиву, який не є css правилом
+      cssRulesList.splice(i, 1);
+      i--;
+      continue;
+    }
+
+    // trim() для [властивість] та [значення]
+    for (let q = 0; q < cssRulesList[i].length; q++) {
+      cssRulesList[i][q] = cssRulesList[i][q].trim();
+    }
+  }
+
+  // записує css правила в новому форматі
+  const styles = cssRulesList.reduce((prev, rule) => {
+    return {
+      ...prev,
+      [rule[0]]: rule[1],
+    };
+  }, {});
+
+  return styles;
 }
 
 module.exports = convertToObject;

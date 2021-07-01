cd6e83a3c6c78c1a111451fd6405bbeef5e0a413
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-01 14:54:59 +0300

    task is completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,25 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const unfilteredList = sourceString.split('\n');
+
+  unfilteredList.forEach((property, i, list) => {
+    list[i] = property.trim().replace(/;/g, '');
+  });
+
+  const filteredList = unfilteredList.filter(property => {
+    return property.length > 0;
+  });
+
+  const stylesList = filteredList.reduce((prev, property) => {
+    const splittedProperty = property.split(':');
+
+    prev[splittedProperty[0].trim()] = splittedProperty[1].trim();
+
+    return prev;
+  }, {});
+
+  return stylesList;
 }
 
 module.exports = convertToObject;

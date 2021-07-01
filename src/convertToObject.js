93b442d1d6f663d7da42bc6cae2a4b2a97fb993d
Andrii Milishchuk <andrewmilishchuk@gmail.com>
2021-07-01 15:21:47 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,24 +15,19 @@ function convertToObject(sourceString) {
   const cssRulesList = sourceString.split(';');
 
   for (let i = 0; i < cssRulesList.length; i++) {
-    // якщо у рядку є ":" значить цей рядок містить css правило
     if (cssRulesList[i].includes(':')) {
-      // розбиває css правило на [властивість][значення]
       cssRulesList[i] = cssRulesList[i].split(':');
     } else {
-      // видаляє елемент масиву, який не є css правилом
       cssRulesList.splice(i, 1);
       i--;
       continue;
     }
 
-    // trim() для [властивість] та [значення]
     for (let q = 0; q < cssRulesList[i].length; q++) {
       cssRulesList[i][q] = cssRulesList[i][q].trim();
     }
   }
 
-  // записує css правила в новому форматі
   const styles = cssRulesList.reduce((prev, rule) => {
     return {
       ...prev,

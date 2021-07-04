01c99d1c071224ebbcd01a11ec1cdf47b44def58
Dana Pryshelets <tanyarud53@gmail.com>
2021-07-04 08:49:51 +0300

    first commit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArr = sourceString.replace(/\n/g, '').split(';').map(element =>
+    element.trim()).filter(element =>
+    element.length > 0);
+
+  const stylesObj = {};
+
+  for (const style of stylesArr) {
+    const partOfStyle = style.split(':');
+
+    stylesObj[partOfStyle[0].trim()] = partOfStyle[1].trim();
+  }
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

9b54972a22e5ea099bc591f1bcc7a8c08ed4a312
Maksym Starodubtsev <maksym.starodubtsev@gmail.com>
2021-07-06 14:58:39 +0300

    victory

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,21 +14,25 @@
 function convertToObject(sourceString) {
   const arrayOfValues = sourceString.split(';');
 
-  arrayOfValues.forEach(element => element.split(':'));
+  const arrayPreResult = arrayOfValues.map(
+    element => {
+      return element.split(':');
+    });
 
-  const arrayPreResult = arrayOfValues.map(element => {
-    return element.trim().split(':');
-  });
+  return arrayPreResult.reduce(
+    (acumulator, element) => {
+      const result = { };
 
-  const result = {};
+      if (element[1] === undefined) {
+        return { ...acumulator };
+      }
+      result[`${element[0].trim()}`] = element[1].trim();
 
-  for (const char of arrayPreResult) {
-    if (char[1] !== undefined) {
-      result[`${char[0].trim()}`] = char[1].trim();
-    }
-  }
-
-  return result;
+      return {
+        ...acumulator,
+        ...result,
+      };
+    }, {});
 }
 
 module.exports = convertToObject;

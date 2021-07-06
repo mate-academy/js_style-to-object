57ab4477ab9cb845cdfdc3976d4ec432b4d087a2
Михаил <mihay0113@gmail.com>
2021-07-06 13:30:55 +0300

    redacted

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,16 +12,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const semicolon = sourceString.split(';');
-  const doublequote = semicolon.map(line => line.trim().split(':'));
-  const filtred = doublequote.filter(value => value.length !== 1);
+  const arrayOfAttributesWithValue = sourceString.split(';');
+  const trimmedArrayOfCouples
+    = arrayOfAttributesWithValue.map(line => line.trim().split(':'));
+  const filtredEmptyElements
+    = trimmedArrayOfCouples.filter(value => value.length !== 1);
 
-  filtred.forEach(couple => {
-    couple[0] = couple[0].trim();
-    couple[1] = couple[1].trim();
-  });
+  const styles = filtredEmptyElements.reduce((prev, attribute) => {
+    return {
+      ...prev,
+      [attribute[0].trim()]: attribute[1].trim(),
+    };
+  }, {});
 
-  return Object.fromEntries(filtred);
+  return styles;
 }
 
 module.exports = convertToObject;

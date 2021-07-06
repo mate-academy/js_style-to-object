2204ab630e562c5c1dcf287c516fed5af2eb9dec
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-07-06 08:49:51 +0300

    reduce added

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,18 +14,18 @@
 function convertToObject(sourceString) {
   const properties = sourceString
     .split(';')
-    .map(property => property.trim())
-    .filter(property => property.length > 0);
+    .map(property => property.split(':'))
+    .filter(property => property.length > 1);
 
-  const keys = properties.map(property => property.split(':')[0].trim());
-  const values = properties.map(property => property.split(':')[1].trim());
+  const addProperty = (objects, property) => {
+    return {
+      ...objects,
+      [property[0].trim()]: property[1].trim(),
+    };
+  };
 
-  const result = {};
+  const cssProperties = properties.reduce(addProperty, {});
 
-  for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
-  }
-
-  return result;
+  return cssProperties;
 }
 module.exports = convertToObject;

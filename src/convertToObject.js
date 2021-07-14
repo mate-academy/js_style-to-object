ea60719d8b74129896aea331e85c07f56c822337
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2021-07-14 14:38:05 +0300

    Task Done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,46 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const newString = sourceString.replace(/\n/g, '');
+
+  const array = newString.split(';');
+
+  const object = {};
+
+  array.forEach(element => {
+    const colon = element.indexOf(':');
+    let key = element.slice(0, colon);
+    const keyChar = key.split('');
+
+    while (keyChar[0] === ' ') {
+      keyChar.shift();
+    };
+
+    while (keyChar[keyChar.length - 1] === ' ') {
+      keyChar.pop();
+    };
+
+    key = keyChar.join('');
+
+    let value = element.slice(colon + 1);
+    const valueChar = value.split('');
+
+    while (valueChar[0] === ' ') {
+      valueChar.shift();
+    };
+
+    while (valueChar[valueChar.length - 1] === ' ') {
+      valueChar.pop();
+    };
+
+    value = valueChar.join('');
+
+    if (key.length !== 0 && value.length !== 0) {
+      object[key] = value;
+    }
+  });
+
+  return object;
 }
 
 module.exports = convertToObject;

83e9a9363a76d662d8acc8245ae9dfe114487045
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-06 17:20:49 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,17 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+    .split('\n')
+    .filter(string => string.trim().length > 2)
+    .map(item => item.split(':').map(x => x.trim()))
+    .reduce((obj, arr) => {
+      const y = {};
+
+      y[arr[0]] = arr[1].split(';')[0].trim();
+
+      return Object.assign(obj, y);
+    }, {});
 }
 
 module.exports = convertToObject;

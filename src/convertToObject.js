70b9b4e0af4d6ec12f5fbbe58e375f84d129818d
lera-yaskevych <redrum.back@gmail.com>
2021-08-11 21:46:50 +0300

    solved with reduce where callback is right inside the method

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArray = sourceString
+    .split(';')
+    .map(entries => entries.split(':')
+      .map(property => property.trim()));
+
+  const stylesObject = stylesArray
+    .filter(entry => entry.length > 1)
+    .reduce((prev, [key, value]) => Object.assign(prev, { [key]: value }), {},);
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

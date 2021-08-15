5fea2866fed55caf8af04a16e1a0882468d11121
unknown <hado.roman.ihorovych@gmail.com>
2021-08-15 22:51:56 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const separateStrings = sourceString.split(';')
+    .map(element => element.split(':'));
+  const stringFilter = separateStrings
+    .map(element => element.map(elementTrim => elementTrim.trim()))
+    .filter(element => element.length > 1)
+    .reduce((step, item) => {
+      return {
+        ...step,
+        [item[0]]: item[1],
+      };
+    }, {});
+
+  return stringFilter;
 }
 
 module.exports = convertToObject;

fb14623c96edad7ff5fe8d5a9fb059fe056e9715
Yevhenii Sviderskyi <evgen1sider@gmail.com>
2021-08-16 19:12:37 +0300

    remove needless cicle

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,35 +14,16 @@
 function convertToObject(sourceString) {
   const stylesObject = {};
 
-  const styleToArray = sourceString
-    .split(';\n')
-    .filter(name => name.length > 0);
-  const newArrayOfStyles = [];
+  const breakToListStyles = sourceString.split(';')
+    .map(style => style.split(':')
+      .map(prop => prop.trim())
+    ).filter(item => item.length > 1);
 
-  for (let i = 0; i < styleToArray.length; i++) {
-    if (styleToArray[i].length > 5) {
-      const items = styleToArray[i].split(': ');
+  for (const properties of breakToListStyles) {
+    const key = properties[0];
+    const values = properties[1];
 
-      newArrayOfStyles.push(items);
-    }
-  }
-
-  for (const items of newArrayOfStyles) {
-    const chank = items[0].split(' ')
-      .filter(name => name.length > 0);
-
-    if (chank.length > 1) {
-      items[0] = chank[1];
-    } else {
-      items[0] = chank[0];
-    }
-
-    const value = items[1].split(' ')
-      .filter(name => name.length > 0);
-
-    items[1] = value.join(' ');
-
-    stylesObject[items[0]] = items[1];
+    stylesObject[key] = values;
   }
 
   return stylesObject;

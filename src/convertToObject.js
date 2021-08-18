f690c6346494eeee199c84a354759d8332c61cf3
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2021-08-18 17:49:05 +0300

    final

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesFormated = sourceString.split(';')
+    .map(style => style.split(':')
+      .map(prop => prop.trim()))
+    .filter(style => style.length === 2);
+
+  const stylesObject = {};
+
+  for (const style of stylesFormated) {
+    stylesObject[style[0]] = style[1];
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

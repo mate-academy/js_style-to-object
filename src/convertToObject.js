abae0febca314892addfbb480c1948ea935ec9ce
Oleksij Lyashenko <oleksijlyashenko@MacBook-Air-Mary.local>
2021-09-17 18:42:54 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sum = {};
+
+  const result = sourceString.split(`\n`);
+
+  result.map((x) => {
+    if (x.length > 3) {
+      const index = x.indexOf(`:`);
+      const index2 = x.indexOf(`;`);
+      const key = x.slice(0, index).trim();
+      const val = x.slice(index + 1, index2).trim();
+
+      return (sum[key] = val);
+    }
+  });
+
+  return sum;
 }
 
 module.exports = convertToObject;

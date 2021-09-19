4d04710c2af3807410d32dea6b34996c0da980c7
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-09-19 11:44:20 +0300

    1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  let arr = sourceString.split(';');
+
+  arr = arr.map(string => string.trim());
+  arr = arr.map(string => string.split(':'));
+  arr = arr.map(string => string.map((value) => value.trim()));
+
+  const arr1 = arr.filter(string => string.length > 1);
+
+  for (const string of arr1) {
+    result[string[0]] = string[1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

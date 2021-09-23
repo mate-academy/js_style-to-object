99a33bfc2aad76ed3b1653f14389a41223b585a1
Vitalii Strohush <vitalikstrog2003v@gmail.com>
2021-09-23 21:38:23 +0300

    style-to-object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArray = sourceString.split(';\n');
+  const stylesObject = {};
+
+  let filteredArray = stylesArray.map(item => {
+    const splittedItem = item.split(':');
+    const set = new Set(splittedItem);
+
+    set.delete('\n');
+    set.delete('');
+
+    const res = [...set].map(value => value.trim());
+
+    return res.join(':');
+  });
+
+  const removeEmptyLines = new Set(filteredArray);
+
+  removeEmptyLines.delete('');
+  filteredArray = [...removeEmptyLines];
+
+  filteredArray.forEach(item => {
+    const splitted = item.split(':');
+
+    stylesObject[splitted[0]] = splitted[1];
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

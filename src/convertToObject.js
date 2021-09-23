5f4436200770b1b6c6ddc203b9e65ee802aa22c4
Dina Samoilova <s13.dina@gmail.com>
2021-09-23 21:45:53 +0300

    Solution after review

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,25 +12,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const styleObj = {};
-  const arr1 = sourceString.split(';\n');
-  const arr2 = arr1.filter(el => el.includes(':'));
-  const arr3 = arr2.map(el => el.trim('\n') || el.trim('\n\n')
-    || el.trim('      ') || el.trim('     ')
-    || el.trim('    ') || el.trim('   ') || el.trim('  ')
-    || el.trim(' '));
+  // const styleObj = {};
+  // const arr1 = sourceString.split(';\n');
+  // const arr2 = arr1.filter(el => el.includes(':'));
+  // const arr3 = arr2.map(el => el.trim('\n') || el.trim('\n\n')
+  //   || el.trim('      ') || el.trim('     ')
+  //   || el.trim('    ') || el.trim('   ') || el.trim('  ')
+  //   || el.trim(' '));
 
-  for (let i = 0; i < arr3.length; i++) {
-    arr3[i] = arr3[i].split(':');
-  }
+  // for (let i = 0; i < arr3.length; i++) {
+  //   arr3[i] = arr3[i].split(':');
+  // }
 
-  for (const item of arr3) {
-    if (!styleObj[item[0].trim(' ')]) {
-      styleObj[item[0].trim(' ')] = item[1].trim(' ');
-    }
-  }
+  // for (const item of arr3) {
+  //   if (!styleObj[item[0].trim(' ')]) {
+  //     styleObj[item[0].trim(' ')] = item[1].trim(' ');
+  //   }
+  // }
 
-  return styleObj;
+  // return styleObj;
+
+  const values = sourceString
+    .split(';\n')
+    .map(phrase => phrase.split(':').map(el => el.trim()))
+    .filter(pair => pair.length === 2);
+
+  return Object.fromEntries(values);
 }
 
 module.exports = convertToObject;

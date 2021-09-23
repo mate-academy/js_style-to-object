a3610782a8079dde968d3634d3cca0b896c6b088
VladSaen <qwefghjklgfd@gmail.com>
2021-09-23 12:52:28 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sort = sourceString.replace(/(\r\n|\n|\r)/gm, '').split(';')
+    .filter(x => x !== '  ')
+    .filter(x => x !== '');
+
+  const result = {};
+
+  for (let i = 0; i < sort.length; i++) {
+    result[sort[i].split(':')[0].trim()] = sort[i].split(':')[1].trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

54920cfbc0e4675e5a7df5c2dc0e89647a5fa573
Dima Cherednyk <dicherednyk@gmail.com>
2021-10-14 13:31:09 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  let rows = '';
+
+  for (const letter of sourceString) {
+    if (letter !== '\n') {
+      rows = rows + letter;
+    };
+  };
+
+  const splitRows = rows.split(';');
+
+  const filterRows = splitRows.filter(x => x.includes(':'));
+
+  const splitRows2 = filterRows.map(x => x.split(':'));
+
+  const trimRows = splitRows2.map(x => x[0].trim() + ':' + x[1].trim());
+
+  const splitRows3 = trimRows.map(x => x.split(':'));
+
+  const callback = (prev, x) => ({
+    ...prev,
+    [x[0]]: x[1],
+  });
+
+  const result = splitRows3.reduce(callback, {});
+
+  return result;
+};
 
 module.exports = convertToObject;

a02639153894810caf20bcefb359bbfd1dd10b3d
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-10-18 11:46:31 +0300

    delete comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,23 +1,5 @@
 'use strict';
 
-// function convertToObject(sourceString) {
-//   const arrStyles = sourceString
-//     .split(';')
-//     .map(style => style.trim())
-//     .filter(style => style.length > 0);
-
-//   const result = arrStyles.reduce((prev, current) => {
-//     const item = current.split(':').map((string) => string.trim());
-
-//     return {
-//       ...prev,
-//       [item[0]]: item[1],
-//     };
-//   }, {});
-
-//   return result;
-// }
-
 function convertToObject(sourceString) {
   return sourceString
     .split(';')

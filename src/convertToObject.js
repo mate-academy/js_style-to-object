a4e1f32b3283f44a68590f57acb62c41ad25e4c7
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2021-10-18 11:45:47 +0300

    immediately return

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,21 +1,36 @@
 'use strict';
 
+// function convertToObject(sourceString) {
+//   const arrStyles = sourceString
+//     .split(';')
+//     .map(style => style.trim())
+//     .filter(style => style.length > 0);
+
+//   const result = arrStyles.reduce((prev, current) => {
+//     const item = current.split(':').map((string) => string.trim());
+
+//     return {
+//       ...prev,
+//       [item[0]]: item[1],
+//     };
+//   }, {});
+
+//   return result;
+// }
+
 function convertToObject(sourceString) {
-  const arrStyles = sourceString
+  return sourceString
     .split(';')
     .map(style => style.trim())
-    .filter(style => style.length > 0);
+    .filter(style => style.length > 0)
+    .reduce((prev, current) => {
+      const item = current.split(':').map((string) => string.trim());
 
-  const result = arrStyles.reduce((prev, current) => {
-    const item = current.split(':').map((string) => string.trim());
-
-    return {
-      ...prev,
-      [item[0]]: item[1],
-    };
-  }, {});
-
-  return result;
+      return {
+        ...prev,
+        [item[0]]: item[1],
+      };
+    }, {});
 }
 
 module.exports = convertToObject;

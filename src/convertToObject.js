6537866867ce87022190ab4ee3cbdc36aa9597d9
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-19 11:01:46 +0300

    fixed again

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,9 +14,14 @@
 function convertToObject(sourceString) {
   const resultObject = {};
 
-  sourceString.split(';').map(line => line.trim()).filter(x => x.length > 0)
-    .map(property => property.split(':').map(x => x.trim()))
-    .map(element => {resultObject[element[0]] = element[1]});
+  sourceString.split(';')
+    .map(line => line.trim())
+    .filter(x => x.length > 0)
+    .map(property => property.split(':')
+    .map(x => x.trim()))
+    .map(element => {
+      resultObject[element[0]] = element[1];
+    });
 
   return resultObject;
 }

ce9ef8ce0c2c1a4c790ab8dec3c1081d8d649f3f
unknown <skirik0117@gmail.com>
2021-11-15 18:08:17 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arr = sourceString.split(';').filter((el) =>
+    el !== '' || el !== ' ');
+  const result = {};
+  const newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].trim().toLowerCase() !== arr[i].trim().toUpperCase()) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  const trim = newArr.map((index) =>
+    index.trim()).join(':').split(':').map((x) =>
+    x.trim());
+
+  for (let i = 0; i < trim.length; i += 2) {
+    result[trim[i]] = trim[i + 1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

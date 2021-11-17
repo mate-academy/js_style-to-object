f8cb4f0f56ac0eb44ebad87638f78dfe46c64403
WorkAccCom <work.acc.com@gmail.com>
2021-11-17 23:39:56 +0200

    First solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,22 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const properties = sourceString.split(';');
+
+  for (let i = 0; i < properties.length; i++) {
+    const property = properties[i].split(':');
+    const trimed = property.map(item => item.trim());
+
+    if (trimed[0] !== '') {
+      result[trimed[0]] = trimed[1];
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

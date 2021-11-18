c9513f86af8610929895d2352967232d7cea9896
Vladyslav Tuzenko <karten.vlad@gmail.com>
2021-11-18 16:38:17 +0200

    Fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,13 +15,12 @@ function convertToObject(sourceString) {
   const arrayOfStyles = sourceString
     .split(';')
     .map(element => element.split(':'))
-    .filter(element => element.length > 1)
-    .map(element => [element[0].trim(), element[1].trim()]);
+    .filter(element => element.length > 1);
 
   const objectOfStyles = arrayOfStyles.reduce((acc, [key, value]) => {
     return {
       ...acc,
-      [key]: value,
+      [key.trim()]: value.trim(),
     };
   }, {});
 

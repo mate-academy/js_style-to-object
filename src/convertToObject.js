289fb5319b0a5cf06e513b1e328e2493a18a3a90
Katya Suvorova <katerinasuvorova97@gmail.com>
2021-11-25 14:08:31 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,42 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const masivStrings = sourceString.split('\n');
+
+  const onlyWithProperties = masivStrings.filter((string) => string.length > 3);
+
+  const callback = (string) => {
+    const result = string.split(':');
+
+    return result;
+  };
+
+  const keyWithProrertiesInMasiv = onlyWithProperties.map(callback);
+
+  const callb = (prev, masiv) => {
+    const newM = [];
+
+    for (const key in masiv) {
+      const str = masiv[key];
+      const stringWithoutSpaces = str.trim();
+
+      newM.push(stringWithoutSpaces);
+    }
+
+    const first = newM[0];
+    const second = newM[1];
+    const x = second.slice(0, -1);
+    const y = x.trim();
+
+    return {
+      ...prev,
+      [first]: y,
+    };
+  };
+
+  const newObj = keyWithProrertiesInMasiv.reduce(callb, {});
+
+  return newObj;
 }
 
 module.exports = convertToObject;

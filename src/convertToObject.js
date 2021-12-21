0c667e8c6d1c3ed6c7960dab7b9d80f8c782d7fd
Niki <tes.kolia@gmail.com>
2021-12-21 17:24:52 +0200

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,21 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const arr = sourceString.split(';');
+  const cssStyles = {};
+
+  arr.forEach(item => {
+    const tempValue = item.split(':');
+
+    if (tempValue[0] !== undefined && tempValue !== undefined) {
+      cssStyles[tempValue[0].trim()] = tempValue[1].trim();
+    }
+  });
+
+  return cssStyles;
 }
 
 module.exports = convertToObject;

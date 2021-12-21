34b5168eb4199d30a42f343537474954d295b321
Alona Snigur <alonaatamanenkoo>
2021-12-21 22:21:56 +0200

    object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssObj = {};
+
+  sourceString.split(';')
+    .forEach(sign => {
+      const newSource = sign.split(':');
+
+      if (newSource.length > 1) {
+        cssObj[newSource[0].trim()] = newSource[1].trim();
+      }
+    });
+
+  return cssObj;
 }
 
 module.exports = convertToObject;

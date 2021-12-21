35d4b2d504e7bc4d0c1aa275717e900ea7a18fb1
levix2l <alexleviz13@gmail.com>
2021-12-21 15:43:55 +0200

    string to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = [];
+  const obj = {};
+
+  for (const item of sourceString.split(';')) {
+    const split = item.split(':');
+    const temp = [];
+
+    for (const value of split) {
+      temp.push(value.trim());
+    }
+
+    result.push(temp);
+  }
+
+  for (const item of result) {
+    if (item.length > 1) {
+      obj[item[0]] = item[1];
+    }
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

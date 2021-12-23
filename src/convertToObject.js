ef81ce8a725f9deac5bb7db9d7b394934324b7b9
SavelovSerhii <savelov12111@gmail.com>
2021-12-23 14:40:17 +0200

    First version

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,47 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let words = sourceString.split('');
+  const cssCode = {};
+
+  words.forEach((el, index) => {
+    if ('\n'.includes(el)) {
+      delete words[index];
+    }
+  });
+
+  words = words.join('')
+    .split(';');
+
+  words.forEach((word, index) => {
+    const wordTrim = word.trim();
+
+    words[index] = wordTrim;
+
+    if (wordTrim === '') {
+      words.splice(index, 1);
+    }
+  });
+
+  words.forEach((word, index) => {
+    const wordTrim = word.trim();
+
+    words[index] = wordTrim;
+
+    if (wordTrim === '') {
+      words.splice(index, 1);
+    }
+  });
+
+  words.forEach((word, index) => {
+    words.splice(index, 1, word.split(':'));
+  });
+
+  words.forEach((word) => {
+    cssCode[word[0].trim()] = word[1].trim();
+  });
+
+  return cssCode;
 }
 
 module.exports = convertToObject;

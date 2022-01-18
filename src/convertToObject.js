126815159ad1fd4c10ead1d63d9a43eb2687929f
Roma Afanasiev <romafanasiev123@gmail.com>
2022-01-18 19:55:08 +0200

    add function convertToObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,27 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+
+  const arr = sourceString.trim().slice(0, -1).split(';');
+  const arr1 = [];
+  const result = {};
+
+  arr.forEach((str, index) => {
+    if (str.length > 1) {
+      arr1[index] = str.replace(/\s+/g, ' ').replace(' :', ':').trim();
+    }
+  });
+
+  arr1.forEach((element) => {
+    if (element.length > 1) {
+      const key = element.split(': ')[0];
+      const value = element.split(': ')[1];
+
+      result[key] = value;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

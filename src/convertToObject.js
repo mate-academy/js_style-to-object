e56932b58b0e45c38679319f8c8471730029039c
Roma Afanasiev <romafanasiev123@gmail.com>
2022-01-19 19:39:00 +0200

    change function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,27 +13,6 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-
-  // const arr = sourceString.trim().slice(0, -1).split(';');
-  // const arr1 = [];
-  // const result = {};
-
-  // arr.forEach((str, index) => {
-  //   if (str.length > 1) {
-  //     arr1[index] = str.replace(/\s+/g, ' ').replace(' :', ':').trim();
-  //   }
-  // });
-
-  // arr1.forEach((element) => {
-  //   if (element.length > 1) {
-  //     const key = element.split(': ')[0];
-  //     const value = element.split(': ')[1];
-
-  //     result[key] = value;
-  //   }
-  // });
-
-  // return result;
   const elements = sourceString.split(';');
   const values = elements.filter((el) => el.length > 4);
   const clearValues = values.map((element) => {

c02e14713ab4e5ef16d08e8cb5bde3b75359999f
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-24 14:15:30 +0200

    regex for life, compling wasn't so useless after all

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+  const properties = [...sourceString.matchAll(/[-a-z]+? *?:.+?;/g)];
+
+  properties.forEach(function(property) {
+    const propertyPair = property[0].replace(/  +|;/g, '').split(/: */g);
+
+    styles[propertyPair[0]] = propertyPair[1];
+  });
+
+  return styles;
 }
 
 module.exports = convertToObject;

415e13bdf297f59ae63f4d05b392cefa4802605a
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-26 13:25:54 +0200

    )))

--- src/dlia-dushi.js
+++ src/dlia-dushi.js
@@ -12,23 +12,17 @@
  * @return {object}
  */
 
-function convertToObject(sourceString) {
+function flexRegex(sourceString) {
   const styles = {};
-  const properties = sourceString
-    .split(';')
-    .map(property => property.trim());
+  const properties = [...sourceString.matchAll(/[-a-z]+? *?:.+?;/g)];
 
   properties.forEach(function(property) {
-    if (property) {
-      const propertyPair = property
-        .split(':')
-        .map(p => p.trim());
+    const propertyPair = property[0].replace(/  +|;/g, '').split(/: */g);
 
-      styles[propertyPair[0]] = propertyPair[1];
-    }
+    styles[propertyPair[0]] = propertyPair[1];
   });
 
   return styles;
 }
 
-module.exports = convertToObject;
+module.exports = flexRegex;

ff74a559011f1a2df8fc1e1cd2b3b645cb536054
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-01-30 18:05:11 +0200

    sol

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,57 @@
  *
  * @return {object}
  */
+
+const stylesString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
+
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesData = {};
+
+  sourceString.split(';').forEach(function(item) {
+    stylesData[item.substr(0, item.indexOf(':')).trim()]
+      = item.substr(item.indexOf(':') + 1).trim();
+  }
+  );
+
+  delete stylesData[''];
+
+  return stylesData;
 }
+
+console.log(convertToObject(stylesString));
 
 module.exports = convertToObject;

61e30a1926469028a86d7094c3524f2e55308f07
Misha Grinko <glukym@gmail.com>
2022-01-31 08:26:02 +0200

    Update convertToObject.js

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,11 +3,19 @@
 /**
  * Implement convertToObject function:
  *
+<<<<<<< HEAD
  * Function takes a string with styles
  * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
  * and values are the values of related CSS properties
  * (see an exampl in [test file](./convertToObject.test.js))
+=======
+ * Function takes a string with styles
+(see an example in [stylesString.js](./stylesString.js))
+ * and returns an object where CSS properties are keys
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
+>>>>>>> be580c2 (Update convertToObject.js)
  *
  * @param {string} sourceString
  *

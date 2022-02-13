cb1151787a0034abd36fc9093d6edf57be8f0708
Misha Molnar <michaellmuller423@fmail.com>
2022-02-13 14:08:34 +0200

    solved with reduce

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,20 @@
 'use strict';
 
-/**
- * Implement convertToObject function:
- *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
- *
- * @param {string} sourceString
- *
- * @return {object}
- */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+    .split(';')
+    .map((row) => row.trim())
+    .reduce((acc, el) => {
+      if (!el) {
+        return acc;
+      }
+
+      return {
+        ...acc,
+        [el.split(':')[0].trim()]: el.split(':')[1].trim(),
+      };
+    }, {});
 }
 
 module.exports = convertToObject;

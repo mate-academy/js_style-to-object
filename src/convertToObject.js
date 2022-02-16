fc5dd3efec3e38d8aef6a94891c7da5defeda77e
Olga <arnab@ukr.net>
2022-02-16 13:35:38 +0200

    Fixed solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  const objectFromString = sourceString.split(';')
+  const transformedString = sourceString.split(';')
     .map(pair => {
       return pair.split(':').map(item => item.trim());
     })
     .filter(item => item.length === 2)
-    .reduce((object, currentItem) => {
-      object[currentItem[0]] = currentItem[1];
+    .reduce((object, [key, value]) => {
+      object[key] = value;
 
       return object;
     }, {});
 
-  return objectFromString;
+  return transformedString;
 };
 
 module.exports = convertToObject;

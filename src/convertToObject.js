ee37c278d50be96a9a9a49ef91966f38f98c3b4a
Roman Khemych <roman19940912@gmail.com>
2022-02-17 15:10:13 +0200

    Change For loop to Reduce

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,17 +1,17 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  const cssStyles = {};
-  const arrOfStyles = sourceString
+  const styles = sourceString
     .split(';')
     .map(elem => elem.split(':'))
     .filter(elem => elem.length > 1);
 
-  for (const style of arrOfStyles) {
-    cssStyles[style[0].trim()] = style[1].trim();
-  }
-
-  return cssStyles;
+  return styles.reduce((previus, [key, value]) => {
+    return {
+      ...previus,
+      [key.trim()]: value.trim(),
+    };
+  }, {});
 }
 
 module.exports = convertToObject;

a3c6535fc0cc4eb93f2e9b2e5e9359b87bc118c0
Batick3737 <antonandriy@ukr.net>
2022-03-05 17:08:27 +0200

    write code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,31 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of
+ *  related CSS properties (see an exampl in
+ * [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const masive = sourceString.split(':');
+  const masiveNoSpace = masive.map(x => x.trim());
+  const masiveJoin = masiveNoSpace.join(':');
+  const masiveResult = masiveJoin.split(';');
+  const result = masiveResult.filter(masiv => masiv.trim() !== '');
+  const callback = (prev, x) => {
+    return {
+      ...prev,
+      [x[0]]: x[1],
+    };
+  };
+
+  return result.map(masiv => masiv.trim().split(':')).reduce(callback, {});
 }
 
 module.exports = convertToObject;

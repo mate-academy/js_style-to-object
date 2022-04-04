9e5364e664a1aa440b5a4707a0ea74bfe4555dd3
Denys Lipin <lipin.transstream@gmail.com>
2022-04-04 12:49:46 +0300

    'Solution2'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,12 +14,11 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const list = sourceString.split(';').map(
-    style => style.trim()).filter(
-    style => style.length > 0).map(
-    style => style.split(':')
-  );
-  const object = list.reduce((prev, style) => ({
+  const array = sourceString.split(';');
+  const trimArray = array.map(style => style.trim());
+  const filterArray = trimArray.filter(style => style.length > 0);
+  const splitArray = filterArray.map(style => style.split(':'));
+  const object = splitArray.reduce((prev, style) => ({
     ...prev,
     [style[0].trim()]: style[1].trim(),
   }), {});

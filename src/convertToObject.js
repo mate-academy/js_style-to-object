8412147ccd336a2ae9760fb1026a29319c3d22dc
unknown <avicostov2@gmail.com>
2022-04-20 22:49:26 +0200

    solut2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,26 +8,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const newArr = sourceString.replace(/\r?\n/g, '').split('');
 
-  newArr.unshift(';');
+  const begin = sourceString.replace(/\r?\n/g, '');
 
-  const newObject = {};
+  const lines = begin.split(';').map((line) => line.split(':'));
 
-  const result = newArr.reduce((arr, sym, index) => sym === ';'
-    ? [...arr, index] : sym === ':' ? [...arr, index] : arr, []);
-
-  for (let i = 0; i < result.length - 2; i = i + 2) {
-    const key = newArr.slice(result[i] + 1, result[i + 1]);
-    const value = newArr.slice(result[i + 1] + 1, result[i + 2]);
-
-    if (!key.join('').trim().length) {
-      continue;
+  const resultObj = lines.reduce(function(obj, [ key, value ]) {
+    if (key && value) {
+      obj[key.trim()] = value.trim();
+    } else {
+      return obj;
     }
 
-    newObject[key.join('').trim()] = value.join('').trim();
-  }
+    return obj;
+  }, {});
 
-  return newObject;
+  return resultObj;
 }
 module.exports = convertToObject;

d81c33b03ed252b9f878b9221e40b28315292057
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-05-11 21:59:38 +0200

    styles are fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,28 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles (see an example
+ * in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const transform = sourceString
+    .split(';')
+    .map(line => line.split(':'))
+    .filter(item => item.length > 1);
+
+  for (const [a, b] of transform) {
+    obj[a.trim()] = b.trim();
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

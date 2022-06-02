68609d9ef41869252d9827ad4dbedf10268e03e1
Pavlo Pokryvets <pavelpokryvets@gmail.com>
2022-06-02 12:51:50 +0300

    solution 2.0

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,18 +14,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const str = sourceString.split(/\n/g);
+  const str = sourceString.split(';');
   let resultObj = {};
 
   for (const element of str) {
-    if (element.includes(':') && element.includes(';')) {
+    if (element.includes(':')) {
       const objElement = {
         [(element.slice(0, element.indexOf(':'))).trim()]:
-        (element.slice(element.indexOf(':') + 1, element.indexOf(';'))).trim(),
+        (element.slice(element.indexOf(':') + 1)).trim(),
 
       };
 
-      resultObj = Object.assign({}, resultObj, objElement);
+      resultObj = Object.assign(resultObj, objElement);
     }
   }
 

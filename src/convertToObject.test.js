9c7f43bc8b69ebc9f96612e446debccdc985489d
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-06-18 14:28:43 +0300

    Solution

--- src/convertToObject.test.js
+++ src/convertToObject.test.js
@@ -37,7 +37,7 @@ test('Convert string to object', () => {
     .toEqual(EXPECTED_OBJECT);
 });
 
-test('Object should have same count of keys as styles in css', () => {
+test.only('Object should have same count of keys as styles in css', () => {
   expect(Object.keys(convertToObject(stylesString)).length)
     .toBe(Object.keys(EXPECTED_OBJECT).length);
 });

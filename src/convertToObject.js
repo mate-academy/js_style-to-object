2a9d07f0356c1f741b149378b16d04c93b0e6dfd
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-06-18 06:11:26 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,32 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  function getFormatDeclaration(prev, prop) {
+    return ({
+      ...prev,
+      [getTrim(prop[0])]: getTrim(prop[1]),
+    });
+  }
+
+  function getTrim(property) {
+    return property.trim();
+  }
+
+  return sourceString.split(';')
+    .map(partString => partString.split(':'))
+    .filter(notEmptyDeclaration => notEmptyDeclaration.length > 1)
+    .reduce(getFormatDeclaration, {});
 }
 
 module.exports = convertToObject;

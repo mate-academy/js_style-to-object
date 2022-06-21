3742710ca02ba0f0b7d80025727220a21c143f2e
Roman Marunkevich <vinnibea@gmail.com>
2022-06-21 17:36:37 +0300

    solution, replaced .map() with .forEach, splitted at char(:), created copy of basic array

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,20 +3,19 @@
 function convertToObject(sourceString) {
   const formatedStyles = {};
 
-  let propertyArray = sourceString.replace(/[\n]/gi, '').split(';');
+  const propertyArray = sourceString.replace(/[\n]/gi, '').split(';');
 
-  propertyArray = propertyArray.filter(element => element.length > 2);
+  const filteredProperties = propertyArray
+    .filter(element => element.length > 2);
 
-  propertyArray.map((element) => {
-    const key = element.slice(0, element.indexOf(':')).trim();
-    const value = element
-      .slice(element.indexOf(':') + 1, element.length)
-      .trim();
+  filteredProperties.forEach((element) => {
+    const keysAndValues = element.split(':');
+    const key = keysAndValues[0].trim();
+    const value = keysAndValues[1].trim();
 
     formatedStyles[key] = value;
   });
 
   return formatedStyles;
 }
-
 module.exports = convertToObject;

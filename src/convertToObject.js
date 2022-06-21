30f40ff5332f515879c5607fffcd7ad908dab806
Dmytro Mostovyi <7kozita@gmail.com>
2022-06-21 19:18:39 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,17 +1,17 @@
 'use strict';
 
-// *
-//  * Implement convertToObject function:
-//  *
-//  * Function takes a string with styles (see an example in [stylesString.js]
-//  * (./stylesString.js))
-//  * and returns an object where CSS properties are keys
-//  * and values are the values of related CSS properties (see an exampl
-//  * in [test file](./convertToObject.test.js))
-//  *
-//  * @param {string} sourceString
-//  *
-//  * @return {object}
+/* *
+ * Implement convertToObject function:
+ *
+ * Function takes a string with styles (see an example in [stylesString.js]
+ * (./stylesString.js))
+ * and returns an object where CSS properties are keys
+ * and values are the values of related CSS properties (see an exampl
+ * in [test file](./convertToObject.test.js))
+ *
+ * @param {string} sourceString
+ *
+ * @return {object} */
 
 function convertToObject(sourceString) {
   const arrStyle = sourceString.split(';');

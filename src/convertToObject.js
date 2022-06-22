bf0dc03d590731e076ecf26083cfc8df4980edec
Dima Belotskyi <dimonbelotskiy@gmail.com>
2022-06-22 15:23:08 +0300

    Fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,15 +7,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const arrayOfStyles = sourceString.split(';')
+  const arrayOfStyles = sourceString
+    .split(';')
     .map(str => str.trim().split(':'))
     .filter(arr => arr.length > 1);
 
   const objectOfStyles = {};
 
-  for (const style of arrayOfStyles) {
-    objectOfStyles[style[0].trim()] = style[1].trim();
-  }
+  arrayOfStyles
+    .forEach(style => (objectOfStyles[style[0].trim()] = style[1].trim())
+    );
+
+  // for (const style of arrayOfStyles) {
+  //   objectOfStyles[style[0].trim()] = style[1].trim();
+  // }
 
   return objectOfStyles;
 }

1e5d6dd2ee9e6fa0c7cc622e7447cbc0c0ce6c2d
Aleksandr Kovalenko <dias.koval@gmail.com>
2022-07-07 08:50:55 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,15 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = sourceString.split(';').map(key =>
-    key.split(':').map(key1 => key1.trim())).reduce((key, value) => {
-    key[value[0]] = value[1];
-    delete key[value];
+  return sourceString
+    .split(';')
+    .map(key => key
+      .split(':')
+      .map(key1 => key1.trim()))
+    .reduce((key, value) => {
+      key[value[0]] = value[1];
+      delete key[value];
 
-    return key;
-  }, {});
-
-  return result;
+      return key;
+    }, {});
 }
 
 module.exports = convertToObject;

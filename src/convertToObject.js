8f62c336c5de69d50f461f1e1f80f5b14d7d9f84
Yevhen Nikolaienko <illnino380@gmail.com>
2022-07-14 16:28:40 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,34 @@
 'use strict';
 
+const stylesString = require('./stylesString');
+
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+
+  const rules = stylesString.split(';\n');
+  const separateRules = rules.map(rule => rule.split(':'));
+  const rulesWithoutBlanklines = separateRules.filter(rule => (
+    rule.length === 2
+  ));
+
+  rulesWithoutBlanklines.forEach(([rule, value]) => (
+    styles[rule.trim()] = value.trim()
+  ));
+
+  return styles;
 }
 
 module.exports = convertToObject;

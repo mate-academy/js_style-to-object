a1bbb16928d5e74f5055cbe2204cdf80b8d71795
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-19 00:30:51 +0300

    function to create Object of style string was created

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,23 @@
 'use strict';
 
-/**
- * Implement convertToObject function:
- *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
- *
- * @param {string} sourceString
- *
- * @return {object}
- */
 function convertToObject(sourceString) {
-  // write your code here
+  const sortedSourceArray = sourceString
+    .replace(/\s+/g, ' ')
+    .trim()
+    .split(';')
+    .map(splitProperty => splitProperty.split(':'))
+    .filter(cleanUp => cleanUp.length > 1);
+
+  const createObject = (prev, [ property, value ]) => {
+    return {
+      ...prev,
+      [property.trim()]: value.trim(),
+    };
+  };
+
+  const sourceObject = sortedSourceArray.reduce(createObject, {});
+
+  return sourceObject;
 }
 
 module.exports = convertToObject;

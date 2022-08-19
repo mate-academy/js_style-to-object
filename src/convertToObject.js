8a1a034c50d399d10b8ac5fe7a2fb6e51520c6c7
Andrew Zaitsev <alzay007@gmail.com>
2022-08-19 11:14:09 +0300

    renamed variables, according to comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,9 +10,10 @@
 function convertToObject(sourceString) {
   const styleCss = sourceString
     .split(';')
-    .filter(el => el.trim())
-    .map(el => el.split(':'))
-    .map(el => el.map(word => word.trim()));
+    .filter(currentStyleElement => currentStyleElement.trim())
+    .map(currentStyleElement => currentStyleElement.split(':'))
+    .map(currentStyleElement =>
+      currentStyleElement.map(currentStyKey => currentStyKey.trim()));
 
   const objStyleCss = Object.fromEntries(styleCss);
 

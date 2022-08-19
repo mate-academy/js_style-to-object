82f999e481ef3d329b5190b94e320e581fb7bf62
Andrew Zaitsev <alzay007@gmail.com>
2022-08-19 08:58:38 +0300

    change method map to filter , according comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,8 +10,7 @@
 function convertToObject(sourceString) {
   const styleCss = sourceString
     .split(';')
-    .map(el => el.trim())
-    .filter(el => el.length > 0)
+    .filter(el => el.trim())
     .map(el => el.split(':'))
     .map(el => el.map(word => word.trim()));
 

ba8735a3dfe7e9e6991125b734747b44860d5689
Ostap Blystsiv <ostapcr7@gmail.com>
2022-08-23 17:07:47 +0300

    fixed code style

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,10 +3,12 @@
 function convertToObject(sourceString) {
   const result = {};
 
-  const resultArray = sourceString.split(';').map(el => el.trim())
-    .filter(el => el.length > 0)
-    .map(el => el.split(':'))
-    .map(el => el.map(word => word.trim()));
+  const resultArray = sourceString
+    .split(';')
+    .map(row => row.trim())
+    .filter(item => item.length > 0)
+    .map(style => style.split(':'))
+    .map(style => style.map(item => item.trim()));
 
   resultArray.forEach(([key, value]) => {
     result[key] = value;

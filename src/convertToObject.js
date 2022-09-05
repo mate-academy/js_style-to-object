92f2a0cb8cec9c7ede2a5c8d4d7f4cd72f2a3bd7
Maria Bramnik <mariabramnik@gmail.com>
2022-09-05 21:29:31 +0300

    Fixes after code review

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,9 +12,7 @@ function convertToObject(sourceString) {
   // write your code here
 
   const resObj = sourceString.replace(/\s+/g, ' ')
-    .split(';').filter(function(entry) {
-      return /\S/.test(entry);
-    }).reduce((obj, item) => {
+    .split(';').filter(styleStr => /\S/.test(styleStr)).reduce((obj, item) => {
       const [key, value] = item.split(':').map(element => element.trim());
 
       obj[key] = value;

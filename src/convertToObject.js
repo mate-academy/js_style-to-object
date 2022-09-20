4a8f889beb6b9711e0ac62c19a24d00dbd50059c
Datsenko Mariia <datsenko.mate@gmail.com>
2022-09-20 18:52:28 +0300

    sting has been converted to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,34 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssObject = {};
+
+  const modifiedString = sourceString
+    .replace(/[\n]*/g, '')
+    .split(';')
+    .map(el => el.trim())
+    .filter(el => el !== '');
+
+  for (let i = 0; i < modifiedString.length; i++) {
+    const singleStyle = modifiedString[i].split(':');
+    const key = singleStyle[0].trim();
+    const value = singleStyle[1].trim();
+
+    cssObject[key] = value;
+  }
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

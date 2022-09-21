22862b25a9d7489a383e7e12c76bd6e27acd57db
Andriy Hladkevych <andriyglad@gmail.com>
2022-09-21 15:14:10 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styledObj = sourceString.split(';').map(prop => prop.slice(2).trim())
+    .map(prop => {
+      return prop.split(':').map(elem => elem.trim());
+    })
+    .filter(prop => prop.length === 2)
+    .map(prop => {
+      if (prop[0].startsWith('webkit')) {
+        prop[0] = '-' + prop[0];
+      }
+
+      return prop;
+    })
+    .reduce((prev, prop) => {
+      prev[prop[0]] = prop[1];
+
+      return prev;
+    }, {});
+
+  return styledObj;
 }
 
 module.exports = convertToObject;

c53688b30c071f4ac280647f706e8c30c15561e1
Andrew Satyr <and.satur@gmail.com>
2022-09-26 21:30:12 +0300

    Fixed point 2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -2,17 +2,17 @@
 
 function convertToObject(sourceString) {
   let strings = sourceString.split(';');
-  const obj = {};
+  const resultObj = {};
 
   strings.map(str => {
     strings = str.trim().split(':');
 
     if (strings[0] && strings[1]) {
-      obj[strings[0].trim()] = strings[1].trim();
+      resultObj[strings[0].trim()] = strings[1].trim();
     }
   });
 
-  return obj;
+  return resultObj;
 }
 
 module.exports = convertToObject;

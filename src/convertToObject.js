4e2fe96987732cd35f8d438d40bc1e8ae974a0ab
zbyn3k <coding707@mailbox.org>
2022-11-16 19:42:28 +0100

    Implemented the solution for convertToObject function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,33 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an example in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayOfLines = sourceString
+    .split(';')
+    .map((str) => str.trim())
+    .filter((str) => str.length > 0);
+  const newObject = {};
+
+  arrayOfLines.forEach((line) => {
+    const key = line.split(':')[0].trim();
+    const value = line.split(':')[1].trim();
+
+    if (key && value) {
+      newObject[key] = value;
+    }
+  });
+
+  return newObject;
 }
 
 module.exports = convertToObject;

7a4a5a84542fafc6ceb484e20f038fba37e7d348
Artem Hrechuk <grechuck.artem.ag@gmail.com>
2022-12-01 16:30:53 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,39 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesString = sourceString.split(';')
+    .filter(value => value.includes(':'))
+    .map(property => property.trim());
+
+  const callback = (allItems, item) => {
+    return {
+      ...allItems,
+      [item.slice(0, item.indexOf(':')).trim()]:
+        item.slice(item.indexOf(':') + 1).trim(),
+    };
+  };
+
+  const stylesObject = stylesString.reduce(callback, {});
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
+// const iteration = splitedString.forEach(string => {
+//   const filteredString = string.trim().split(' ').filter(letter => (
+//     letter !== ' '
+//   )).join(' ');
+
+//   const firstPart = filteredString.slice(0, filteredString.indexOf(':') + 1);
+//   const secondPart
+//     = filteredString.slice(filteredString.indexOf(':') + 1).trim();
+
+//   stylesObject[firstPart] = secondPart;
+// });

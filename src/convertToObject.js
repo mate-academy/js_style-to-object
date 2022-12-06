41be38def5d59c37383b4cee90fa0eca6f9ab228
Illia Shcherbak <illiashcherbak6@gmail.com>
2022-12-06 17:12:41 +0200

    fixed some remarks

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,13 +10,13 @@ function convertToObject(sourceString) {
     .split(';')
     .map(item => item.trim())
     .filter(item => item !== '')
-    .map(item => item.split(':').map(value => value.trim()))
+    .map(item => item.split(':')
+      .map(value => value.trim()))
     .reduce((prev, curr) => ({
       ...prev,
       [curr[0]]: curr[1],
     }), {});
 
-  // console.log(convertedStyles);
   return convertedStyles;
 }
 

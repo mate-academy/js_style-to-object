2ea115f96329743653cf09b2bdd79e25183632d4
haduigon <haduigon@gmail.com>
2022-12-09 21:02:16 +0200

    second commit to pass tests

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,9 +3,11 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
@@ -13,24 +15,24 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  let final = sourceString
-  .split('\n')
-  .filter(string => string.length > 1)
-  .map(string => string.split(':'))
-  .map(string =>
-    string.map((string, i) => i === 0
-    ? string.trim()
-    : string.slice(0, -1)
+  const final = sourceString
+    .split('\n')
+    .filter(string => string.length > 1)
+    .map(string => string.split(':'))
+    .map(string =>
+      string.map((string3, i) => i === 0
+        ? string3.trim()
+        : string3.slice(0, -1)
+      )
     )
-  )
-  .filter(string => string.length > 1)
-  .map(string => string.map(string => string.trim()))
-  .reduce((a, x) =>
-  {
-    a[x[0]] = x[1]
-    return a;
-  }, {}
-  )
+    .filter(string => string.length > 1)
+    .map(string => string.map(string2 => string2.trim()))
+    .reduce((a, x) => {
+      a[x[0]] = x[1];
+
+      return a;
+    }, {}
+    );
 
   return final;
 }

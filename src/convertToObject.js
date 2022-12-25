edfef25141bade6a376253b026dd752984a90f70
Nikitin Evgeny <ngenya81@gmail.com>
2022-12-25 11:34:50 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -23,22 +23,4 @@ function convertToObject(sourceString) {
 
   return result;
 };
-
-// function convertToObject(sourceString) {
-//   const words = sourceString.split(';');
-//   const result = {};
-
-//   for (const word of words) {
-//     const splitedWord = word.split(':');
-
-//     if (splitedWord.length === 2) {
-//       const [key, value] = splitedWord;
-
-//       result[key.trim()] = value.trim();
-//     }
-//   }
-
-//   return result;
-// };
-
 module.exports = convertToObject;

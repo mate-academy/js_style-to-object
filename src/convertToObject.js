bdf3ed6333b7824fc40ef693273696cfa355d55a
Nazarii Kyselevych <nazarkysel@gmail.com>
2022-12-30 19:00:52 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,34 @@
 'use strict';
+// no max-len
 
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with
+ * styles (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties (see an example
+ * in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map(e => e.trim())
+    .reduce((stylesObject, styleString) => {
+      if (styleString) {
+        const [property, value] = styleString
+          .split(':')
+          .map(e => e.trim());
+
+        stylesObject[property] = value;
+      }
+
+      return stylesObject;
+    }, {});
 }
 
 module.exports = convertToObject;

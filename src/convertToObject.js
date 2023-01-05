23a437ba6a91e0a19489386bb8253027d17a6485
Oleksandr Kardash <sashonchic@gmail.com>
2023-01-05 07:24:12 +0200

    checklist

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,13 +3,17 @@
 function convertToObject(sourceString) {
   const styles = {};
 
-  const stylesArr = sourceString.trim().split(';');
+  const stylesArr = sourceString
+    .trim()
+    .split(';');
 
   stylesArr.map(item => {
     const element = item.split(':');
 
     if (element[1]) {
-      styles[element[0].trim()] = item.split(':')[1].trim();
+      styles[element[0].trim()] = item
+        .split(':')[1]
+        .trim();
     }
   });
 

ddde3d1f7949ad81d4fe8463c6c0636241f02a70
Oleh Chermak <ochermak2002@gmail.com>
2023-01-10 14:42:45 +0200

    Method reduce used instead of forEach

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,19 +1,17 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  const stylesObject = {};
   const styleAndPropArr = sourceString
     .replace(/\n/g, '')
     .split(';')
     .map(item => item.trim().split(':'))
     .filter(arr => arr.length > 1);
 
-  styleAndPropArr
-    .forEach(style => (
-      stylesObject[style[0].trim()] = style[1].trim())
-    );
+  return styleAndPropArr.reduce((stylesObj, style) => {
+    stylesObj[style[0].trim()] = style[1].trim();
 
-  return stylesObject;
+    return stylesObj;
+  }, {});
 }
 
 module.exports = convertToObject;

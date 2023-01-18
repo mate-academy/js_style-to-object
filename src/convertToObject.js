709ddf6fd1726959a884904799373edb2254b27a
Oleksandr Maslo <98667890az@gmail.com>
2023-01-18 17:10:03 +0200

    Fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,7 @@
  * Function takes a string with styles (see an example in
  * [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties 
+ * and values are the values of related CSS properties
  * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
@@ -15,13 +15,12 @@
  */
 function convertToObject(sourceString) {
   const stringsObject = {};
-  const subStrings
-    = sourceString.split(';').filter(
-      item => item.includes(':') && item.length > 1);
+  const subStrings = sourceString
+    .split(';')
+    .filter(item => item.includes(':') && item.length > 1);
 
   for (let i = 0; i < subStrings.length; i++) {
-    const key = subStrings[i].split(':')[0];
-    const property = subStrings[i].split(':')[1];
+    const [key, property] = subStrings[i].split(':');
 
     stringsObject[key.trim()] = property.trim();
   }

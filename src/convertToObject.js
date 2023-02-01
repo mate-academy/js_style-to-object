0fc3fad32e4a263b6ad147bb069b5ce5d7b6f445
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-02-01 16:17:34 +0200

    method map was removed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,26 +14,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const sourceArray = sourceString.split(';');
-
-  const cleanedArray = sourceArray.map(e => {
-    const separator = e.indexOf(':');
-    const key = e.slice(0, separator).trim();
-    const value = e.slice(separator + 1).trim();
-
-    return [key, value];
-  });
-
-  const object = cleanedArray.reduce((acc, item) => {
-    const key = item[0];
-    const value = item[1];
+  const object = sourceString.split(';').reduce((acc, item) => {
+    const separator = item.indexOf(':');
+    const key = item.slice(0, separator).trim();
+    const value = item.slice(separator + 1).trim();
 
     acc[key] = value;
 
     return acc;
   }, {});
 
-  // if we have some empty key we need to delete  it
   delete object[''];
 
   return object;

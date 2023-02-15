5c12fc7c990171c4d2ac69b6e64e1f2df803184d
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-02-15 19:26:57 +0200

    Initial commit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,60 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
+// function convertToObject(sourceString) {
+//   const str = sourceString;
+//   let resultStr = '';
+//   const obj = {};
+
+//   for (const ch of str) {
+//     resultStr += ch.trim();
+//   }
+
+//   const arr = resultStr.split(';');
+
+//   arr.pop();
+
+//   const resultArray = arr.map(item => item.split(':'));
+
+//   const result = [].concat(...resultArray);
+
+//   for (let i = 0; i < result.length; i += 2) {
+//     if (result[i] === '') {
+//       continue;
+//     }
+//     obj[result[i]] = result[i + 1];
+//   }
+
+//   return obj;
+// }
+
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const array = sourceString.split(';').map(item => item.split(':'));
+
+  const result = [].concat(...array);
+
+  const resArray = result.map(item => item.trim());
+
+  for (let i = 0; i < resArray.length; i += 2) {
+    if (resArray[i] === '') {
+      continue;
+    }
+
+    obj[resArray[i]] = resArray[i + 1];
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

04cb0be445ef0db8690b2439ae1afefa03f47e8b
andrej <andersontwentytwo@gmail.com>
2023-02-22 20:09:53 +0200

    task done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,34 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ // eslint-disable-next-line max-len
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const regex = /, |:|\n/g;
+  const normalizedString = sourceString.replace(regex, '');
+
+  const complexProperties = normalizedString.split(';');
+
+  const properties = complexProperties.map(
+    element => element.split(' ').filter(item => item !== '')
+  ).filter(element => element.length !== 0);
+
+  const computedProperties = {};
+
+  properties.forEach(element => {
+    computedProperties[element[0]] = element.splice(1).join(' ');
+  });
+
+  return computedProperties;
 }
 
 module.exports = convertToObject;

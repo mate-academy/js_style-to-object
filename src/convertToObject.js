a6f688439abdc798ce90081b2108420be4d28f3f
Kateryna Babina <katerynababina01@gmail.com>
2023-02-23 17:24:24 +0200

    optimized code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,8 +1,8 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  const styleDeclarations = sourceString.split(';');
-  const stylesObject = styleDeclarations
+  return sourceString
+    .split(';')
     .filter((style) => style.trim())
     .reduce((styleObject, style) => {
       const [key, value] = style.split(':');
@@ -11,8 +11,6 @@ function convertToObject(sourceString) {
 
       return styleObject;
     }, {});
-
-  return stylesObject;
 }
 
 module.exports = convertToObject;

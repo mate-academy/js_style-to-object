bcda9690dd432046e243f2658e2b6e260f96e5e3
Vadym Dubniak <dubnyakv@gmail.com>
2023-03-20 20:52:43 +0200

    added function for object converting

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceStringArray = sourceString
+    .split(';')
+    .map(styleString => styleString.trim().split(':'))
+    .filter(a => a.length > 1);
+
+  const sourceStringObject = sourceStringArray
+    .reduce((resultObject, arrayElement) => ({
+      ...resultObject,
+      [arrayElement[0].trim()]: arrayElement[1].trim(),
+    }), {});
+
+  return sourceStringObject;
 }
 
 module.exports = convertToObject;

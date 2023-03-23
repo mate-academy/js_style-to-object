a1dcd2eb3819e52f5417c51642c6e67ebf80334b
Hamuud <artemlask2014@gmail.com>
2023-03-23 23:08:57 +0200

    Convert string to Object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,31 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // now my dreams consist only of split, map, map, filter, filter and reduce...
+  const strToObject = sourceString
+    .split(';')
+    .map(el => el.split(':')
+      .map(space => space.trim())
+      .filter(zeroValue => zeroValue !== ''))
+    .filter(space => space.length > 0)
+    .reduce((accumulator, [key, value]) => {
+      accumulator[key] = value;
+
+      return accumulator;
+    }, {});
+
+  return strToObject;
 }
 
 module.exports = convertToObject;

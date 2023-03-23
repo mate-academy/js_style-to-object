ef8813b6531b12b62c17b08ba4decdd46735883f
Mark Boiko <boikomark555@gmail.com>
2023-03-23 09:06:13 +0200

    fix mistakes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,7 +4,7 @@ function convertToObject(sourceString) {
   const styles = sourceString.split(';');
   const objectStyles = {};
 
-  const formatedStyles = styles.map(style => {
+  const formattedStyles = styles.map(style => {
     let styleWithoutSpace = style.slice(1);
 
     if (styleWithoutSpace.includes('\n')) {
@@ -17,11 +17,11 @@ function convertToObject(sourceString) {
   })
     .filter(style => style.length > 1);
 
-  for (const style of formatedStyles) {
+  formattedStyles.forEach(style => {
     const [property, value] = style;
 
     objectStyles[property.trim()] = value.trim();
-  }
+  });
 
   return objectStyles;
 }

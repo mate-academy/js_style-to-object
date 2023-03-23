e0fdd64fce2d5cf3622518a1711377ec18cdaca1
Ivan Tkachenko <ivantkachenko3301@gmail.com>
2023-03-23 21:34:08 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,32 @@
 'use strict';
 
+// Implement convertToObject function:
+// eslint-disable-next-line max-len
+// Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+// and returns an object where CSS properties are keys
+// eslint-disable-next-line max-len
+// and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
 /**
- * Implement convertToObject function:
- *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
- *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleCssObject = {};
+  const fromStringToArray = sourceString
+    .split(';')
+    .map(elem => elem.split(':'))
+    .filter(elem => elem.length !== 1)
+    .map(elem => elem.map(elem2 => elem2.trim()));
+
+  fromStringToArray.forEach(item => {
+    const key = item[0];
+    const property = item[1];
+
+    styleCssObject[key] = property;
+  });
+
+  return styleCssObject;
 }
 
 module.exports = convertToObject;

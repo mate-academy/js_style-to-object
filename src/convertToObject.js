99f6ce40d9c8c25eaaa36b6c1238c2bdf620fda6
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-03-23 23:55:25 +0200

    convert to object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,27 @@
 'use strict';
 
+const stylesString = require('./stylesString');
+
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return Object.fromEntries(
+    stylesString
+      .split(';')
+      .map(style => style.split(':').map(item => item.trim()))
+      .filter(([key, value]) => key && value)
+  );
 }
 
 module.exports = convertToObject;

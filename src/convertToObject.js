fe320f0afa8429b20b582b9e43239fc2c38ff98a
Artem Kyrylenko <kyrylenko.a.a@gmail.com>
2023-03-24 15:05:23 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,45 +5,11 @@
  *
  * Function takes a string with styles (see an example in [stylesString.js]
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl
+ * and values are the values of related CSS properties (see an example)
  * @param {string} sourceString
  *
  * @return {object}
  */
-
-// const stylesString = `
-//   background-color:      #fff;
-// -webkit-border-radius: 5px;
-//   border-radius     : 5px;
-//   border: 1px solid #e8e8e8;
-//   -webkit-box-sizing: border-box;
-//   box-sizing: border-box;
-//   clear   : both       ;
-//   cursor: pointer;
-//   float: left;
-//   font-family: inherit;
-//       font-size: 14px;
-//   font-weight: 400;
-//   height: 42px;
-//   line-height:    40px;
-//   outline: 0;
-//   padding-left    : 18px;
-//   padding-right: 30px;
-//   ;
-
-//   ;
-//   position: relative;
-
-//   text-align: left !important;
-//   -webkit-transition: all .2s ease-in-out;
-//   transition: all .2s ease-in-out;
-//   -moz-user-select: none;
-//   -ms-user-select: none;
-//   user-select: none;
-
-//   white-space: nowrap;
-//   width: auto;
-// `;
 
 function convertToObject(sourceString) {
   const stylesObject = {};
@@ -51,7 +17,7 @@ function convertToObject(sourceString) {
   sourceString
     .split(';')
     .map(elem => elem.trim())
-    .filter(item => item !== '')
+    .filter(elem => elem !== '')
     .map(style => style.split(':').map(elem => elem.trim()))
     .forEach(elem => (stylesObject[elem[0]] = elem[1]));
 

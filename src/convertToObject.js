ce9269eba71fc4c80d9c328e3b801d809112a209
dmytro-pelovych <falcon.patron@gmail.com>
2023-03-28 12:14:22 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,17 +1,31 @@
 'use strict';
 
+/**
+ * Implement convertToObject function:
+ *
+ * Function takes a string with styles
+ * and returns an object where CSS properties are keys
+ * and values are the values of related CSS properties
+ *
+ * @param {string} sourceString
+ *
+ * @return {object}
+ */
 function convertToObject(sourceString) {
   return sourceString
-    .split(';')
-    .reduce((cssRulesObject, unparsedRule) => {
-      const [property, value]
-        = unparsedRule.split(':').map(ruleItem => ruleItem.trim());
+    .split(';') // split by <;>
+    .reduce((rules, unparsedRule) => {
+      // retrieve trimmed key and value
+      const [key, value] = unparsedRule
+        .split(':')
+        .map(rule => rule.trim());
 
-      if (property && value) {
-        cssRulesObject[property] = value;
+      // write rule if key and value exists
+      if (key && value) {
+        rules[key] = value;
       }
 
-      return cssRulesObject;
+      return rules;
     }, {});
 }
 

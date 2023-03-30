3980d9fb3658b6fa4cc2451be66cab95c3f29d44
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-30 13:20:24 +0300

    replaced for loop with forEach method

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,14 +6,14 @@ function convertToObject(sourceString) {
   // write your code here
   const splitByNewLine = sourceString.split(';');
 
-  for (let i = 0; i < splitByNewLine.length; i++) {
-    const lines = splitByNewLine[i].trim().split(':');
+  splitByNewLine.forEach(element => {
+    const lines = element.trim().split(':');
     const clearLines = lines.map(el => el.trim());
 
     if (clearLines[1]) {
       cssParams[clearLines[0]] = clearLines[1];
     }
-  }
+  });
 
   return cssParams;
 }

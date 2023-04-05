c959df3d43a5c0a74ea5d6ceb15ae10d52818a72
Ihor Chaikovskyi <ihorchaikovskyiweb@gmail.com>
2023-04-05 21:55:02 +0300

    third attempt

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,9 +13,13 @@ function convertToObject(sourceString) {
   const array = sourceString.split(`\n`).filter(x => x.length > 4);
 
   const object = array.reduce((prev, oneElement) => {
-    const name = oneElement.slice(0, oneElement.indexOf(':')).trim();
-    const value = oneElement.slice(
-      oneElement.indexOf(':') + 1, oneElement.length - 1).trim();
+    const helperSlice = (element, start, end) => (
+      element.slice(start, end).trim()
+    );
+
+    const name = helperSlice(oneElement, 0, oneElement.indexOf(':'));
+    const value = helperSlice(oneElement, oneElement.indexOf(':') + 1,
+      oneElement.length - 1);
 
     return {
       ...prev,

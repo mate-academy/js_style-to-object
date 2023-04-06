faad0360a409c4233f761d8c4f1221b83c1dbb5d
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-04-06 15:29:36 +0200

    solution v1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,34 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const lines = sourceString.split(';').map(line => {
+    const splittedLine = line.split(':');
+    const lineName = splittedLine[0].trim() || '_____toRemove';
+    let lineValue = splittedLine[1] || '';
+
+    lineValue = lineValue.trim();
+
+    return [lineName, lineValue];
+  });
+
+  const style = {};
+
+  lines.forEach(styleLine => (style[styleLine[0]] = styleLine[1]));
+
+  delete style._____toRemove;
+
+  return style;
 }
 
 module.exports = convertToObject;

94e4e6796eaa14a8c93cbbbede6c04541cbc2954
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-04-07 08:08:59 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,6 +13,18 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const cssStyles = {};
+
+  const cssPropertiesAndValues = sourceString
+    .split(';')
+    .map(propertyOrValue => propertyOrValue.split(':'))
+    .filter(propertyOrValue => propertyOrValue.length === 2);
+
+  for (const propertyOrValue of cssPropertiesAndValues) {
+    cssStyles[propertyOrValue[0].trim()] = propertyOrValue[1].trim();
+  }
+
+  return cssStyles;
 }
 
 module.exports = convertToObject;

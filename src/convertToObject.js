f4518b94f1126a7021a767a6c11309280675480b
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-04-13 13:52:38 +0200

    Fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,8 +3,8 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles
- * (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with resultStyles
+ * (see an example in [resultStylesString.js](./resultStylesString.js))
  * and returns an object where CSS properties are keys
  * and values are the values of related CSS properties
  * (see an exampl in [test file](./convertToObject.test.js))
@@ -15,36 +15,22 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const style = {};
-  let dividedProperties = sourceString.split(';');
+  const resultStyle = {};
   const propertiesSplited = [];
 
-  dividedProperties = dividedProperties.filter(
-    element => element.trim().length > 3
+  sourceString.split(';').filter(
+    pair => pair.trim().length > 3
+  ).forEach(
+    fixedPair => propertiesSplited.push(fixedPair.split(':'))
   );
 
-  dividedProperties.forEach(
-    element => propertiesSplited.push(element.split(':'))
+  propertiesSplited.filter(
+    cssPair => cssPair[1] !== undefined
+  ).forEach(
+    validPair => (resultStyle[validPair[0].trim()] = validPair[1].trim())
   );
 
-  const propertiesFixed = propertiesSplited.filter(
-    element => element[1] !== undefined
-  );
-
-  propertiesFixed.forEach(
-    element => (style[element[0].trim()] = element[1].trim())
-  );
-  // for (let i = 0; i < propertiesSplited.length; i++) {
-  //   style[propertiesFixed[i][0].trim()] = propertiesFixed[i][1].trim();
-  // }
-
-  return style;
+  return resultStyle;
 }
 
 module.exports = convertToObject;
-
-// for (let i = 0; i < propertiesSplited.length; i++) {
-//   if (propertiesSplited[i][1] !== undefined) {
-//     style[propertiesSplited[i][0].trim()] = propertiesSplited[i][1].trim();
-//   }
-// }

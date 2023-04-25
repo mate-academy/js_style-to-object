bb5b0abe393a1e77a8b7b0682982e1a2f48f1c16
spencerspe <spencerspe@gmail.com>
2023-04-25 12:56:49 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,9 +3,11 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
@@ -13,6 +15,26 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  // const stringToarray = sourceString.replace(/\s/g, '').split(';');
+
+  // const cssAtions = stringToarray.filter(action => action.split(':'));
+  // const cleaned = cssAtions.filter(action => action.length !== 0);
+  // const cssArray = cleaned.map(item => item.split(':'));
+
+  // const cssObject = cssArray.reduce((o, array) =>
+  //   Object.assign(o, { [array[0]]: array[1] }), {});
+
+  // return cssObject;
+
+  const stringToarray = sourceString.replace(/\s\s+/g, ' ').split(';');
+
+  const cleaned = stringToarray.filter(action => action.length > 1);
+  const cssArray = cleaned.map(item => item.split(':'));
+
+  const cssObject = cssArray.reduce((o, array) =>
+    Object.assign(o, { [array[0].trim()]: String(array[1]).trim() }), {});
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

63eec4a5c0024f912b541d4caa370fa12f5ea2ef
Leonid Krainik <rialleons@gmail.com>
2023-04-28 21:25:19 +0300

    Convert style string to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,28 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an example in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const lines = sourceString.split('\n');
+  const filledLines = lines.filter(line => line.match(/[a-z]/));
+  const semicolonRemove = filledLines.map(line => line.slice(0, -1));
+  const keyValuePairs = semicolonRemove.map(elem => elem.split(':'));
+  const stylesObject = {};
+
+  for (const pair of keyValuePairs) {
+    stylesObject[pair[0].trim()] = pair[1].trim();
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

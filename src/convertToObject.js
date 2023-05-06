3218ecdffee6a4bc4a68d69a536838dbfb1d96ce
domkab <dominykas.bobinas@gmail.com>
2023-05-06 16:46:18 +0300

    return object shorthand added

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,36 +1,5 @@
 'use strict';
 
-const stylesString = `
-  background-color:      #fff;
--webkit-border-radius: 5px;
-  border-radius     : 5px;
-  border: 1px solid #e8e8e8;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  clear   : both       ;
-  cursor: pointer;
-  float: left;
-  font-family: inherit;
-      font-size: 14px;
-  font-weight: 400;
-  height: 42px;
-  line-height:    40px;
-  outline: 0;
-  padding-left    : 18px;
-  padding-right: 30px;
-  ;
-  ;
-  position: relative;
-  text-align: left !important;
-  -webkit-transition: all .2s ease-in-out;
-  transition: all .2s ease-in-out;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-  white-space: nowrap;
-  width: auto;
-`;
-
 function convertToObject(sourceString) {
   const styles = sourceString
     .split(';')
@@ -39,14 +8,13 @@ function convertToObject(sourceString) {
     .reduce((acc, style) => {
       const [key, value] = style.split(':').map(str => str.trim());
 
-      acc[key] = value;
-
-      return acc;
+      return {
+        ...acc,
+        [key]: value,
+      };
     }, {});
 
   return styles;
 }
-
-convertToObject(stylesString);
 
 module.exports = convertToObject;

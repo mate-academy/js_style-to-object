39bce8094a0e116adf6b43215011e43633aad715
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-17 23:38:10 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,59 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+
+  sourceString.split(';').map(string =>
+    string.trim()).filter(string =>
+    string !== '').map(string => string.split(':')).forEach(string => {
+    const [key, value] = string;
+
+    stylesObject[key.trim()] = value.trim();
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;
+
+convertToObject(`
+background-color:      #fff;
+-webkit-border-radius: 5px;
+border-radius     : 5px;
+border: 1px solid #e8e8e8;
+-webkit-box-sizing: border-box;
+box-sizing: border-box;
+clear   : both       ;
+cursor: pointer;
+float: left;
+font-family: inherit;
+    font-size: 14px;
+font-weight: 400;
+height: 42px;
+line-height:    40px;
+outline: 0;
+padding-left    : 18px;
+padding-right: 30px;
+;
+
+;
+position: relative;
+
+
+text-align: left !important;
+-webkit-transition: all .2s ease-in-out;
+transition: all .2s ease-in-out;
+-moz-user-select: none;
+-ms-user-select: none;
+user-select: none;
+
+
+white-space: nowrap;
+width: auto;
+`);

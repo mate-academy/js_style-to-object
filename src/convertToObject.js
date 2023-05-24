3f03a694e8a5029c7216dba53d2e1774e89a7a90
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-24 16:54:27 +0300

    code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,31 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const returnObj = {};
+  const splitByString = sourceString.trim().split(';');
+
+  splitByString.forEach(style => {
+    const [key, value] = style
+      .split(':')
+      .map(part => part.trim());
+
+    if (key && value) {
+      returnObj[key] = value;
+    }
+  });
+
+  return returnObj;
 }
 
 module.exports = convertToObject;
--- src/stylesString.js
+++ src/stylesString.js
@@ -2,7 +2,7 @@
 
 const stylesString = `
   background-color:      #fff;
--webkit-border-radius: 5px;
+  -webkit-border-radius: 5px;
   border-radius     : 5px;
   border: 1px solid #e8e8e8;
   -webkit-box-sizing: border-box;
@@ -11,7 +11,7 @@ const stylesString = `
   cursor: pointer;
   float: left;
   font-family: inherit;
-      font-size: 14px;
+  font-size: 14px;
   font-weight: 400;
   height: 42px;
   line-height:    40px;

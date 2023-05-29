b910ba783020502411abd768dcee6bc7e75af5e8
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-29 18:34:31 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,11 +1,14 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
@@ -13,6 +16,16 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const arr = sourceString.split(";");
+
+  return arr
+    .map((style) => style.trim().split(":"))
+    .filter(([property, value]) => property && value)
+    .reduce((styles, [property, value]) => {
+      styles[property.trim()] = value.trim();
+
+      return styles;
+    }, {});
 }
 
 module.exports = convertToObject;

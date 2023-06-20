7120f59671dbb54f14ac22952aaa7360bc0362a0
Nadiia Kovalchuk <nadiaaaa1256@gmail.com>
2023-06-20 20:02:19 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 /**
@@ -11,8 +12,19 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceObj = sourceString.split(';')
+    .map(source => source.trim())
+    .filter(source => source.length > 1)
+    .map(source => source.split(':'))
+    .reduce((obj, curr) => {
+      return {
+        ...obj, [curr[0].trim()]: curr[1].trim(),
+      };
+    }, {});
+
+  return sourceObj;
 }
 
 module.exports = convertToObject;

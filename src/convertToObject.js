2c32acd8ae9101106126db139349ef2c68bc83d1
ihorhladkov <ihor.hladkov@gmail.com>
2023-06-20 23:12:42 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,74 @@
 'use strict';
 
+const styleString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
+
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles (see an example in
+ *  [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ *  (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const convertStyles = sourceString.split(';').map(styles => {
+    return styles.trim().split(':').map(stylesElement => {
+      return stylesElement.trim();
+    }).join(':');
+  }).filter(elem => {
+    return elem !== '';
+  });
+
+  const stylesObject = {};
+
+  convertStyles.forEach(style => {
+    const [key, value] = style.split(':');
+
+    stylesObject[key] = value;
+  });
+
+  return stylesObject;
 }
+
+convertToObject(styleString);
 
 module.exports = convertToObject;

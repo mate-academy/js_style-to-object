3c853ecfc8bae91b77caa3766e9be33073cad168
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-20 19:56:33 +0300

    Add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,28 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObj = {};
+  const formattedStyles = sourceString
+    .split(';')
+    .map(str => str.split(':').map(word => word.trim()))
+    .filter(arr => arr.length === 2);
+
+  formattedStyles.forEach(([property, value]) => {
+    stylesObj[property] = value;
+  });
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

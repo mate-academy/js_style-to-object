c41388c479f1b58f5243008d3f56d9dc6aac7b42
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-06-22 15:39:43 +0300

    Fix linter issue

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,30 +1,36 @@
-/*eslint-disable*/
-"use strict";
+'use strict';
 
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const splitedString = sourceString.split(";");
+  const splitedString = sourceString.split(';');
 
   const divide = splitedString.map((prop) => {
-    const dividedArr = prop.trim().split(":");
+    const dividedArr = prop.trim().split(':');
 
-    return dividedArr.map((prop) => prop.trim()).filter((prop) => prop !== "");
+    return dividedArr
+      .map((property) => property.trim())
+      .filter((property) => property !== '');
   });
 
   const styledObj = divide
     .filter((prop) => prop.length > 0)
     .reduce((prev, [key, value]) => {
-      return { ...prev, [key]: value };
+      return {
+        ...prev,
+        [key]: value,
+      };
     }, {});
 
   return styledObj;

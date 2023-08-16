48bb47462aef34d80d050aef1ec37b2080400534
Volodymyr <psn22782@gmail.com>
2023-08-16 20:54:05 +0300

    fixed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,10 +11,10 @@ function convertToObject(sourceString) {
   const styleArray = sourceString.split(';');
 
   for (const style of styleArray) {
-    const styleParams = style.split(':');
+    const [key, value] = style.split(':');
 
-    if (styleParams[0] && styleParams[1]) {
-      result[styleParams[0].trim()] = styleParams[1].trim();
+    if (key && value) {
+      result[key.trim()] = value.trim();
     }
   }
 

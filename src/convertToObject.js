6f26bc71716b3f60a9cabe7484879221d19e28da
KlymSudakov <sudakov.klym@gmail.com>
2023-08-19 13:35:54 +0300

    add fixes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const STYLES_SPLITTER = ';';
+const STYLE_KEY_VALUE_SPLITTER = ':';
+
 function convertToObject(sourceString) {
   return sourceString
     .trim()
-    .split(';')
-    .filter(style => style.trim() !== '')
+    .split(STYLES_SPLITTER)
     .reduce((styles, stylePair) => {
-      const [key, value] = stylePair.split(':');
+      const [key, value] = stylePair.split(STYLE_KEY_VALUE_SPLITTER);
 
       if (key && value) {
         styles[key.trim()] = value.trim();

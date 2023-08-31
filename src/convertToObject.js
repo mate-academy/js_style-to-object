fc89065517e1aee6fde9d4f443c345f7430111bb
Sergiy Sergiyenko <comith5@ukr.net>
2023-08-31 08:41:26 +0300

    Changes by suggestion 1. The variable 'arrOfProperties' declare and assign on the same line. 2. Instead of checking "if 'prop' is undefined" you used ".filter". 3. ".reduce" added to a chain

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,30 +14,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const arrOfProperties
-    = sourceString.split(';')
-      .map((property) => {
-        if (property.indexOf(':') >= 0) {
-          const convertedProp = property.split(':');
+  const objOfProperties = sourceString.split(';')
+    .map((property) => {
+      if (property.indexOf(':') >= 0) {
+        const convertedProp = property.split(':');
 
-          convertedProp[0] = convertedProp[0].trim();
-          convertedProp[1] = convertedProp[1].trim();
+        convertedProp[0] = convertedProp[0].trim();
+        convertedProp[1] = convertedProp[1].trim();
 
-          return convertedProp;
-        }
-      });
-
-  const objOfProperties = arrOfProperties.reduce((prev, prop) => {
-    if (prop === undefined) {
+        return convertedProp;
+      }
+    })
+    .filter((property) => property !== undefined)
+    .reduce((prev, prop) => {
       return {
-        ...prev,
+        ...prev, [prop[0]]: prop[1],
       };
-    }
-
-    return {
-      ...prev, [prop[0]]: prop[1],
-    };
-  }, {});
+    }, {});
 
   return objOfProperties;
 }

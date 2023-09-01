cdecec3d21880a53f00ab5332ae4942cb600a9d8
Bohdan Svystun <bogdansvystun02@gmail.com>
2023-09-01 17:15:59 +0300

    Solution [Svystun Bohdan]

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,44 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * Function takes a string with styles
+ *  (see an example in [stylesString.js](./stylesString.js))
+ * and returns an
+ * object where CSS properties are keys
+ * and values are the
+ * values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  // Split the input stylesString into an array of lines
+  const stylesArray = stylesString.split('\n');
+
+  // Initialize an empty object to store the CSS properties and values
+  const stylesObject = {};
+
+  // Iterate over each line in the stylesArray
+  for (const style of stylesArray) {
+    // Split each line into
+    // a property and a value using the colon (:) as the separator
+    const [property, value] = style.split(':').map(item => item.trim());
+
+    // Check if both property and value are non-empty
+    if (property && value) {
+      // Remove trailing semicolon from the value if it exists
+      const cleanedValue = value.endsWith(';') ? value.slice(0, -1) : value;
+
+      // Trim any leading or trailing spaces from the property value
+      stylesObject[property] = cleanedValue.trim();
+    }
+  }
+
+  // Return the resulting object with
+  // CSS properties as keys and cleaned values as values
+  return stylesObject;
 }
 
 module.exports = convertToObject;

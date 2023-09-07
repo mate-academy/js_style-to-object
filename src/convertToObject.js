8629895d44ce0c3c1af5cab43e84ab00ff27ec7b
«Maria» <nedymai@gmail.com>
2023-09-07 17:37:39 +0300

    ‘Solution2’

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -16,19 +16,12 @@
 function convertToObject(sourceString) {
   const source = sourceString.split(';');
   const result = {};
-  let key = '';
-  let value = '';
-  let startKeyIndex = 0;
-  let endKeyIndex = 0;
 
   for (let prop = 0; prop < source.length; prop++) {
-    startKeyIndex = source[prop].lastIndexOf('\n ') + 1;
-    endKeyIndex = source[prop].indexOf(':');
-    key = source[prop].slice(startKeyIndex, (endKeyIndex)).trim();
-    value = source[prop].slice(endKeyIndex + 1).trim();
+    const [key, value] = source[prop].split(':');
 
     if (key && value) {
-      result[key] = value;
+      result[key.trim()] = value.trim();
     }
   }
 

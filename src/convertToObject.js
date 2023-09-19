c4416ff8c8ab91889360ada26fe54f963c1b96af
Viktoriia Soloviova <ivicki.vs@gmail.com>
2023-09-19 14:08:58 +0300

    with .reduce()

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,21 +1,17 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  const result = {};
+  return sourceString
+    .split(';')
+    .reduce((result, str) => {
+      const [key, value] = str.split(':').map(el => el.trim());
 
-  const arrOfStr = sourceString.split(';');
+      if (key && value) {
+        result[key] = value;
+      };
 
-  for (const str of arrOfStr) {
-    const [key, value] = str
-      .split(':')
-      .map(el => el.trim());
-
-    if (key && value) {
-      result[key] = value;
-    }
-  }
-
-  return result;
+      return result;
+    }, {});
 }
 
 module.exports = convertToObject;

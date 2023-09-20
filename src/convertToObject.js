8ba4eecad3fde44843839ca2d4bab25f9a39707f
koros <korosov.j@gmail.com>
2023-09-20 15:01:30 +0300

    added task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,40 @@
 'use strict';
 
+const normalize = (styleString) => {
+  const styles = styleString.replace(/(\n)|(:)/g, '').split(';');
+  const result = [];
+
+  for (const style of styles) {
+    result.push(style.split(' '));
+  }
+
+  return result
+    .map(x => x.filter(a => a))
+    .filter(b => b.length !== 0);
+};
+
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return normalize(sourceString).reduce((accumulator, string) => {
+    const [property, ...values] = string;
+
+    return {
+      ...accumulator,
+      [property]: values.join(' '),
+    };
+  }, {});
 }
 
 module.exports = convertToObject;

37f6fe4d6e42ded4dd13249b9d2b6efff5b922c0
Viktor Bykh <bykhviktor21@gmail.com>
2023-10-17 13:20:35 +0300

    first version of hw js_style-to-object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,36 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const LINE_SEPARATOR = ';';
+  const PROPERTY_SEPARATOR = ':';
+  const KEY_INDEX = 0;
+  const VALUE_INDEX = 1;
+
+  const arrayLines = sourceString.split(LINE_SEPARATOR);
+  const result = {};
+
+  for (const line of arrayLines) {
+    const arrayProperties = line.trim().split(PROPERTY_SEPARATOR);
+    const key = arrayProperties[KEY_INDEX];
+    const value = arrayProperties[VALUE_INDEX];
+
+    if (key.length !== 0 && value.length !== 0) {
+      result[key.trim()] = value.trim();
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

0698221e8f3d7e4d39c9b6a88c8672950ba8bf56
Dmytro Bashlai <imaxs2016@gmail.com>
2023-10-22 12:53:07 +0300

    The convertToObject function has ben implemented

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,47 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related
+ * CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceArr = sourceString.replace(/\s +|\n/g, '').split(';');
+
+  return sourceArr
+    .filter(item => item !== '')
+    .map(str => str.replace(/^:/, ''))
+    .reduce((objectWithStyle, el) => {
+      const key = el.split(':')[0];
+      const value = el.split(':')[1].replace(/^\s/g, '');
+
+      return {
+        ...objectWithStyle, [key]: value,
+      };
+    }, {});
+  // return sourceString
+  //   .split(';:')
+  //   .join(';')
+  //   .split(';')
+  //   .map(substring => substring.trim())
+  //   .filter(substring => substring !== '')
+  //   .join(';')
+  //   .split(':')
+  //   .map(substring => substring.trim())
+  //   .join(':')
+  //   .split(';')
+  //   .map(substring => substring.split(':'))
+  //   .reduce((styles, sub, index) => {
+  //     return {
+  //       ...styles, [sub[0]]: sub[1],
+  //     };
+  //   }, {});
 }
 
 module.exports = convertToObject;

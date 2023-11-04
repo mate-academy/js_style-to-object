fe6796d3f10cbfdeab2c3a4fa5877ffc4ad579e4
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2023-11-04 04:23:23 +0200

    feat [js-adv]: implement `string-to-object` mapper

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,35 @@
 'use strict';
 
+const SEMICOLON = ';';
+const COLON = ':';
+const TRIM_COLON_REGEX = new RegExp('\\s*:\\s*', 'g');
+
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString.split(SEMICOLON)
+    .map(item => item.trim().replace(TRIM_COLON_REGEX, COLON))
+    .filter(value => value)
+    .forEach(item => {
+      const properties = item.split(COLON);
+
+      result[properties[0]] = properties[1];
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

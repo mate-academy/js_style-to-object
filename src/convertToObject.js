d259d5c0b782ec3cfb881b18d3cc879c549ba4a3
Robert Szałapak <robsza1212@gmail.com>
2023-11-05 14:43:39 +0100

    almost ready solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let solution = {};
+    const stringWithNoSpaces = sourceString.trim();
+  const atributes = stringWithNoSpaces.split(';');
+  for (const elements of atributes) {
+    const [keys, values] = elements.split(':');
+    solution[keys] = values;
+  }
+
+  return solution;
 }
+
+console.log(convertToObject(`
+cursor: pointer;
+float: left;
+font-family: inherit;
+font-size: 14px;
+font-weight: 400;
+height: 42px;
+line-height: 40px;
+outline: 0;`))
 
 module.exports = convertToObject;

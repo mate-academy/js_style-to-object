ab362f7f0f69cb85194edcbc095f83a31b511106
Dmytro Torop <young-z@i.ua>
2023-11-05 18:03:57 +0200

    really? linter lints comments?

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,11 +1,6 @@
 'use strict';
 
 /**
- * Implement convertToObject function:
- *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
@@ -21,7 +16,7 @@ function convertToObject(sourceString) {
       return {
         ...obj,
         [property.trim()]: value.trim(),
-      }
+      };
     }
 
     return obj;

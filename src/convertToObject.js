412dd2a9edf3ea043acd6d81538a039dbed02127
jacekkosiba <jac.kosiba@gmail.com>
2023-11-08 15:47:36 +0100

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -13,30 +13,50 @@
  *
  * @return {object}
  */
+
+//new solution
 function convertToObject(sourceString) {
   return sourceString
-    // splitting by new line
     .split('\n')
-    // filtering out falsy values
-    .filter(Boolean)
-    // trimming ends od lines
-    .map(line => line.trim())
-    // filtering out lines with 1 char
-    .filter(line => line.length > 1)
-    // reducing the array into an object
     .reduce((result, line) => {
-      // destructuring line into property and value variables
-      const [property, value] = line.split(':')
-        // getting rid of semicolon
-        .map(part => part.replace(';', '')
-          // trimming ends of tha property and value
-          .trim());
-      // assinging property and value to the result object
-      result[property] = value;
-      //returning the result object
+      if (line.trim().length > 1) {
+        const [property, value] = line.split(':');
+
+        result[property.trim()] = value.replace(';', '').trim();
+      }
+
       return result;
-    // initValue for reduce method set to an empty object
-    }, {}) 
+    }, {});
 }
+
+//old solution
+
+// function convertToObject(sourceString) {
+//   return sourceString
+//     // splitting by new line
+//     .split('\n')
+//     // filtering out falsy values
+//     .filter(Boolean)
+//     // trimming ends od lines
+//     .map(line => line.trim())
+//     // filtering out lines with 1 char
+//     .filter(line => line.length > 1)
+//     // reducing the array into an object
+//     .reduce((result, line) => {
+//       // destructuring line into property and value variables
+//       const [property, value] = line.split(':')
+//         // getting rid of semicolon
+//         .map(part => part.replace(';', '')
+//           // trimming ends of tha property and value
+//           .trim());
+
+//       // assinging property and value to the result object
+//       result[property] = value;
+
+//       // returning the result object
+//       return result;
+//     // initValue for reduce method set to an empty object
+//     }, {});
+// }
 
 module.exports = convertToObject;

a3107cb2452e158438ced93401380a2d93369a0c
Fluebubble <Trots.anatoly@gmail.com>
2023-11-09 20:06:01 +0200

    task finished

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,42 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ *  (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const objectWithCssParams = {};
+
+  sourceString
+    .split(/\n/)
+    .filter((param) => {
+      const trimmedParam = param.trim();
+
+      return !(trimmedParam.length <= 1);
+    })
+    .map((param) => {
+      return param.split(':').map((value) => {
+        const fixedValue = value.includes(';')
+          ? value.slice(0, value.indexOf(';'))
+          : value;
+
+        return fixedValue.trim();
+      });
+    })
+    .reduce((accumulator, param) => {
+      accumulator[param[0]] = param[1];
+
+      return accumulator;
+    }, objectWithCssParams);
+
+  return objectWithCssParams;
 }
 
 module.exports = convertToObject;

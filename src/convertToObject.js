b6c8a01950252a820e1f8f6c5958e1507a2dd526
Robert Szałapak <robsza1212@gmail.com>
2023-11-13 17:52:48 +0100

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,29 +1,33 @@
+/* eslint-disable max-len */
+/* eslint-disable no-console */
 'use strict';
 
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+* Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+* and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  let solution = {};
-    const stringWithNoSpaces = sourceString.trim();
-  const atributes = stringWithNoSpaces.split(';');
+  const solution = {};
+  const atributes = sourceString.split(';').map((item) => item.trim()).filter((item) => item);
+
   for (const elements of atributes) {
     const [keys, values] = elements.split(':');
-    solution[keys] = values;
+
+    solution[keys.trim()] = values.trim();
   }
 
   return solution;
 }
 
-console.log(convertToObject(`
+// eslint-disable-next-line func-call-spacing
+console.log (convertToObject(`
 cursor: pointer;
 float: left;
 font-family: inherit;
@@ -31,6 +35,6 @@ font-size: 14px;
 font-weight: 400;
 height: 42px;
 line-height: 40px;
-outline: 0;`))
+outline: 0;`));
 
 module.exports = convertToObject;

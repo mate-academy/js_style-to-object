f504a29842582dc374d5ba583b795620515f3835
Your NameNatalia <you@example.comnetzelnatalia@gmail.com>
2023-11-16 10:57:16 +0100

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,31 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles (see an example
+ * in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties (see an exampl
+ * in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const formattedString = sourceString
+    .replace(/\s+/g, ' ')
+    .replace(/ :/g, ':')
+    .split(';')
+    .filter(style => style !== ' ')
+    .map(style => style.trim());
+
+  const stringToObject = formattedString.reduce(function(acc, curr) {
+    acc[curr.split(':')[0]] = curr.split(': ')[1];
+
+    return acc;
+  }, {});
+
+  return stringToObject;
 }
 
 module.exports = convertToObject;

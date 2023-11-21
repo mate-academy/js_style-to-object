0c85b8e83072fad0937deac22cc89363011b8fce
Paweł Nowicki <pawel.nowicki87@tlen.pl>
2023-11-21 22:46:05 +0100

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,17 +14,13 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // Remove unnecessary whitespace and split the string into lines
   const lines = sourceString.trim().split(';');
 
   const stylesObject = {};
 
-  // Process each line
   lines.forEach(line => {
-    // Split each line into property and value using colon as a separator
     const [property, value] = line.split(':').map(item => item.trim());
 
-    // Add the property and value to the stylesObject
     if (property && value) {
       stylesObject[property] = value;
     }

cc3d1b1ac3900bb112258e79a4c570de98683fdf
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-11-27 16:27:48 +0200

    Revert "stylesheet"
    This reverts commit 16724685e2a18caca49d59e3781913c74acf1514.

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,32 +3,16 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles
- * (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties
- * (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const styles = sourceString.split(';');
-
-  const result = {};
-
-  for (const style of styles) {
-    const index = style.indexOf(':');
-    const property = style.slice(0, index).trim();
-    const value = style.slice(index + 1).trim();
-
-    if (property) {
-      result[property] = value;
-    }
-  }
-
-  return result;
+  // write your code here
 }
 
 module.exports = convertToObject;

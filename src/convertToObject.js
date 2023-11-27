77a1f87cabcfd998453924623b69d34f647d2d9d
Wojciech Sęk <wojtasskate@wp.pl>
2023-11-27 11:43:24 +0100

    version1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,8 +11,20 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+  function convertToObject(sourceString) {
+    const array = sourceString.replace(/[\r\n]/gm, '').split(/[;:]+/);
+    const object = {};
+    const trim = array.map(x => x.trim(' '));
+  
+    for (let i = 0; i < trim.length - 1; i=i+2) {
+      if (trim[i].length === 0) {
+        continue;
+      }
+      object[trim[i]]= trim[i + 1];
+    }
+  
+    return object;
+  }
+
 
 module.exports = convertToObject;

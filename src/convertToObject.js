8565924e60bc5fb07c309ab8d96d89447fc093d8
unknown <anorifrell@gmail.com>
2023-12-01 11:21:36 +0200

    converted to Obj

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,9 +3,11 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
@@ -13,6 +15,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const noSemicolon = sourceString.split(';');
+
+  return noSemicolon.reduce((prev, element) => {
+    const [key, value] = element
+      .split(':')
+      .map(text => text.trim());
+
+    if (key) {
+      prev[key] = value;
+    }
+
+    return prev;
+  }, {});
 }
 
 module.exports = convertToObject;

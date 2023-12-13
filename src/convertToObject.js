3a48adb14b88cd9efe630a4fea175595ac764275
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2023-12-13 23:20:47 +0200

    add tast solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,33 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const convertObject = {};
+  const arrString = sourceString.split(';');
+
+  arrString.forEach((element) => {
+    const arr = element.replace(/\n/g, '').split(':');
+
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].trim();
+    }
+
+    if (arr[0] !== '') {
+      convertObject[arr[0]] = arr[1];
+    }
+  });
+
+  return convertObject;
 }
 
 module.exports = convertToObject;

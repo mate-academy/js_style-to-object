18c597b273491eaa35ee95bf96a4c2229b1f94db
Vlad Pashko <vladpavloggg@gmail.com>
2023-12-20 22:25:21 +0200

    convertation func

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,36 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const formattedCss = {};
+
+  sourceString
+    .split(';')
+    .forEach(rule => {
+      const [property, value] = rule.split(':');
+
+      if (property && value) {
+        const formattedProperty = removeSpaces(property);
+
+        formattedCss[formattedProperty] = value.trim();
+      }
+    });
+
+  return formattedCss;
+}
+
+function removeSpaces(str) {
+  return str.replace(/\s/g, '');
 }
 
 module.exports = convertToObject;

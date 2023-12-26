43c3e599ed294e3f29215d8db60503e532e0b9bf
Kostrubitskiy Nazarii <kostrubitskiiwork@gmail.com>
2023-12-26 13:50:20 +0200

    remove console.log, split the code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -16,9 +16,13 @@
 function convertToObject(sourceString) {
   const lines = sourceString.split('\n');
 
+  const removeSemicolon = line => line.replace(';', '');
+
+  const isNotEmptyLine = line => line.trim().length !== 0;
+
   const formatedLine = lines
-    .map(line => line.replace(';', ''))
-    .filter(line => line.trim().length !== 0)
+    .map(removeSemicolon)
+    .filter(isNotEmptyLine)
     .reduce((result, line) => {
       const arr = line.split(':').map(lineKey => lineKey.trim());
       const [key, value] = arr;
@@ -28,46 +32,7 @@ function convertToObject(sourceString) {
       return result;
     }, {});
 
-  // eslint-disable-next-line no-console
-  console.log(formatedLine);
-
   return formatedLine;
 }
-
-convertToObject(`
-background-color:      #fff;
--webkit-border-radius: 5px;
-border-radius     : 5px;
-border: 1px solid #e8e8e8;
--webkit-box-sizing: border-box;
-box-sizing: border-box;
-clear   : both       ;
-cursor: pointer;
-float: left;
-font-family: inherit;
-    font-size: 14px;
-font-weight: 400;
-height: 42px;
-line-height:    40px;
-outline: 0;
-padding-left    : 18px;
-padding-right: 30px;
-;
-
-;
-position: relative;
-
-
-text-align: left !important;
--webkit-transition: all .2s ease-in-out;
-transition: all .2s ease-in-out;
--moz-user-select: none;
--ms-user-select: none;
-user-select: none;
-
-
-white-space: nowrap;
-width: auto;
-`);
 
 module.exports = convertToObject;

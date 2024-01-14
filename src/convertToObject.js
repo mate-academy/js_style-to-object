8416c546e070e79976556d05e35f0a94f2ebe96f
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-14 13:21:06 +0100

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,37 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // Split the input string by newline characters
+  const lines = sourceString.split('\n');
+
+  return lines.reduce((result, line) => {
+    const trimmedLine = line.trim();
+
+    if (trimmedLine === '') {
+      return result;
+    }
+
+    const [property, valueWithSemicolon] = trimmedLine.split(':');
+
+    if (property && valueWithSemicolon) {
+      const cleanValue = valueWithSemicolon.replace(';', '');
+
+      result[property.trim()] = cleanValue.trim();
+    }
+
+    return result;
+  }, {});
 }
 
 module.exports = convertToObject;

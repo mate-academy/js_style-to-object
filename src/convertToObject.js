ea93b0fc24003392449b0a0122fbe2bd2bd404b9
Roman Sylman <woody.falcone.00@gmail.com>
2024-01-23 19:52:47 +0200

    solutionV2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,9 +3,6 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
- * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *

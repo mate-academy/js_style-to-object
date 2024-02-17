45efdcd1c93a7582f60c69f36c113af173cc1c05
Max Petrashishin <rikogjdefolt@gmail.com>
2024-02-17 20:26:01 +0200

    move to nev lines

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,15 +1,18 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  return sourceString.split('\n').slice(1).reduce((prev, item) => {
-    const [key, value] = item.split(':');
+  return sourceString
+    .split('\n')
+    .slice(1)
+    .reduce((prev, item) => {
+      const [key, value] = item.split(':');
 
-    if (key && value) {
-      prev[key.trim()] = value.slice(0, -1).trim();
-    };
+      if (key && value) {
+        prev[key.trim()] = value.slice(0, -1).trim();
+      };
 
-    return prev;
-  }, {});
+      return prev;
+    }, {});
 }
 
 module.exports = convertToObject;

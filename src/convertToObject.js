36a334c44588c12dec7d59b0443a5c62238a13eb
Max Petrashishin <rikogjdefolt@gmail.com>
2024-02-18 12:30:58 +0200

    add variable

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,18 +1,19 @@
 'use strict';
 
 function convertToObject(sourceString) {
-  return sourceString
-    .split('\n')
-    .slice(1)
-    .reduce((prev, item) => {
-      const [key, value] = item.split(':');
+  const splittedVariable = sourceString.split('\n');
+  const slicedVariable = splittedVariable.slice(1);
+  const reduceVariable = slicedVariable.reduce((prev, item) => {
+    const [key, value] = item.split(':');
 
-      if (key && value) {
-        prev[key.trim()] = value.slice(0, -1).trim();
-      };
+    if (key && value) {
+      prev[key.trim()] = value.slice(0, -1).trim();
+    };
 
-      return prev;
-    }, {});
+    return prev;
+  }, {});
+
+  return reduceVariable;
 }
 
 module.exports = convertToObject;

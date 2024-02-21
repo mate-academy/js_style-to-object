78fc9df36524e79f6e013a69ebbca0711fdf7504
winniepuhh <mihajlevskijbogdan@gmail.com>
2024-02-21 18:03:02 +0200

    update variable name

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -14,18 +14,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const obj = {};
+  const convertedObj = {};
   const newLine = sourceString.split(';');
 
   newLine.forEach((line) => {
     const [key, value] = line.split(':');
 
     if (key.trim() && value.trim()) {
-      obj[key.trim()] = value.trim();
+      convertedObj[key.trim()] = value.trim();
     }
   });
 
-  return obj;
+  return convertedObj;
 }
 
 module.exports = convertToObject;

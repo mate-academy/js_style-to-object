d222e24b96a70254cf927e33db28a7d418d31ebb
IvanGritsenko <gritsenkofar2@gmail.com>
2024-02-22 19:31:02 +0200

    Variables have been renamed. The unnecessary check for an empty string has been removed.

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,9 +11,8 @@ function convertToObject(sourceString) {
   return sourceString
     .replace(/\n/g, '')
     .split(';')
-    .map((str) => str.split(':'))
-    .map((str) => str.map((value) => value.trim()))
-    .filter((elem) => elem[0] !== '')
+    .map((lineOfCode) => lineOfCode.split(':').map((value) => value.trim()))
+    .filter((elem) => elem[0])
     .reduce((acc, [key, value]) => {
       acc[key] = value;
 

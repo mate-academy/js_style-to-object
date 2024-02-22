d4b684198657362147cf35568a1f726b7847f689
Lesia Bihun <lbr15@ukr.net>
2024-02-22 11:31:42 +0200

    add style to object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,16 +3,28 @@
 /**
  * Implement convertToObject function:
  *
- * Function takes a string with styles (see an example in [stylesString.js](./stylesString.js))
+ * Function takes a string with styles
+ * (see an example in [stylesString.js](./stylesString.js))
  * and returns an object where CSS properties are keys
- * and values are the values of related CSS properties (see an exampl in [test file](./convertToObject.test.js))
+ * and values are the values of related CSS properties
+ * (see an exampl in [test file](./convertToObject.test.js))
  *
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceArr = sourceString.split(';');
+  const stylesArr = sourceArr.map(style => style.split(':'));
+  const stylesObj = {};
+
+  for (const style of stylesArr) {
+    if (style.length > 1) {
+      stylesObj[style[0].trim()] = style[1].trim();
+    }
+  }
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

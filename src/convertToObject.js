01834c07ce1d21bc28657e19bd77ec996a5a611c
Oleksandra Kuziura <radioday2014@gmail.com>
2024-02-28 12:41:33 +0200

    implement solution of convert string css property in object with keys is css property and value is values of property

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -12,7 +12,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayOfCssValue = sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0);
+  const resultListOfCssPropertes = {};
+
+  for (const ch of arrayOfCssValue) {
+    const cssProperties = ch.split(':');
+
+    resultListOfCssPropertes[cssProperties[0].trim()] = cssProperties[1].trim();
+  }
+
+  return resultListOfCssPropertes;
 }
 
 module.exports = convertToObject;

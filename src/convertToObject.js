054e73bb740d61f2332d1a28a5411b1dfe0bbbea
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-02-29 15:06:52 +0100

    Update

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,7 +4,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  return 0;
+  const objStyles = {};
+
+  let arrStyles = [];
+
+  arrStyles = ((sourceString.split(';')).map(x =>
+    x.trim())).filter(x => x !== '');
+
+  for (const y of arrStyles) {
+    const arr = y.split(':');
+
+    objStyles[array[0].trim()] = arr[1].trim();
+  }
+
+  return objStyles;
 }
 
 module.exports = convertToObject;

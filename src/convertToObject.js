def4a439542bfc9bcadb2d78a0975ac82de34980
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-03-01 16:07:31 +0100

    solution convert

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+function convertToObject(stylesString) {
+  const stylesArray = stylesString
+    .split(";")
+    .filter((style) => style.trim() !== "")
+    .map((style) => style.trim());
 
+  const stylesObject = {};
+
+  stylesArray.forEach((style) => {
+    const [property, value] = style.split(":").map((part) => part.trim());
+
+    stylesObject[property] = value;
+  });
+
+  return stylesObject;
+}
 module.exports = convertToObject;

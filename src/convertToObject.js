70e62ae1fcb4404972a4f547c14a5a6d13102e2b
Maksym-Atanasov <157310647+Maksym-Atanasov@users.noreply.github.com>
2024-03-02 18:52:03 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,91 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const letters = sourceString.split('');
+  let param = '';
+  let paramValue = '';
+  let toggle = 0;
+
+  letters.forEach((value, index) => {
+    if (value === ':') {
+      toggle = 1;
+    }
+
+    if (value === ';' || index === letters.length - 1) {
+      if (value !== ';') {
+        paramValue += value;
+      }
+
+      if (param && paramValue) {
+        result[param] = paramValue;
+      }
+
+      toggle = 0;
+      paramValue = '';
+      param = '';
+    }
+
+    if (
+      value !== ' ' &&
+      value !== ';' &&
+      value !== ':' &&
+      value !== '\n' &&
+      value !== '\t' &&
+      toggle === 0
+    ) {
+      param += value;
+    }
+
+    if (
+      value !== ' ' &&
+      value !== ';' &&
+      value !== ':' &&
+      value !== '\n' &&
+      value !== '\t' &&
+      toggle === 1
+    ) {
+      paramValue += value;
+    }
+
+    if (
+      value === ' ' &&
+      letters[index + 1] !== ' ' &&
+      letters[index + 1] !== ';' &&
+      letters[index + 1] !== ':' &&
+      letters[index + 1] !== '\t' &&
+      letters[index + 1] !== '\n' &&
+      letters[index - 1] !== ' ' &&
+      letters[index - 1] !== ';' &&
+      letters[index - 1] !== ':' &&
+      letters[index - 1] !== '\t' &&
+      letters[index - 1] !== '\n'
+    ) {
+      switch (toggle) {
+        case 0:
+          param += value;
+          break;
+
+        case 1:
+          paramValue += value;
+          break;
+      }
+    }
+
+    if (value === ',') {
+      switch (toggle) {
+        case 0:
+          param += '\n\t';
+          break;
+
+        case 1:
+          paramValue += '\n\t';
+          break;
+      }
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

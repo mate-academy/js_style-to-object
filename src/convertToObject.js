a30efb8d39dae3317485641f5c808e5d6edfcb86
deezhizyu <kalenskuja@gmail.com>
2024-03-03 10:04:32 +0100

    Add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,25 @@
  *
  * @return {object}
  */
+const complexStylesString = require('./stylesString');
+
 function convertToObject(sourceString) {
-  // write your code here
+  const strings = sourceString
+    .split(';')
+    .map((string) => string.trim())
+    .filter((string) => string !== '');
+
+  const result = {};
+
+  for (const string of strings) {
+    const style = string.split(':').map((part) => part.trim());
+
+    result[style[0]] = style[1];
+  }
+
+  return result;
 }
+
+convertToObject(complexStylesString);
 
 module.exports = convertToObject;

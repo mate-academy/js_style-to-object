f54a340f14e9eff5fc3de0f38e730b9fa72927fd
Maksym-Atanasov <157310647+Maksym-Atanasov@users.noreply.github.com>
2024-03-03 18:17:09 +0200

    extreme optimisation

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,89 +6,14 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
-  const letters = sourceString.split('');
-  let param = '';
-  let paramValue = '';
-  let toggle = 0;
+  const letters = sourceString.split(';');
+  const result = letters.reduce((accumulator, value, index) => {
+    const params = value.split(':').map((element) => element.trim());
 
-  letters.forEach((value, index) => {
-    if (value === ':') {
-      toggle = 1;
-    }
+    accumulator[params[0]] = params[1];
 
-    if (value === ';' || index === letters.length - 1) {
-      if (value !== ';') {
-        paramValue += value;
-      }
-
-      if (param && paramValue) {
-        result[param] = paramValue;
-      }
-
-      toggle = 0;
-      paramValue = '';
-      param = '';
-    }
-
-    if (
-      value !== ' ' &&
-      value !== ';' &&
-      value !== ':' &&
-      value !== '\n' &&
-      value !== '\t' &&
-      toggle === 0
-    ) {
-      param += value;
-    }
-
-    if (
-      value !== ' ' &&
-      value !== ';' &&
-      value !== ':' &&
-      value !== '\n' &&
-      value !== '\t' &&
-      toggle === 1
-    ) {
-      paramValue += value;
-    }
-
-    if (
-      value === ' ' &&
-      letters[index + 1] !== ' ' &&
-      letters[index + 1] !== ';' &&
-      letters[index + 1] !== ':' &&
-      letters[index + 1] !== '\t' &&
-      letters[index + 1] !== '\n' &&
-      letters[index - 1] !== ' ' &&
-      letters[index - 1] !== ';' &&
-      letters[index - 1] !== ':' &&
-      letters[index - 1] !== '\t' &&
-      letters[index - 1] !== '\n'
-    ) {
-      switch (toggle) {
-        case 0:
-          param += value;
-          break;
-
-        case 1:
-          paramValue += value;
-          break;
-      }
-    }
-
-    if (value === ',') {
-      switch (toggle) {
-        case 0:
-          param += '\n\t';
-          break;
-
-        case 1:
-          paramValue += '\n\t';
-          break;
-      }
-    }
-  });
+    return accumulator;
+  }, {});
 
   return result;
 }

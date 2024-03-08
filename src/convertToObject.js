54f855ef40aa5edc2d762f26abebe18a832b4c34
Vladyslav <fantom2569@gmail.com>
2024-03-08 15:55:03 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+  // Розділяємо рядок на окремі оголошення CSS
+  const declarations = sourceString.split(';');
+
+  // Проходимося по кожному оголошенню CSS
+  declarations.forEach((declaration) => {
+    // Розділяємо оголошення на назву стилю та його значення
+    const parts = declaration.split(':');
+
+    if (parts.length === 2) {
+      // Видаляємо зайві пробіли trim
+      const property = parts[0].trim();
+      const value = parts[1].trim();
+
+      // Додаємо стиль
+      styles[property] = value;
+    }
+  });
+
+  return styles;
 }
 
 module.exports = convertToObject;

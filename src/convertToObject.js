ec21cb9acc22a6290cf74a76d60cd5d1beb91476
Erik Hulinian <gulinianerik2@gmail.com>
2024-03-10 18:42:28 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const trimmedString = sourceString
+    .split(';')
+    .filter((item) => item.trim() !== '');
+
+  const propertyValuePairs = trimmedString.map((item) => {
+    const [property, value] = item.split(':');
+
+    return [property.trim(), value.trim()];
+  });
+
+  const resultingObject = {};
+
+  propertyValuePairs.map((item) => {
+    resultingObject[item[0]] = item[1];
+  });
+
+  return resultingObject;
 }
 
 module.exports = convertToObject;

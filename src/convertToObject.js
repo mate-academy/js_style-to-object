730744d7de004c1522ea147174cbcd8af5d23de0
hyriix <shyrii11bohdan@gmail.com>
2024-03-12 11:43:33 +0100

    liquidated

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .filter((elem) => elem.trim())
+    .reduce((obj, elem) => {
+      let [key, style] = elem.split(':');
+
+      key = key.trim();
+      style = style.trim();
+
+      if (key && style) {
+        obj[key] = style;
+      }
+
+      return obj;
+    }, {});
 }
 
 module.exports = convertToObject;

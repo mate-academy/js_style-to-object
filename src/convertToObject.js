3b24deacca01b14865ab464ec442f5b6379bd3d6
Olaf <olaf.chuszno@gmail.com>
2024-03-16 16:37:53 +0100

    Add Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,44 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  // rulesString === [
+  //   '  width:   15px   ',
+  //   'padding:    15px',
+  // ]
+  // Trim all surrounding white spaces and split each rule on a semi-colon
+  const rulesStrings = sourceString.trim().split(';');
+
+  // Map through key value pair strings
+  // Elements are long strings with white spaces === '  width:   15px   ',)
+  const keyValueStringArray = rulesStrings.map(function (rule) {
+    // Separate each key and a value
+    const keyValuePair = rule.split(':');
+
+    // Trim both the key and the value
+    const trimmedKeyValuePair = keyValuePair.map((element) => element.trim());
+
+    return trimmedKeyValuePair;
+  }, rulesStrings);
+
+  // Remove empty elements created cause of extra semicolons
+  const keyValueArray = keyValueStringArray.filter(
+    (element) => element[0].length > 0,
+  );
+
+  // Sort the rules alphabetically
+  const sortedRules = keyValueArray.sort((a, b) => a[0].localeCompare(b[0]));
+
+  // Add key value pairs to the result object
+  sortedRules.forEach(function (rule) {
+    const key = rule[0];
+    const value = rule[1];
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

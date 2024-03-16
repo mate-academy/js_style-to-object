51ee22563e09d65f455013c7cadb4c249011a344
svyatoslav <svyatoslavik9@gmail.com>
2024-03-16 13:43:47 +0100

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,70 @@
-'use strict';
+function formatString(string) {
+  let result = '';
 
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
-function convertToObject(sourceString) {
-  // write your code here
+   for (let i = 0; i < string.length; i++) {
+      if (string[i] === '\n') {
+        continue;
+      }
+
+      result += string[i];
+    }
+
+    return result;
 }
 
-module.exports = convertToObject;
+function convertToObject(sourceString) {
+  // write your code here
+    const separator = ';';
+    const styles =  formatString(sourceString).split(separator);
+    const object = {};
+
+    for (const style of styles) {
+      const values = style.split(':');
+      const [property, value] = values;
+
+      if (property && value) {
+        object[property.trim()] = value.trim();
+      }
+    }
+
+    return object;
+}
+
+const complexStylesString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
+
+
+convertToObject(complexStylesString);

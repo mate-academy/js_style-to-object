c4ea74622d010d6ed033390180e405332834c1ce
Olaf <olaf.chuszno@gmail.com>
2024-03-18 11:58:16 +0100

    Rewrite solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,44 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
+  const rules = sourceString
+    .split(';')
+    .map((rule) => rule.split(':').map((part) => part.trim()))
+    .filter((rule) => rule[0] && rule[1]);
 
-  // rulesString === [
-  //   '  width:   15px   ',
-  //   'padding:    15px',
-  // ]
-  // Trim all surrounding white spaces and split each rule on a semi-colon
-  const rulesStrings = sourceString.trim().split(';');
+  const rulesObject = rules.reduce((prev, current) => {
+    return {
+      ...prev,
+      [current[0]]: current[1],
+    };
+  }, {});
 
-  // Map through key value pair strings
-  // Elements are long strings with white spaces === '  width:   15px   ',)
-  const keyValueStringArray = rulesStrings.map(function (rule) {
-    // Separate each key and a value
-    const keyValuePair = rule.split(':');
-
-    // Trim both the key and the value
-    const trimmedKeyValuePair = keyValuePair.map((element) => element.trim());
-
-    return trimmedKeyValuePair;
-  }, rulesStrings);
-
-  // Remove empty elements created cause of extra semicolons
-  const keyValueArray = keyValueStringArray.filter(
-    (element) => element[0].length > 0,
-  );
-
-  // Sort the rules alphabetically
-  const sortedRules = keyValueArray.sort((a, b) => a[0].localeCompare(b[0]));
-
-  // Add key value pairs to the result object
-  sortedRules.forEach(function (rule) {
-    const key = rule[0];
-    const value = rule[1];
-
-    result[key] = value;
-  });
-
-  return result;
+  return rulesObject;
 }
 
 module.exports = convertToObject;

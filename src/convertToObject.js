b5f382bf19069f8c3b4c32a01189ff524fb3bc0a
Bartek <kowalski_1410@wp.pl>
2024-03-25 21:48:53 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,20 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const tab = sourceString
+    .split(';')
+    .join(':')
+    .split(':')
+    .filter((pom) => /\S/.test(pom))
+    .map((el) => el.trim());
+
+  for (let i = 0; i < tab.length; i += 2) {
+    result[tab[i]] = tab[i + 1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

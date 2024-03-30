a20b43acdf3deb7146d2c7f8533bfc7917218907
kinqbert <perehodko1010@gmail.com>
2024-03-30 19:22:49 +0200

    eslint fixes and minor improvements

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* eslint-disable */
-
 /**
  * @param {string} sourceString
  *
@@ -11,12 +9,12 @@ function convertToObject(sourceString) {
   const result = {};
 
   // splitting every property for separate string
-  let props = sourceString
+  const props = sourceString
     .split(';')
     .filter((item) => item !== '' && item !== ' ');
 
   // removing breaking line symbols
-  for (let i in props) {
+  for (const i in props) {
     props[i] = props[i].split('\n').join('');
     props[i] = props[i].split('\t').join('');
   }
@@ -26,12 +24,13 @@ function convertToObject(sourceString) {
 
   // splitting property names and property values
   for (const i in props) {
-    let splittedProp = props[i].split(':');
+    const splittedProp = props[i].split(':');
+
     propsNames[i] = splittedProp[0];
     propsValues[i] = splittedProp[1];
   }
 
-  propsNames = propsNames.filter(item => validProp(item));
+  propsNames = propsNames.filter((item) => validProp(item));
   propsValues = propsValues.filter((item) => validProp(item));
 
   // removing all spaces for each property name
@@ -48,12 +47,12 @@ function convertToObject(sourceString) {
   }
 
   // adding linebreaks for properties with multiple lines
-  for (let i in propsValues) {
-    propsValues[i] = addLinebreaks(propsValues[i]);
+  for (const i in propsValues) {
+    propsValues[i] = propsValues[i].split(',').join(',\n');
   }
 
   // adding properties to an object
-  for (let i in propsNames) {
+  for (const i in propsNames) {
     result[propsNames[i]] = propsValues[i];
   }
 
@@ -98,12 +97,6 @@ function removeStartEndSpaces(string) {
   }
 
   return result;
-}
-
-function addLinebreaks(string) {
-  const separatedProps = string.split(',');
-
-  return separatedProps.join(',\n');
 }
 
 module.exports = convertToObject;

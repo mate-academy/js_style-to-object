cca8b37fa5a813c8c470c7a0a9a0edf713cbc51b
ropanefarias <rodrigopanefarias27@gmail.com>
2024-04-01 18:28:29 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,45 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+
+  const cleanString = sourceString.trim();
+
+  const rules = cleanString.split(';');
+
+  rules.forEach((rule) => {
+    const [rawKey, rawValue] = rule.split(':');
+
+    if (rawKey && rawValue) {
+      const key = rawKey.trim();
+      let value = rawValue.trim();
+
+      if (value.includes(',')) {
+        const lines = value.split(',').map((line) => line.trim());
+
+        value = lines
+          .map((line, index) => {
+            if (index === 0) {
+              return line;
+            } else {
+              return `          ${line}`;
+            }
+          })
+          .join(',\n');
+      }
+
+      styles[key] = value;
+    }
+  });
+
+  return styles;
 }
 
 module.exports = convertToObject;
+
+/**
+ * tenho que transformar a propriedade do CSS em 'key' e seu valor em 'value'
+ * para isso tenho que separar a string antes dos dois pontos como 'key'
+ * e depois dos dois pontos como 'value'
+ *
+ */

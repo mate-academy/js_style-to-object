6b3ffa8cf39efa067a77179d2da13f5e4b871388
Yana Momot <yanamomot2004@gmail.com>
2024-04-04 15:51:38 +0300

    done task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const toArray = sourceString.split(';');
+  const newObject = {};
+
+  toArray.forEach((part) => {
+    if (part.trim() !== '') {
+      const [property, value] = part.split(':').map((remove) => remove.trim());
+
+      if (property && value) {
+        newObject[property] = value;
+      }
+    }
+  });
+
+  return newObject;
 }
 
 module.exports = convertToObject;

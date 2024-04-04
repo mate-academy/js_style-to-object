4210154cd431c048450cc8325f8f23f220ba594e
marchAlbion <marchAlbion@gmail.com>
2024-04-04 19:41:47 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,29 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const trimmedString = sourceString.trim();
+
+  const splittedString = trimmedString.split(';');
+  const removeDuplicated = splittedString.filter(
+    (element) => element.length > 2,
+  );
+
+  const removeSpaces = removeDuplicated.map((element) => {
+    return element.trim().split(':');
+  });
+
+  const removeEmptyString = removeSpaces.filter(
+    (elemet) => elemet.length !== 1,
+  );
+
+  const result = removeEmptyString.reduce((acc, element) => {
+    return { ...acc, [element[0].trim()]: element[1].trim() };
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

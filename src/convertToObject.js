6d3c48975a29dbc0a9fa8b0b0a7659d09210fdaf
Anton Yupko <antonyupkodm@gmail.com>
2024-04-04 16:10:38 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = sourceString.split(';');
+
+  const cssStyles = styles
+    .filter((style) => style.trim().length > 0)
+    .reduce((acc, style) => {
+      let normalizedStyle = style.trim();
+
+      if (normalizedStyle.charAt(0) === '\n') {
+        normalizedStyle = normalizedStyle.slice(1);
+      }
+
+      const splittedStyle = normalizedStyle.split(':');
+      const propertyName = splittedStyle[0].trim();
+      const propertyValue = splittedStyle[1].trim();
+
+      acc[propertyName] = propertyValue;
+
+      return acc;
+    }, {});
+
+  return cssStyles;
 }
 
 module.exports = convertToObject;

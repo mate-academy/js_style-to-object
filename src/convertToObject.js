9a6ba0c8a072193c645ba71c864bcad8588566a9
Anton Yupko <antonyupkodm@gmail.com>
2024-04-04 16:30:14 +0300

    some fixes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,13 +11,7 @@ function convertToObject(sourceString) {
   const cssStyles = styles
     .filter((style) => style.trim().length > 0)
     .reduce((acc, style) => {
-      let normalizedStyle = style.trim();
-
-      if (normalizedStyle.charAt(0) === '\n') {
-        normalizedStyle = normalizedStyle.slice(1);
-      }
-
-      const splittedStyle = normalizedStyle.split(':');
+      const splittedStyle = style.split(':');
       const propertyName = splittedStyle[0].trim();
       const propertyValue = splittedStyle[1].trim();
 

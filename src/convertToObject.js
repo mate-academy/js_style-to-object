986ec90180a4a2d147b984f29d0e6945cca95fe3
Vladislav <7evenLays@gmail.com>
2024-04-04 15:23:04 +0300

    It works

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString) {
+    return {};
+  }
+
+  const STYLES_OBJECT = sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .map((style) => style.split(':'))
+    .filter((pair) => pair[0] !== '')
+    .reduce((acc, [key, value]) => {
+      acc[key.trim()] = value.trim();
+
+      return acc;
+    }, {});
+
+  return STYLES_OBJECT;
 }
 
 module.exports = convertToObject;
+
+// && pair[1] !== undefined

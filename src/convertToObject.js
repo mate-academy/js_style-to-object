bcceb1df1ede41b7de77ceaa12d4cfbf3fcce637
Esther Guimarães <estherguimaraesl@gmail.com>
2024-04-07 20:24:31 -0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssObj = {};
+
+  const cssNormalized = sourceString.replaceAll('\t', '');
+  const cssSplited = cssNormalized.split(';');
+  const cssCommands = cssSplited.filter(
+    (css) => css.replaceAll('\n', '').replaceAll(' ', '') !== '',
+  );
+
+  for (const css of cssCommands) {
+    let [key, value] = css.split(':');
+
+    key = key.trim();
+    value = value.trim();
+    cssObj[key] = value;
+  }
+
+  return cssObj;
 }
 
 module.exports = convertToObject;
---
+++ src/teste.js
@@ -0,0 +1,4 @@
+const convertToObject = require('./convertToObject');
+const complexStylesString = require('./stylesString');
+
+convertToObject(complexStylesString);

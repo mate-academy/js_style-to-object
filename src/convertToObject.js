0fbb6b7d2af47258ddf8bb4126a0617565d65d06
Mariia Lutska <mariialutskayar@gmail.com>
2024-04-07 23:54:12 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,28 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  if (sourceString.length > 0) {
-    const styles = sourceString
-      .trim()
-      .split(';')
-      .filter((style) => style !== '');
+  const styles = sourceString
+    .trim()
+    .split(';')
+    .filter((style) => style !== '');
 
-    const stylesObject = styles.reduce((acc, style) => {
-      const semicolonIndex = style.indexOf(':');
-      const property = style.slice(0, semicolonIndex).trim();
-      const value = style.slice(semicolonIndex + 1).trim();
+  const stylesObject = styles.reduce((acc, style) => {
+    const semicolonIndex = style.indexOf(':');
+    const property = style.slice(0, semicolonIndex).trim();
+    const value = style.slice(semicolonIndex + 1).trim();
 
-      if (property.length > 0) {
-        return { ...acc, [property]: value };
-      } else {
-        return acc;
-      }
-    }, {});
+    if (property.length > 0) {
+      return { ...acc, [property]: value };
+    } else {
+      return acc;
+    }
+  }, {});
 
-    return stylesObject;
-  }
-
-  return {};
+  return stylesObject;
 }
 
 module.exports = convertToObject;

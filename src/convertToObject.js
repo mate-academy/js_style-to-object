c7a66ba321dd353584aaf46286e35e01530f0e6a
Vova Stepanyuk <stepanyukvova200@gmail.com>
2024-04-08 02:54:21 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,26 @@
 'use strict';
 
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
 function convertToObject(sourceString) {
-  // write your code here
+  const lines = sourceString
+    .split(';')
+    .filter((line) => {
+      return line.includes(':');
+    })
+    .map((line) => {
+      const lineProperties = line.split(':').map((property, propertyIndex) => {
+        return property.trim();
+      });
+
+      return lineProperties;
+    });
+
+  const cssProperties = lines.reduce((acc, [key, value]) => {
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return cssProperties;
 }
 
 module.exports = convertToObject;

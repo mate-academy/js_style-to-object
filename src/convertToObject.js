751191f3a1ea0fa6dd756b74a9dff42a1028920d
Dmitriy_Kondratiev <d.kondratiev1@gmail.com>
2024-04-08 12:58:31 +0300

    add solution2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,18 +7,19 @@
  */
 function convertToObject(sourceString) {
   const styles = sourceString.split(';');
-  const CssStyles = {};
+  const cssStyles = {};
 
   styles
     .filter((style) => style.trim())
     .forEach((style) => {
-      const styleKey = style.split(':')[0];
-      const styleValue = style.split(':')[1];
+      const parts = style.split(':');
+      const styleKey = parts[0];
+      const styleValue = parts[1];
 
-      CssStyles[styleKey.trim()] = styleValue.trim();
+      cssStyles[styleKey.trim()] = styleValue.trim();
     });
 
-  return CssStyles;
+  return cssStyles;
 }
 
 module.exports = convertToObject;

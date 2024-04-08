0d2bc47de38861c7a061284c773e37cedabc2e91
unknown <pyuliya2016@gmail.com>
2024-04-08 16:00:13 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,9 +4,26 @@
  * @param {string} sourceString
  *
  * @return {object}
+ *
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+const removeSpaces = (str) => str.trim();
 
+function convertToObject(sourceString) {
+  const styleObject = {};
+
+  sourceString
+    .split(';')
+    .map(removeSpaces)
+    .filter((el) => el.length > 0)
+    .reduce((obj, el) => {
+      const propStyle = el.split(':').map(removeSpaces);
+      const [prop, value] = propStyle;
+
+      obj[prop] = value;
+
+      return obj;
+    }, styleObject);
+
+  return styleObject;
+}
 module.exports = convertToObject;

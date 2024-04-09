c5c93fc20adcabaded537d5c7b0f3928a5b1f33f
Ilya Solonetskiy <mr.king101@ukr.net>
2024-04-10 00:12:51 +0300

    convertation Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,18 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const sourceObject = sourceString
+    .split(';')
+    .filter((style) => style.trim().length > 0)
+    .reduce((acc, current) => {
+      const [property, value] = current.split(':');
+
+      acc[property.trim()] = value.trim();
+
+      return acc;
+    }, {});
+
+  return sourceObject;
 }
 
 module.exports = convertToObject;

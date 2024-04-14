9d6a35d73a3663f343604308538c778b34dd710b
Vadym Valchak <vadym.valchak@gmail.com>
2024-04-14 14:42:51 +0300

    Added solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssObject = {};
+  const cssString = sourceString.replace(/;\s*;/g, '').trim().split(';');
+
+  cssString.pop();
+
+  for (const row of cssString) {
+    let [key, value] = row.split(':');
+
+    key = key.trim();
+    value = value.replace(/[\r\n\t]/gm, '').trim();
+
+    cssObject[key] = value;
+  }
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

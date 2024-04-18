86de03944b3e5800a033aa8e236a08396c4d0871
MarkGodnyi <libertyinprogresslife@gmail.com>
2024-04-18 15:24:13 +0200

    implemented convert to object function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const IS_A_LETTER = /[a-z]/i;
+  const styles = sourceString
+    .split(';')
+    .filter((style) => style.search(IS_A_LETTER) !== -1)
+    .map((style) => {
+      const param = style.split(':').map((val) => val.trim());
+      const [key, value] = param;
+
+      return { [key]: value };
+    })
+    .reduce(
+      (accumulator, currentObj) => ({ ...accumulator, ...currentObj }),
+      {},
+    );
+
+  return styles;
 }
 
 module.exports = convertToObject;

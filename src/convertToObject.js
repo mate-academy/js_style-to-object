0137f09abc615e9f3a736e50a231157b5abfc4ae
mikezhylka <115790814+mikezhylka@users.noreply.github.com>
2024-04-20 13:24:13 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const convertedObject = {};
+  const lines = sourceString
+    .split('')
+    .filter((val) => val !== '\n')
+    .join('')
+    .split(';');
+
+  lines.forEach((word) => {
+    const cleanWord = word.trimStart().trimEnd();
+    const dividedLine = cleanWord.split(':');
+
+    if (typeof dividedLine[1] !== 'undefined') {
+      const key = dividedLine[0].trimStart().trimEnd();
+      let value = dividedLine[1].trimStart().trimEnd();
+
+      if (value.includes(',')) {
+        value = value.split(',').join(',\n');
+      }
+
+      convertedObject[key] = value;
+    }
+  });
+
+  return convertedObject;
 }
 
 module.exports = convertToObject;

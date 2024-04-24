ebc7ca654ac405c6191e00cee224f79a5d3cf9a5
Oksana Pokhvalenko <oksanapokhvalenko@gmail.com>
2024-04-24 18:54:39 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,19 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  const stylesArray = stylesString.split(';').filter(Boolean);
+  // Split by semicolons and remove empty elements
+  const result = {};
+
+  stylesArray.forEach((style) => {
+    const [property, value] = style.split(':').map((part) => part.trim());
+
+    // Split each style by colon and trim property and value
+    result[property] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

dfa142f3713a5c888ad81ebd943df39cf68f6967
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-04-30 14:28:39 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,30 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+const breakingTheRuleIntoFragments = (rule) => {
+  const ruleTrim = rule.trim();
+  const colon = ruleTrim.indexOf(':');
+  const property = ruleTrim.slice(0, colon).trim();
+  const value = ruleTrim.slice(colon + 1).trim();
+
+  return [property, value];
+};
+
+const convertToObject = (sourceString) =>
+  Object.fromEntries(
+    sourceString
+      .trim()
+      .split(';')
+      .filter((rule) => {
+        const [property, value] = breakingTheRuleIntoFragments(rule);
+
+        if (property && value) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      .map((rule) => breakingTheRuleIntoFragments(rule)),
+  );
 
 module.exports = convertToObject;

d30d4e8064fec71654cbf93dd4bb49868eaf7893
Den <clixqqk@gmail.com>
2024-05-04 06:44:50 +0300

    qwerty

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const mas = sourceString.split(';');
+
+  const obj = {};
+
+  for (const m of mas) {
+    const [name, value] = m.trim().split(':');
+
+    if (name && value) {
+      obj[name.trim()] = value.trim();
+    }
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

0c766a88a72d05276f2e7548b014ec523652d9f9
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-05-04 10:00:52 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,42 +6,13 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
+  const result = sourceString.split(';').reduce((prev, item) => {
+    const declarations = item.split(':').map((key) => key.trim());
 
-  sourceString.split(';').forEach((item) => {
-    const declarations = item
-      .split(':')
-      .map((declaration) => deleteBreak(declaration));
+    prev[declarations[0]] = declarations[1];
 
-    if ((declarations[0], declarations[1])) {
-      const key = declarations[0];
-      const value = declarations[1];
-
-      result[key] = value;
-    }
-  });
-
-  function deleteBreak(item) {
-    let start = 0;
-    let end = 0;
-    const condition = 'abcdefghijklmnoprstuvwxyz0123456789-#%()';
-
-    for (let i = 0; i < item.length; i++) {
-      if (condition.includes(item[i].toLowerCase())) {
-        start = i;
-        break;
-      }
-    }
-
-    for (let i = item.length - 1; i >= 0; i--) {
-      if (condition.includes(item[i].toLowerCase())) {
-        end = i + 1;
-        break;
-      }
-    }
-
-    return item.slice(start, end);
-  }
+    return prev;
+  }, {});
 
   return result;
 }

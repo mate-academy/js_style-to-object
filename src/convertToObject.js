641d5e7eade6c3b0a4fd6cb93faaa97d79626aab
Pavlo <oksanakopalko@gmail.com>
2024-05-09 17:52:45 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrStyles = sourceString.split(';');
+
+  const objStyles = {};
+
+  for (const style of arrStyles) {
+    const nameStyle = style.split(':');
+
+    if (nameStyle[0].trim() !== '') {
+      objStyles[nameStyle[0].trim()] = nameStyle[1].trim();
+    }
+  }
+
+  return objStyles;
 }
 
 module.exports = convertToObject;

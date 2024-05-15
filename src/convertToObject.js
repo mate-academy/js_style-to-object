60686b2a0e3a3af4271ac94b6342a37f75a86fb5
Daniel Bajer <bagietacyganeta@gmail.com>
2024-05-15 12:52:09 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylish = sourceString
+    .split(';')
+    .filter((things) => things.trim() !== '');
+
+  const result = {};
+
+  for (const thing of stylish) {
+    const [property, value] = thing.split(':').map((part) => part.trim());
+
+    result[property] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

3b003bed6db580345c6f27d096ff0b45a85bca02
Sasha Stopochkin <stopochkin23@gmail.com>
2024-05-16 19:58:05 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const splittedSource = sourceString
+    .replaceAll(' ', '')
+    .replaceAll('\n', '')
+    .split(';');
+
+  for (let i = 0; i < splittedSource.length; i++) {
+    const [key, value] = splittedSource[i].split(':');
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

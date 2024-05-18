c5cca5b0f2e740e1752614419062e42ff1a24c9f
Sasha Korkushko <krkshk000@gmail.com>
2024-05-18 16:37:38 +0300

    changed code style

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,19 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const splittedString = sourceString
+  // const result = sourceString
+  //   .split(';')
+  //   .map((str) => str.split(':'))
+  //   .map((str) => str.map((value) => value.trim()))
+  //   .map((str) => str.filter((value) => value !== ''))
+  //   .filter((str) => str.length !== 0)
+  //   .reduce((object, item) => {
+  //     object[item[0]] = item[1];
+
+  //     return object;
+  //   }, {});
+
+  // return result;
+  return sourceString
     .split(';')
-    .map((str) => str.split(':'))
-    .map((str) => str.map((value) => value.trim()))
-    .map((str) => str.filter((value) => value !== ''))
-    .filter((str) => str.length !== 0)
-    .reduce((object, item) => {
-      object[item[0]] = item[1];
-
-      return object;
-    }, {});
-
-  return splittedString;
+    .map((str) => str.split(':').map((value) => value.trim()))
+    .filter(([key, value]) => key && value)
+    .reduce((object, [key, value]) => ({ ...object, [key]: value }), {});
 }
 
 module.exports = convertToObject;

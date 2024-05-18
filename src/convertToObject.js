c2801027b1ebc77d20abe45a75a725d4d165174a
Sasha Korkushko <krkshk000@gmail.com>
2024-05-18 16:39:22 +0300

    deleted comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,19 +6,6 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // const result = sourceString
-  //   .split(';')
-  //   .map((str) => str.split(':'))
-  //   .map((str) => str.map((value) => value.trim()))
-  //   .map((str) => str.filter((value) => value !== ''))
-  //   .filter((str) => str.length !== 0)
-  //   .reduce((object, item) => {
-  //     object[item[0]] = item[1];
-
-  //     return object;
-  //   }, {});
-
-  // return result;
   return sourceString
     .split(';')
     .map((str) => str.split(':').map((value) => value.trim()))

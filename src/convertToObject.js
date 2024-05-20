8f5a4db9583973db85d0f9f7c9c02ed3a55ff2b9
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-05-20 11:29:02 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,48 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrSoursString = sourceString;
+  const result = {};
+
+  for (let i = 0; i < arrSoursString.length; i++) {
+    if (arrSoursString[i] === ':') {
+      result[`${suchKey(arrSoursString, i)}`] = such(arrSoursString, i);
+    }
+  }
+
+  return result;
+}
+
+function such(arr, index) {
+  let word = '';
+
+  for (let i = index; i < arr.length; i++) {
+    if (arr[i] === '\n') {
+      word = word + arr[i];
+    }
+
+    if ('abcdefghijklmnopqrstuvwxyz- ,1234567890!.#%()/'.includes(arr[i])) {
+      word = word + arr[i];
+    }
+
+    if (arr[i] === ';' || i === arr.length - 1) {
+      return word.trim();
+    }
+  }
+}
+
+function suchKey(arr, index) {
+  let word = '';
+
+  for (let i = index; i >= 0; i--) {
+    if ('abcdefghijklmnopqrstuvwxyz- ,1234567890!.#%()/'.includes(arr[i])) {
+      word = word + arr[i];
+    }
+
+    if (arr[i] === ';' || i === 0) {
+      return word.trim().split('').reverse().join('');
+    }
+  }
 }
 
 module.exports = convertToObject;

7d37d880cc026c1e7af8b3a621fcb0a6e1fd6f1a
Svitlana-Vitryk <vitriksvitlana@gmail.com>
2024-05-21 07:40:46 +0300

    Style-to-object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const sourceObject = {};
+  const declarations = sourceString.split(';');
 
+  declarations.forEach((declaration) => {
+    const [property, value] = declaration.split(':');
+
+    if (property && value) {
+      const trimmedProperty = property.trim();
+      const trimmedValue = value.trim();
+
+      sourceObject[trimmedProperty] = trimmedValue;
+    }
+  });
+
+  return sourceObject;
+}
 module.exports = convertToObject;

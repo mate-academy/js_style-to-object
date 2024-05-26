c67774962478e915c29a626240510351a567d8da
Mian <5mystogan5@gmail.com>
2024-05-26 23:06:42 +0300

    Program implemented

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const separatedByLines = sourceString
+    .split(';')
+    .filter((line) => line.trim() !== '');
+  const stringObj = {};
+
+  for (const item of separatedByLines) {
+    const separetedItem = item.split(':');
+    const key = separetedItem[0].trim();
+    const value = separetedItem[1].trim();
+
+    stringObj[key] = value;
+  }
+
+  return stringObj;
 }
 
 module.exports = convertToObject;

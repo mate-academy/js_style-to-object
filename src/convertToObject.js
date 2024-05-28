1e0718948780fcc81796d246d2f8c08a11c1d9b2
Yaroslav Avramidi <passenger1177@gmail.com>
2024-05-28 14:33:39 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,26 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  // eslint-disable-next-line no-debugger
+  if (!sourceString) {
+    return {};
+  }
+
+  const styleToObject = {};
+  const partsOfExpresions = sourceString.split(';');
+
+  partsOfExpresions.forEach((part) => {
+    const index = part.indexOf(':');
+
+    const name = part.substring(0, index).trim();
+    const value = part.substring(index + 1).trim();
+
+    if (name && value) {
+      styleToObject[name] = value;
+    }
+  });
+
+  return styleToObject;
 }
 
 module.exports = convertToObject;

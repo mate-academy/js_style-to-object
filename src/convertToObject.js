9ba349250251ecd5f37797a5e36155a3642ee0d6
Yaroslav Avramidi <passenger1177@gmail.com>
2024-05-28 14:44:20 +0300

    add changes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,20 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-  // eslint-disable-next-line no-debugger
   if (!sourceString) {
     return {};
   }
 
   const styleToObject = {};
-  const partsOfExpresions = sourceString.split(';');
+  const styleDeclarations = sourceString.split(';');
 
-  partsOfExpresions.forEach((part) => {
-    const index = part.indexOf(':');
+  styleDeclarations.forEach((declaration) => {
+    const index = declaration.indexOf(':');
 
-    const name = part.substring(0, index).trim();
-    const value = part.substring(index + 1).trim();
+    const name = declaration.substring(0, index).trim();
+    const value = declaration.substring(index + 1).trim();
 
     if (name && value) {
       styleToObject[name] = value;

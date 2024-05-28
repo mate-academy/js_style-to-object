64547fa560ed33e57c476cf7f4980b365549b681
LitviVit <Litv_jn71@student.itstep.org>
2024-05-29 01:34:35 +0300

    task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item.trim() !== '')
+    .reduce((stylesObject, item) => {
+      const pair = item.split(':');
+
+      const key = pair[0].trim();
+      const value = pair[1].trim();
+
+      stylesObject[key] = value;
+
+      return stylesObject;
+    }, {});
 }
 
 module.exports = convertToObject;

9b4ae1349d04ec68af64a9bba859ee6d01e0739b
priemssonia <priemssonia@gmail.com>
2024-05-29 16:56:08 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,28 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceObject = {};
+
+  sourceString.split(';').forEach((line) => {
+    const trimmedLine = line.trim();
+
+    if (trimmedLine === '') {
+      return;
+    }
+
+    const colonIndex = trimmedLine.indexOf(':'); // Corrected this line
+
+    if (colonIndex === -1) {
+      return;
+    }
+
+    const property = trimmedLine.slice(0, colonIndex).trim();
+    const value = trimmedLine.slice(colonIndex + 1).trim();
+
+    sourceObject[property] = value;
+  });
+
+  return sourceObject;
 }
 
 module.exports = convertToObject;

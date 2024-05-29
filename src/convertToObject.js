42afae5876d65841c6c5377a224314daee522fe2
Hamid Faiza <hamid.faiza811@gmail.com>
2024-05-29 22:45:41 +0300

    task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssProperties = sourceString.split(';');
+  const cssCode = {};
+
+  cssProperties.forEach((property) => {
+    const propertyArr = property.split(':');
+
+    if (propertyArr[0].trim().length > 0) {
+      cssCode[propertyArr[0].trim()] = propertyArr[1].trim();
+    }
+  });
+
+  return cssCode;
 }
 
 module.exports = convertToObject;

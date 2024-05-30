045fcce5cb3b4d5348df7033a380c3c0168c6e37
Kasprukov <kasprukov99@gmail.com>
2024-05-30 14:07:05 +0300

    fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,16 +8,14 @@
 function convertToObject(sourceString) {
   const stylesObject = sourceString
     .split(';')
-    .map((styles) => styles.trim())
-    .filter((styles) => styles.length > 0)
     .map((styles) => styles.split(':'))
-    .reduce(
-      (object, styles) => ({
-        ...object,
-        [styles[0].trim()]: styles[1].trim(),
-      }),
-      {},
-    );
+    .reduce((object, [key, value]) => {
+      if (key && value !== undefined && value.trim() !== '') {
+        object[key.trim()] = value.trim();
+      }
+
+      return object;
+    }, {});
 
   return stylesObject;
 }

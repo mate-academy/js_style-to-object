7f2313e0a81e4bb2439a3a9e4ba6aeba3aecc8bd
Vitalii <shut.vitaliy.ol@gmail.com>
2024-05-30 17:58:08 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  let strToObject = sourceString
+    .trim()
+    .split(';')
+    .filter((str) => str.trim());
 
+  strToObject = strToObject.map((str) => {
+    return str
+      .trim()
+      .split(':')
+      .map((s) => s.trim());
+  });
+
+  strToObject = Object.fromEntries(strToObject);
+
+  for (const key in strToObject) {
+    if (strToObject[key] === undefined) {
+      delete strToObject[key];
+    }
+  }
+
+  return strToObject;
+}
 module.exports = convertToObject;

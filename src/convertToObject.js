830a7a9ca4ee8f83b0529e6ab249b4f6d24b330e
Stanislav Kushnir <florian.of.lorch@gmail.com>
2024-05-31 15:59:55 +0300

    made the following changes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,13 +6,15 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const sourceArray = sourceString.split(';');
+  const sourceArray = sourceString
+    .split(';')
+    .filter((property) => property.trim() !== '');
 
   const objectProperties = sourceArray.reduce((acc, property) => {
     const propertyArray = property.trim().split(':', 2);
     const [keyName, keyValue] = propertyArray;
 
-    if (keyValue) {
+    if (keyName && keyValue) {
       acc[keyName.trim()] = keyValue.trim();
     }
 

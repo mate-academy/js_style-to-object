e35bc35f926c2300f72e8174c9a40e5b8a68d25e
Maksym Hryvko <grivkomaksim70@gmail.com>
2024-05-31 07:59:36 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,22 @@
-'use strict';
-
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
 function convertToObject(sourceString) {
-  // write your code here
+  const filteredArr = sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item);
+
+  const newArr = filteredArr
+    .map((item) => item.split(':'))
+    .map((item) => item.map((subItem) => subItem.trim()));
+
+  const resultObject = newArr.reduce((obj, item) => {
+    const [key, value] = item;
+
+    obj[key] = value;
+
+    return obj;
+  }, {});
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

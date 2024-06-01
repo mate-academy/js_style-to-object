257f5b2745072dc3c2648b2e6cb1577b7277cfb0
Sa4ok <sasha.rusinka@gmail.com>
2024-06-01 23:22:11 +0300

    Sol

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,23 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(styles) {
+  const styleObject = {};
+
+  // Поділ  рядків крапками з комою
+  const declarations = styles.split(';');
+
+  declarations.forEach((declaration) => {
+    // Розділ кожну декларацію двокрапкою
+    const [property, ...values] = declaration.split(':');
+
+    // Перевірте на існ  власт, так і значн, і видал зайві пробіли
+    if (property && values.length > 0) {
+      styleObject[property.trim()] = values.join(':').trim();
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

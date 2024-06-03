1854afdf79ee97520e98054d261f0bff19b06544
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-03 20:56:50 +0300

    Add styles converter.

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const filteredStylesList = sourceString
+    .split(';')
+    .filter((style) => /\S/.test(style));
+  const resultObject = {};
+
+  for (const styleItem of filteredStylesList) {
+    const keyValue = styleItem.split(':').map((item) => item.trim());
+    const [property, value] = [...keyValue];
+
+    resultObject[property] = value;
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

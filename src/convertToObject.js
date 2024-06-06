02b090aa51bcea8739891298c3921569f32bd7ef
Rusłan Karaniewski <karane88vka@gmail.com>
2024-06-06 10:05:22 +0200

    my solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const trimText = (text) => text.trim();
+  const removedSpasesText = sourceString
+    .split(';')
+    .map((line) => trimText(line))
+    .filter((line) => line !== '')
+    .map((line) => line.split(':'));
+  const finalObject = removedSpasesText.reduce(
+    (result, currentLine) => ({
+      ...result,
+      [trimText(currentLine[0])]: trimText(currentLine[1]),
+    }),
+    {},
+  );
+
+  return finalObject;
 }
 
 module.exports = convertToObject;

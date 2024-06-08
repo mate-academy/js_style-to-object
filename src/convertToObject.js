d04d7390a486c51f17e838574795491eacb6c677
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-06-08 15:08:48 +0300

    ready to pull request

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const convertedObject = {};
+
+  const splited = sourceString.split(';');
+
+  splited.forEach((split) => {
+    if (split.trim()) {
+      const [property, value] = split.split(':');
+
+      convertedObject[property.trim()] = value.trim();
+    }
+  });
+
+  return convertedObject;
 }
 
 module.exports = convertToObject;

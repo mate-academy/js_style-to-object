b83512f09fddf106ce4abf185a6dd01e235b2e4b
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-10 16:16:49 +0300

    [Feature] created styles converter

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArray = sourceString.split(';');
+
+  const stylesObj = stylesArray.reduce((acc, style) => {
+    const [key, value] = style.split(':');
+
+    if (!key || !value) {
+      return acc;
+    }
+
+    return { ...acc, [key.trim()]: value.trim() };
+  }, {});
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

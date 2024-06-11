d36727259e63b470e83c8309ea55ee881b4f3eba
Olexander <v.h.olexander@gmail.com>
2024-06-11 14:40:00 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,36 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splitThis = sourceString.split(';');
+  const result = {};
+
+  for (const oneString of splitThis) {
+    let word = '';
+    let wordTwo = '';
+    let fazeTwo = false;
+
+    for (const symbol of oneString) {
+      if (symbol.toLowerCase() !== symbol.toUpperCase() || symbol === '-') {
+        if (fazeTwo !== true) {
+          word += symbol;
+        }
+      }
+
+      if (fazeTwo === true) {
+        wordTwo += symbol;
+      }
+
+      if (symbol === ':') {
+        fazeTwo = true;
+      }
+    }
+
+    if (word.length !== 0) {
+      result[word] = wordTwo.trim();
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

6408777b41c5935913334e96d665702657fd713f
Iura Osadchuk <iuriiosad@gmail.com>
2024-06-11 11:57:20 +0200

    commit task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylePairs = sourceString
+    .split(';')
+    .filter((pair) => pair.trim() !== '');
+
+  const stylesObject = {};
+
+  stylePairs.forEach((pair) => {
+    const [key, value] = pair.split(':').map((part) => part.trim());
+
+    stylesObject[key] = value;
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

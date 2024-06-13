ee25af0a6a7ef81e75c8ff24450ea69225b39137
yursprava <yursprava@gmail.com>
2024-06-13 00:15:40 -0400

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const mainObj = {};
+
+  const workArr = sourceString.split(';');
+
+  const cleanArr = workArr
+    .map(function (item) {
+      const result = item.replace(/\n/g, '').trim();
+
+      return result;
+    })
+    .filter((item) => item !== '');
+
+  for (const pair of cleanArr) {
+    const index = pair.indexOf(':');
+
+    if (index > -1) {
+      const key = pair.slice(0, index);
+      const trimKey = key.trim();
+      const value = pair.slice(index + 1);
+      const trimValue = value.trim();
+
+      mainObj[trimKey] = trimValue;
+    }
+  }
+
+  return mainObj;
 }
 
 module.exports = convertToObject;

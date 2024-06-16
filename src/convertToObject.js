a1f2d58d380ef9f5fea03ff76834e946e701f647
luter <newgglutt@gmail.com>
2024-06-16 16:51:39 +0300

    nnn

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const styles = sourceString
+    .split(';')
+    .filter((style) => style.trim().length > 0);
+  const styleOBJ = {};
 
+  styles.forEach((style) => {
+    const [key, value] = style.split(':').map((part) => part.trim());
+
+    if (key && value) {
+      styleOBJ[key] = value;
+    }
+  });
+
+  return styleOBJ;
+}
 module.exports = convertToObject;

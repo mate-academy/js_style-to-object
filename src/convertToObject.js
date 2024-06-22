7399caad8febf938bfa77341261bc64c48a3d721
HunchakAndrii <andreygunchak1@gmail.com>
2024-06-22 17:08:01 +0300

    make function convertToObjecrt

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,23 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const parts = sourceString.split(';');
+  const result = {};
+
+  for (const part of parts) {
+    const piece = part.trim().split(':');
+
+    if (piece.length === 2) {
+      const key = piece[0].trim();
+      const value = piece[1].trim();
+
+      result[key] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

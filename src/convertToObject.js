699b1bc0912d71dff108448b6059a2d663b80b8b
Kateryna <moskkat40@gmail.com>
2024-06-26 21:39:00 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,28 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const getDeclarationAsStrings = sourceString
+    .split(';')
+    .map((rawDeclaration) => rawDeclaration.trim());
+  const getArrayOfDeclarationEntries = getDeclarationAsStrings.map(
+    (preparedDeclaration) =>
+      preparedDeclaration
+        .split(':')
+        .map((preparedDeclarationToTrim) => preparedDeclarationToTrim.trim()),
+  );
+
+  return getArrayOfDeclarationEntries.reduce((acc, declarationEntry) => {
+    if (declarationEntry.length === 1) {
+      return acc;
+    }
+
+    return {
+      ...acc,
+      [declarationEntry[0]]: declarationEntry[1],
+    };
+  }, {});
 }
 
 module.exports = convertToObject;

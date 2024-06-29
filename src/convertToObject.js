892df5ca6984fbaf3ba93be35dea9a916e6ee031
Olena Dobrova <dobrovaolena123@gmail.com>
2024-06-29 20:09:35 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssPropertiesAndValues = sourceString.split(';');
+  const result = {};
+
+  cssPropertiesAndValues.forEach((propertyValuePair) => {
+    const trimmedItem = propertyValuePair.trim();
+
+    if (trimmedItem.length < 4) {
+      return;
+    }
+
+    const [key, value] = trimmedItem.split(':');
+
+    if (key && value) {
+      result[key.trim()] = value.trim();
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

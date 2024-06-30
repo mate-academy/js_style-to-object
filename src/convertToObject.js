f5e47074c09363fca35aba0341cb3cbb352035b3
Vladislav <njiuyfqqq123@gmail.com>
2024-06-30 15:12:45 +0300

    successfully

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const splittedArr = sourceString.split(';');
+
+  for (let i = 0; i < splittedArr.length; i++) {
+    const style = splittedArr[i];
+
+    if (style) {
+      const colonIndex = style.indexOf(':');
+
+      if (colonIndex > -1) {
+        const prop = style.slice(0, colonIndex).trim();
+        const value = style.slice(colonIndex + 1).trim();
+
+        result[prop] = value;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

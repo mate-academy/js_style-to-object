18d3c8bccdb524d814d32395fbc76cfa389ea199
haduigon <haduigon@gmail.com>
2024-07-01 18:01:42 +0300

    clear the task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -15,19 +15,6 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const final = sourceString
-    .split(';')
-    .map(string => string.split(':'))
-    .map(string => string.map(string2 => string2.trim()))
-    .filter(string => string.length > 1)
-    .reduce((a, x) => {
-      a[x[0]] = x[1];
-
-      return a;
-    }, {}
-    );
-
-  return final;
 }
 
 module.exports = convertToObject;

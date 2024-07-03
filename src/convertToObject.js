6e498ce2bd6c75018ada94a59924990040d64f2e
Vadym Babisov <vadim.babisov@gmail.com>
2024-07-03 21:31:16 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,24 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const stylesLines = sourceString.split(';').map((item) => item.trim());
+
+  const convertedStylesToObject = {};
+
+  stylesLines.forEach((line) => {
+    const divideIndex = line.indexOf(':');
+
+    if (divideIndex !== -1) {
+      const keys = line.slice(0, divideIndex).trim();
+      const values = line.slice(divideIndex + 1).trim();
+
+      if (keys && values) {
+        convertedStylesToObject[keys] = values;
+      }
+    }
+  });
+
+  return convertedStylesToObject;
 }
 
 module.exports = convertToObject;

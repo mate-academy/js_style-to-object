d0f676174a1c554bb45761e1144510ad72db3a57
Victor Komara <vitioklss@gmail.com>
2024-07-03 12:38:28 +0300

    Second Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,11 +8,20 @@
 function convertToObject(sourceString) {
   const objStyles = {};
 
-  sourceString.split(';').map((style) => {
-    const arrStyle = style.split(':').map((el) => el.trim());
+  sourceString
+    .split(';')
+    .filter((el) => el.length > 0)
+    .forEach((style) => {
+      const arrStyle = style.split(':').map((el) => el.trim());
 
-    objStyles[arrStyle[0]] = arrStyle[1];
-  });
+      if (
+        arrStyle.length === 2 &&
+        arrStyle[0].length > 0 &&
+        arrStyle[1].length
+      ) {
+        objStyles[arrStyle[0]] = arrStyle[1];
+      }
+    });
 
   return objStyles;
 }

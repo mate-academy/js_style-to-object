d934143a0153f776d1ad81fdd1bc682a1661cfb3
Daniel-spec-dell <fvgdanil@gmail.com>
2024-07-03 19:56:02 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,20 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const splittedString = sourceString
+    .split(';')
+    .map((item) => item.split(':'))
+    .map((item) => item.map((value) => value.trim()))
+    .reduce((object, itemObj) => {
+      object[itemObj[0]] = itemObj[1];
+
+      return object;
+    }, {});
+
+  return splittedString;
 }
 
 module.exports = convertToObject;

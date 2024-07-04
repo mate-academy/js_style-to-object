1c19b395fffa0ad793c6ccabff84ceda4f68803c
Vadym Babisov <vadim.babisov@gmail.com>
2024-07-04 12:43:30 +0300

    fixed code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,20 +9,15 @@ function convertToObject(sourceString) {
   // write your code here
   const stylesLines = sourceString.split(';').map((item) => item.trim());
 
-  const cssObject = {};
+  const cssObject = stylesLines.reduce((acc, line) => {
+    const [keys, values] = line.split(':');
 
-  stylesLines.forEach((line) => {
-    const divideIndex = line.indexOf(':');
-
-    if (divideIndex !== -1) {
-      const keys = line.slice(0, divideIndex).trim();
-      const values = line.slice(divideIndex + 1).trim();
-
-      if (keys && values) {
-        cssObject[keys] = values;
-      }
+    if (keys && values) {
+      acc[keys.trim()] = values.trim();
     }
-  });
+
+    return acc;
+  }, {});
 
   return cssObject;
 }

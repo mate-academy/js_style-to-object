13da65ee37068421e980869e7934a40237ce7a31
Nataliia <gzodchie@gmail.com>
2024-07-07 01:11:20 +0300

    add convert to object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const cleanedString = sourceString.replace(/;\s*;/g, ';');
+  const regex = /\s*([^:\s]+)\s*:\s*([^;]+)\s*;?/g;
+  let match;
+
+  while ((match = regex.exec(cleanedString)) !== null) {
+    const key = match[1].includes(';', 0)
+      ? match[1].trim().slice(1)
+      : match[1].trim();
+    const value = match[2].includes(';', 0)
+      ? match[2].trim().slice(1)
+      : match[2].trim();
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

080effff0963b3202502b2cc5d80e4707d0c6eaf
Nataliia <gzodchie@gmail.com>
2024-07-07 01:53:22 +0300

    change convertToObject solution with map() and reduce() methods

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,23 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
-  const cleanedString = sourceString.replace(/;\s*;/g, ';');
-  const regex = /\s*([^:\s]+)\s*:\s*([^;]+)\s*;?/g;
-  let match;
+  return sourceString
+    .split(';')
+    .map((entry) => entry.trim())
+    .filter((entry) => entry)
+    .reduce((cssProperties, entry) => {
+      const [key, value] = entry.split(':').map((str) => str.trim());
 
-  while ((match = regex.exec(cleanedString)) !== null) {
-    const key = match[1].includes(';', 0)
-      ? match[1].trim().slice(1)
-      : match[1].trim();
-    const value = match[2].includes(';', 0)
-      ? match[2].trim().slice(1)
-      : match[2].trim();
+      if (key && value) {
+        cssProperties[key] = value;
+      }
 
-    result[key] = value;
-  }
-
-  return result;
+      return cssProperties;
+    }, {});
 }
 
 module.exports = convertToObject;

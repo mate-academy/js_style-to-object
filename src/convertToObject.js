ff938e760459a4ca8bf7906141f0093b3a8084c1
SerhiiSemennikov <semennik@hotmail.com>
2024-07-08 16:42:14 +0200

    new

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultObject = {};
+  let key = '';
+  let value = '';
+
+  const splitAllstring = sourceString.split(';');
+
+  for (const block of splitAllstring) {
+    const splitBlock = block.split(':');
+
+    key = splitBlock[0];
+    value = splitBlock[1];
+
+    if (value === undefined) {
+      continue;
+    }
+    resultObject[key.trim()] = value.trim();
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

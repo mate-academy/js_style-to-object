b49c2c4073fa7df5f4b4c90d3e1cf83d91973239
Your Name <ruslankurman13@gmail.com>
2024-07-08 19:02:26 +0300

    task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,44 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splitArray = sourceString.trim().split(';');
+  const object = {};
+
+  for (const item of splitArray) {
+    if (item.trim().length > 0) {
+      const resultItemSplit = item.split(':');
+      const key = resultItemSplit[0].trim();
+      const value = resultItemSplit[1].trim();
+
+      object[key] = value;
+    }
+  }
+
+  return object;
 }
 
 module.exports = convertToObject;
+// window.addEventListener('load', function () {
+//   function convertToObject(sourceString) {
+//     let stringWithoutNewLines = complexStylesString.replace(/\n/g, '');
+//     let splitArray = stringWithoutNewLines.trim().split(';');
+//     let object = {};
+//     for (let item of splitArray) {
+//       if (item.length > 0) {
+
+//         let resultItemSplit = item.split(':');
+
+//         if (resultItemSplit[0] && resultItemSplit[1]) {
+//           let key = resultItemSplit[0].trim();
+//           let value = resultItemSplit[1].trim();
+
+//           object[key] = value;
+//         }
+
+//       }
+//     }
+
+//     console.log(object);
+//   }
+//   convertToObject(complexStylesString)
+// })

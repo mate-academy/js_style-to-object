d094d9646d8d863a806e1a527554e2aae9f29d67
Pavlo Chudin <maks.chudin567@gmail.com>
2024-07-11 00:52:38 +0300

    'objectStyles'

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,22 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+
+  const styleArr = sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .filter((style) => style !== '');
+
+  styleArr.forEach((item) => {
+    const [property, value] = item.split(':').map((style) => style.trim());
+
+    styleObject[property] = value;
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

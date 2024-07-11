7a981abab88617379353602f3d118f680e04aad2
Kontroler444 <kontroler444@gmail.com>
2024-07-11 17:06:47 +0300

    implement fuction that takes a string with styles and returns an object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,36 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString.length) {
+    return {};
+  }
+
+  const resultObject = {};
+
+  const parseArray = sourceString.split(';');
+
+  // const filteredArray = partsOfString.filter((row) => row.includes(':'));
+
+  for (let element of parseArray) {
+    element = element.trim();
+
+    if (element.length) {
+      let [title, value] = element.split(':');
+
+      title = title.trim();
+
+      value = value.trim();
+
+      if (value.endsWith(';')) {
+        value = value.slice(0, -1);
+      }
+
+      value = value.trim();
+      resultObject[title] = value;
+    }
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

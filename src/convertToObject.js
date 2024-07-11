17166821994fa298493891c15168c7a2f29662ef
Katya <katya.stelmakhova@gmail.com>
2024-07-11 20:07:00 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultObject = {};
+  const stringModified = sourceString
+    .split(/;\s*/)
+    .filter((item) => item.trim() !== '');
+
+  stringModified.forEach((item) => {
+    const index = item.indexOf(':');
+
+    if (index !== -1) {
+      const objectKey = item.slice(0, index).trim();
+      const objectValue = item.slice(index + 1).trim();
+
+      if (resultObject[objectKey]) {
+        resultObject[objectKey] += ', ' + objectValue;
+      } else {
+        resultObject[objectKey] = objectValue;
+      }
+    }
+  });
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

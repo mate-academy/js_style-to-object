b38ad80bcdd0a956757aa1fb7e16391be79849ad
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-07-12 16:18:12 +0200

    task_done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splitArray = sourceString.trim().split(';');
+  const object = {};
+
+  for (const item of splitArray) {
+    if (item.trim().length > 0) {
+      const resultItemSplit = item.split(':');
+      const key = resultItemSplit[0].trim();
+      const value = resultItemSplit[1].trim();
+
+      object[key] = value;
+    }
+  }
+
+  return object;
 }
 
 module.exports = convertToObject;

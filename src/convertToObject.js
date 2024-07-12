582904d0f2ab09f7fdaf8ca5cbbcc0b7b92ecc7f
Roman Bekh <coldshein@gmail.com>
2024-07-12 12:49:51 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,21 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   // write your code here
+  const stylesArray = sourceString
+    .split(';')
+    .map((item) => item.split(':').map((part) => part.trim()))
+    .filter((item) => item.length === 2 && item[0] && item[1]);
+
+  const stylesObject = {};
+
+  stylesArray.forEach(([key, value]) => {
+    stylesObject[key] = value;
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

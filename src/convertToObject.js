0b90e7e2afa4ac688fb2a64fb68e6e5dccb49b87
Kate Marchuk <pion3219@gmail.com>
2024-07-12 16:48:01 +0300

    Added solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return (
+    sourceString
+      // Breaks sourceString by semicolon into separate stings
+      .split(';')
+
+      // Breaks line into property and value and removes extra space charachters
+      .map((el) => el.split(':').map((x) => x.trim()))
+
+      // Leaves only valid elements
+      .filter((el) => el.length === 2)
+
+      // Generate object from the
+      .reduce((acc, [item, value]) => ({ ...acc, [item]: value }), {})
+  );
 }
 
 module.exports = convertToObject;

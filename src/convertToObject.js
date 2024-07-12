72c34fffbdeb917293365dbcd9f7c9cefbdafbe6
Anastasiia Tatarchenko <neveykina.nastya@gmail.com>
2024-07-12 19:06:30 +0200

    fixed errors in the code

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,19 +5,16 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
   const styleDeclarations = sourceString.trim().split(';');
   const styleObject = {};
 
-  for (const item of styleDeclarations) {
-    if (item.trim().length > 0) {
-      const resultItemSplit = item.split(':');
-      const key = resultItemSplit[0].trim();
-      const value = resultItemSplit[1].trim();
+  styleDeclarations.forEach((element) => {
+    const [key, value] = element.split(':').map((item) => item.trim());
 
-      styleObject[key] = value;
-    }
-  }
+    styleObject[key] = value;
+  });
 
   return styleObject;
 }

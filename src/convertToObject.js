e5783da5cbc56f15375b4e757cdc2a05c94e71f7
Kate Marchuk <pion3219@gmail.com>
2024-07-12 20:13:43 +0300

    removed comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,31 +8,19 @@
  * @return {Object} CSS code converted to keys and values.
  */
 function convertToObject(sourceString) {
-  return (
-    sourceString
-      // Handle cases where the string might start with whitespace or semicolons
-      .trim()
-      // Breaks sourceString by semicolon into separate stings
-      .split(';')
+  return sourceString
+    .trim()
+    .split(';')
 
-      // Breaks line into property and value and removes extra space charachters
-      .map((line) => line.split(':').map((el) => el.trim()))
+    .map((line) => line.split(':').map((el) => el.trim()))
 
-      // Leaves only valid elements
-      .filter(
-        (el) =>
-          el.length === 2 &&
-          // Robust check that both elements are not empty strings
-          el.every((element) => element !== ''),
-      )
+    .filter((el) => el.length === 2 && el.every((element) => element))
 
-      // Generate object from the array
-      .reduce((acc, [item, value]) => {
-        acc[item] = value;
+    .reduce((acc, [item, value]) => {
+      acc[item] = value;
 
-        return acc;
-      }, {})
-  );
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

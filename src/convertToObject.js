bc3723cf81f894ff2296d767fed157e1c527ac38
Yuliia Kosenchuk <yuliia.kosenchuk@gmail.com>
2024-07-13 21:37:05 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,27 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const cssObject = sourceString
+    .split(';')
+    .map((declaration) => declaration.trim())
+    .filter((declaration) => declaration !== '')
+    .map((declaration) => {
+      const propertyValuePair = declaration.split(':');
+
+      propertyValuePair[0] = propertyValuePair[0].trim();
+      propertyValuePair[1] = propertyValuePair[1].trim();
+
+      return propertyValuePair;
+    })
+    .reduce((accumulator, propertyValuePair) => {
+      accumulator[propertyValuePair[0]] = propertyValuePair[1];
+
+      return accumulator;
+    }, {});
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

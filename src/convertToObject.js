1017e412bb9830cc118a5472a30a3466ee523cc2
Olena Nikishyna <lyashenko.ol.yur@gmail.com>
2024-07-14 20:55:07 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString.length === 0) {
+    return {};
+  }
+
+  const result = {};
+  const strings = sourceString.split(';').map((str) => str.trim());
+
+  strings.forEach((string) => {
+    const [key, ...values] = string.split(':');
+    const value = values.join('').trim();
+    const trimmedKey = key.trim();
+
+    if (trimmedKey && value) {
+      result[trimmedKey] = value;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

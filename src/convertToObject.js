07c79fd2d9ac86351d3074565ef65b83f22c755f
Olga Brendak <olga.brendak.dg@gmail.com>
2024-07-15 22:14:53 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString.length === 0) {
+    return {};
+  }
+
+  const GROUP_STYLE = sourceString.trim().split(';');
+  const GET_CLEAN_STYLE = GROUP_STYLE.map((pair) => pair.trim()).filter(
+    (style) => style !== '',
+  );
+
+  const SEPARATION_STYLE = GET_CLEAN_STYLE.map((pair) => {
+    return pair.trim().split(':');
+  });
+  const GET_STYLE_IN_OBJECT = SEPARATION_STYLE.reduce((acc, style) => {
+    acc[style[0].trim()] = style[1].trim();
+
+    return acc;
+  }, {});
+
+  return GET_STYLE_IN_OBJECT;
 }
 
 module.exports = convertToObject;

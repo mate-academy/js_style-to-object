4413be3b80424feb1e04116124328f21a354bcfc
Vadym Urbanovych <vadymurbanovych@example.com>
2024-07-15 23:19:22 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString.split(';').reduce((resultObj, segment) => {
+    const cleanSegment = segment.trim();
+
+    if (!cleanSegment) {
+      return resultObj;
+    }
+
+    const [key, data] = cleanSegment.split(':').map((part) => part.trim());
+
+    if (key && data) {
+      resultObj[key] = data;
+    }
+
+    return resultObj;
+  }, {});
 }
 
 module.exports = convertToObject;

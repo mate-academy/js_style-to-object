0ad90cb820c0f752da5bf8769b8cbc5f0baac485
Aleksandr Shramchenko <mostwar1988@gmail.com>
2024-07-16 14:52:22 +0300

    style to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = sourceString
+    .split(';')
+    .map((item) => item.split(':'))
+    .map((item) => item.map((value) => value.trim()))
+    .map((item) => item.filter((value) => value !== ''))
+    .filter((item) => item.length !== 0)
+    .reduce((object, item) => {
+      object[item[0]] = item[1];
+
+      return object;
+    }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

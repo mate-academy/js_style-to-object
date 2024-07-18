0e4b26719041eed156393e6e13d39926199158df
Anya Laban <laban.anya.a@gmail.com>
2024-07-18 17:45:32 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString.length < 1) {
+    return {};
+  }
+
+  const sourceElements = sourceString.split(';');
+
+  const sourceObject = sourceElements
+    .map((el) => el.trim())
+    .filter((el) => el.length > 0)
+    .reduce((obj, el) => {
+      const key = el.slice(0, el.indexOf(':')).trim();
+      const value = `${el.slice(el.indexOf(':') + 1, el.length).trim()}`;
+
+      return {
+        ...obj,
+        [key]: value,
+      };
+    }, {});
+
+  return sourceObject;
 }
 
 module.exports = convertToObject;

3c8496d1dd5b05fa288fed63fd135575b1584949
Max <max@energyhill.com>
2024-07-19 15:16:26 -0400

    v 1.0

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,44 +1,5 @@
 'use strict';
 
-const sourceString = `
-  background-color:      #fff;
--webkit-border-radius: 5px;
-  border-radius     : 5px;
-  border: 1px solid #e8e8e8;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  clear   : both       ;
-  cursor: pointer;
-  float: left;
-  font-family: inherit;
-      font-size: 14px;
-  font-weight: 400;
-  height: 42px;
-  line-height:    40px;
-  outline: 0;
-  padding-left    : 18px;
-  padding-right: 30px;
-  ;
-
-  ;
-  position: relative;
-
-
-  text-align: left !important;
-  -webkit-transition: all .2s ease-in-out;
-  transition: all .2s ease-in-out;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-
-  box-shadow:
-          inset 0 -3em 3em rgb(0 200 0 / 30%),
-          0 0 0 2px white,
-          0.3em 0.3em 1em rgb(200 0 0 / 60%);
-  white-space: nowrap;
-  width: auto;
-`;
-
 /**
  * @param {string} sourceString
  *
@@ -47,7 +8,7 @@ const sourceString = `
 function convertToObject(sourceString) {
   const cssObject = {};
 
-  const noSpacesArray = sourceString.replace(/\n|\r/g, '').split(';');
+  const noSpacesArray = sourceString.split(';');
   const arrayOfArrays = noSpacesArray
     .map((element) => element.split(':'))
     .filter((element) => element.length === 2)
@@ -59,9 +20,5 @@ function convertToObject(sourceString) {
 
   return cssObject;
 }
-
-const result = convertToObject(sourceString);
-
-console.log(result);
 
 module.exports = convertToObject;

b7311c6b953dd3f827285947c127ebbbb2b801e8
Vlados <butko9587@gmail.com>
2024-07-21 13:43:26 +0200

    New Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,30 +5,21 @@
  *
  * @return {object}n
  */
+
 function convertToObject(stylesString) {
-  // Создаем копию параметра, чтобы не изменять его
-  const modifiedStylesString = stylesString
-    .replace(/;+\s*$/, '') // Удаляет точки с запятой в конце строки
-    .trim();
+  // Создаем массив правил, фильтруем пустые строки и преобразуем их в объект
+  return stylesString
+    .split(/;\s*/) // Разделяем строку на правила
+    .filter((rule) => rule.trim()) // Фильтруем пустые строки
+    .map((rule) => {
+      const [property, value] = rule.split(/:\s*/);
 
-  const stylesObject = {};
-
-  // Разделяем строки на отдельные правила
-  const rules = modifiedStylesString.split(/;\s*/);
-
-  rules.forEach((rule) => {
-    // Пропускаем пустые строки
-    if (rule.trim() === '') {
-      return;
-    }
-
-    const [property, value] = rule.split(/:\s*/);
-
-    if (property && value) {
-      stylesObject[property.trim()] = value.trim();
-    }
-  });
-
-  return stylesObject;
+      return {
+        [property.trim()]: value.trim()
+      };
+    })
+    .reduce((acc, curr) => {
+      return Object.assign(acc, curr);
+    }, {});
 }
 module.exports = convertToObject;

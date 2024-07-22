93786433e9db16346db2994e31175eb73e81bc17
IlliaAstakhov <astakhovillia@gmail.com>
2024-07-22 19:40:06 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,29 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // First - clear string from other symbols
+  const tempString = sourceString.trim().replace(/;+\s*$/, '');
+  const cssList = tempString.split(';');
+  const cssObject = {};
+
+  cssList.forEach((style) => {
+    let [key, value] = style.split(':');
+
+    if (key && value) {
+      key = key.trim();
+      value = value.trim();
+
+      if (key && value) {
+        cssObject[key] = value;
+      }
+    }
+
+    return cssObject;
+  });
+
+  // console.log(cssObject);
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

e8f16ee0a06ed958aeb6530ddfc9f7dd2a00f399
Vasyl Mazurok <vasok1991@gmail.com>
2024-07-24 09:52:41 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arraySorceString = sourceString
+    .split(';')
+    .map((string) => string.trim().split(':'));
+
+  const filteredSorceString = arraySorceString
+    .filter((innerArr) => innerArr.length === 2)
+    .map((element) => element.map((item) => item.trim()));
+
+  // console.log(filteredSorceString);
+
+  const convertingObject = filteredSorceString.reduce((acc, arrayElement) => {
+    const [key, value] = arrayElement;
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return convertingObject;
+  // console.log(convertingObject);
 }
 
 module.exports = convertToObject;

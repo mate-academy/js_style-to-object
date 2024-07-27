28ccfc8607f431fda86099410d1dd4902d191924
Marian <getmoodyup@gmail.com>
2024-07-27 13:24:22 +0300

    npm i

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,37 @@
  *
  * @return {object}
  */
+'use strict';
+
+/**
+ * @param {string} sourceString
+ *
+ * @return {object}
+ */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+
+  sourceString.split('\n').forEach((s) => {
+    const str = s.trim();
+
+    if (str === '') {
+      return;
+    }
+
+    const colonIndex = str.indexOf(':');
+
+    const property = str.slice(0, colonIndex).trim();
+    const value = str.slice(colonIndex + 1).trim();
+
+    if (property && value) {
+      obj[property] = value;
+    }
+  });
+
+  return obj;
 }
+
+module.exports = convertToObject;
+
 
 module.exports = convertToObject;

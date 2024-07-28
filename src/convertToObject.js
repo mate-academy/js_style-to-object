1f9a00794e75c67889d00cc68a12ca772d8cdf78
Jakub <jakubfedorczyk@gmail.com>
2024-07-28 21:22:55 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const sourceStringTrim = sourceString.trim();
+  const tabFromSourceString = sourceStringTrim.split(';');
+
+  for (let i = 0; i < tabFromSourceString.length; i++) {
+    const tabProperty = tabFromSourceString[i].split(':');
+
+    if (tabProperty.length === 2) {
+      const key = tabProperty[0].trim();
+      const value = tabProperty[1].trim();
+
+      if (key && value) {
+        obj[key] = value;
+      }
+    }
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

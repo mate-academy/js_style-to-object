f29c380c473cfbb1885a8c7be1c7c052c055d7fa
Sasha <tgcsdbdgfc@gmail.com>
2024-07-30 21:19:53 +0300

    Solution2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,23 +6,23 @@
  * @return {object}
  */
 function convertToObject(stylesString) {
-  const lines = stylesString.trim().split('\n');
-  const cleanedLines = lines.filter((line) => line.trim() !== '');
+  const cleanedString = stylesString
+    .trim()
+    .split('\n')
+    .filter((line) => line.trim() !== '')
+    .join('\n');
 
-  const cleanedString = cleanedLines.join('\n');
   const declarations = cleanedString.split(';');
 
-  const styleObject = {};
-
-  declarations.forEach((declaration) => {
+  return declarations.reduce((styleObject, declaration) => {
     const [key, value] = declaration.split(':').map((part) => part.trim());
 
     if (key && value) {
       styleObject[key] = value;
     }
-  });
 
-  return styleObject;
+    return styleObject;
+  }, {});
 }
 
 module.exports = convertToObject;

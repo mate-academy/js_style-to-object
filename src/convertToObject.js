22f4db11f6956dc189e0c95791cf70cf53c20d89
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-31 00:17:03 +0200

    some changes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,34 +7,18 @@
  */
 function convertToObject(sourceString) {
   // make an array with strings of sourceString
-  const properties = [];
-
-  let temp = sourceString.trim() + ';';
-
-  while (temp.includes(';')) {
-    properties.push(temp.slice(0, temp.indexOf(';') + 1));
-    temp = temp.slice(temp.indexOf(';') + 1).trim();
-  }
+  const properties = sourceString.split(';').filter((str) => str !== '');
 
   // make an object with properties
   const propObj = {};
 
   properties.forEach((str) => {
-    const prop = str.slice(0, str.indexOf(':')).trim();
+    const prop = str
+      .trim()
+      .split(':')
+      .map((val) => val.trim());
 
-    const index = str.indexOf(';');
-
-    let val;
-
-    if (index !== -1) {
-      val = str.slice(str.indexOf(':') + 1, str.indexOf(';')).trim();
-    } else {
-      val = str.slice(str.indexOf(':') + 1).trim();
-    }
-
-    if (prop.length !== 0 && val.length !== 0) {
-      propObj[prop] = val;
-    }
+    propObj[prop[0]] = prop[1];
   });
 
   return propObj;

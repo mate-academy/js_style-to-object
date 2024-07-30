bfd6824d87557c275c6d289de3022fae48abb324
MOODDDII <71275442+MOODDDII@users.noreply.github.com>
2024-07-30 17:23:47 +0200

    Update convertToObject.js

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,13 +11,6 @@ function convertToObject(sourceString) {
   const parts = sourceString.split(';');
 
   parts.forEach((str) => {
-    const cleanStr = str.trim();
-    const colonIndex = cleanStr.indexOf(':');
-
-    if (colonIndex === -1) {
-      return;
-    }
-
     const [property, value] = cleanStr.split(':').map((item) => item.trim());
 
     if (property && value) {

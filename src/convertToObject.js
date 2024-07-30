b6f1305032f4bcbb147dc77ccc40da9113cb2189
Nila <nilamadhava05@gmail.com>
2024-07-30 14:16:04 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,24 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function ConvertToObject(cssString) {
+  // Инициализируем объект, в который будем добавлять стили
+  const obj = {};
+
+  // Регулярное выражение для поиска пар ключ-значение в CSS строке
+  const pattern = /([^:;]+)\s*:\s*([^;]+)/g;
+
+  // Ищем все вхождения пар ключ-значение в строке
+  let match;
+  const array = cssString.split(';');
+
+  for (const el of array) {
+    const arr = el.split(':');
+
+    obj[arr[0].trim()] = arr[1].trim();
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

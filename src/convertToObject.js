d98a5e28889eb0be8284475be98984e5695d4b64
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-30 18:27:12 +0200

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,38 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // make an array with strings of sourceString
+  const properties = [];
+
+  let temp = sourceString.trim() + ';';
+
+  while (temp.includes(';')) {
+    properties.push(temp.slice(0, temp.indexOf(';') + 1));
+    temp = temp.slice(temp.indexOf(';') + 1).trim();
+  }
+
+  // make an object with properties
+  const propObj = {};
+
+  properties.forEach((str) => {
+    const prop = str.slice(0, str.indexOf(':')).trim();
+
+    const index = str.indexOf(';');
+
+    let val;
+
+    if (index !== -1) {
+      val = str.slice(str.indexOf(':') + 1, str.indexOf(';')).trim();
+    } else {
+      val = str.slice(str.indexOf(':') + 1).trim();
+    }
+
+    if (prop.length !== 0 && val.length !== 0) {
+      propObj[prop] = val;
+    }
+  });
+
+  return propObj;
 }
 
 module.exports = convertToObject;

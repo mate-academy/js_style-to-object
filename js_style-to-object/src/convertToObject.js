f044252b153fce2232199daf597b96887f7c268f
Your Name <you@example.com>
2024-08-05 18:14:10 +0700

    feat: instantiate and return object

---
+++ js_style-to-object/src/convertToObject.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @param {string} sourceString
+ *
+ * @return {object}
+ */
+function convertToObject(sourceString) {
+  // an array of CSS properties and values
+  let declarationsArr = sourceString
+    .split(';')
+    .map((declaration) => {
+      return declaration.replace(/\s/g, '').replace(':', ': '); // erase whitespaces, then add one after ':'
+    })
+    .filter((e) => e); // We filter out empty array values
+
+  const newObj = {};
+
+  for (const e of declarationsArr) {
+    const tmp = e.split(': ');
+    const key = tmp[0];
+    const val = tmp[1];
+    newObj[key] = val;
+  }
+
+  return newObj;
+}
+
+const result = convertToObject(complexStylesString);
+
+return result;

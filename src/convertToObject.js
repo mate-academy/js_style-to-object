6cdce28470d61bb7bd92495f16b6f57f901f0500
Vlad Tkachuk <vlad.tkachuk.04@gmail.com>
2024-08-05 16:30:56 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // if the sourceString is empty return an empty object
+  if (!sourceString) {
+    return {};
+  }
+
+  // remove all tab and new line characters
+  // replaceAll doesn't work on this version of Node
+  const clearedString = sourceString.replace(/[\t\n]/g, '');
+  const stylesArr = clearedString.split(';');
+  const stylesObj = {};
+
+  stylesArr.forEach((style) => {
+    let [name, value] = style.split(':');
+
+    // name and value can be empty or undefined
+    if (name && value) {
+      name = name.trim();
+      value = value.trim();
+      // add a new line character where it is needed
+      value = value.replace(/,/g, ',\n');
+      stylesObj[name] = value;
+    }
+  });
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

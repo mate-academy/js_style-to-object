e390fcdb96edcc6e6e51671328c532ef02df22ee
Your Name <you@example.com>
2024-08-06 15:07:29 +0700

    fix: remove testing code, move comment up a line

--- js_style-to-object/src/convertToObject.js
+++ js_style-to-object/src/convertToObject.js
@@ -1,35 +1,24 @@
-'use strict';
-
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
-function convertToObject(sourceString) {
-  // an array of CSS properties and values
-  if (sourceString.length === 0) {
-    return {};
+class Animal {
+  constructor(health, name) {
+    this.health = 100;
+    this.name = name;
   }
-
-  let declarationsArr = sourceString
-    .split(';')
-    .map((declaration) => {
-      return declaration.replace(/\s/g, '').replace(':', ': '); // erase whitespaces, then add one after ':'
-    })
-    .filter((e) => e); // We filter out empty array values
-
-  const newObj = {};
-
-  for (const e of declarationsArr) {
-    const tmp = e.split(': ');
-    const key = tmp[0];
-    const val = tmp[1];
-    newObj[key] = val;
-  }
-
-  return newObj;
 }
 
-const result = convertToObject(complexStylesString);
+class Herbivore extends Animal {
+  constructor() {
+    this.hidden = false;
+  }
 
-return result;
+  hide() {
+    this.hidden = true;
+  }
+}
+
+class Carnivore extends Animal {
+  bite(animal) {
+    if (animal instanceof Herbivore && animal.hidden === false) {
+      animal.health -= 50;
+    }
+  }
+}
--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,7 +8,8 @@ function convertToObject(sourceString) {
   let declarationsArr = sourceString
     .split(';')
     .map((declaration) => {
-      return declaration.replace(/\r\n|\n|\r/gm, '', '').replace(':', ': '); // erase whitespaces, then add one after ':'
+      // erase whitespaces, then add one after ':'
+      return declaration.replace(/\r\n|\n|\r/gm, '', '').replace(':', ': ');
     })
     .filter((e) => e); // We filter out empty array values
 
@@ -24,41 +25,3 @@ function convertToObject(sourceString) {
   }
   return obj;
 }
-
-const complexStylesString = `
-  background-color:      #fff;
--webkit-border-radius: 5px;
-  border-radius     : 5px;
-  border: 1px solid #e8e8e8;
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  clear   : both       ;
-  cursor: pointer;
-  float: left;
-  font-family: inherit;
-      font-size: 14px;
-  font-weight: 400;
-  height: 42px;
-  line-height:    40px;
-  outline: 0;
-  padding-left    : 18px;
-  padding-right: 30px;
-  ;
-
-  ;
-  position: relative;
-
-
-  text-align: left !important;
-  -webkit-transition: all .2s ease-in-out;
-  transition: all .2s ease-in-out;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-
-
-  white-space: nowrap;
-  width: auto;
-`;
-
-console.log(convertToObject(complexStylesString));

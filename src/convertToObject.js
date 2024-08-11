368cdc5468b881d33bec5fac0ebac126ff71ca6d
yanatoto <yanas@bluewin.ch>
2024-08-11 13:11:26 +0200

    finished task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,18 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const arr = sourceString.split(' ');
+
+  for (let i = 0; i < arr.length; i += 2) {
+    result[arr[i]] = arr[i + 1];
+  }
+
+  return result;
 }
 
+// convertToObject(string);
+// console.log(convertToObject(string));
 module.exports = convertToObject;

793fab7e9285b061c01d403ae7fe201379bec908
yanatoto <yanas@bluewin.ch>
2024-08-11 13:36:35 +0200

    fixed mistakes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,16 +7,15 @@
  */
 
 function convertToObject(sourceString) {
-  const result = {};
-  const arr = sourceString.split(' ');
+  let result = {};
+  let arr = sourceString.replace(/[;:]/g, ',').split(',');
 
-  for (let i = 0; i < arr.length; i += 2) {
-    result[arr[i]] = arr[i + 1];
+  for (let i = 0; i < arr.length; i+= 2) {
+    result[arr[i]] = arr[(i + 1)];
   }
-
+    
   return result;
 }
 
-// convertToObject(string);
-// console.log(convertToObject(string));
+
 module.exports = convertToObject;

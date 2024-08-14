f1822da9ae70bd2106da160cb145546f207fc677
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-08-14 09:41:27 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const source = sourceString;
+  const splitedByASemicolon = source
+    .split(';')
+    .map((string) => string.trim())
+    .filter((string) => string.length > 0);
+  const arrOfArrsSplitedByAColon = [];
+
+  splitedByASemicolon.forEach((string) => {
+    arrOfArrsSplitedByAColon.push(string.split(':'));
+  });
+
+  const finishedArray = arrOfArrsSplitedByAColon.map((element) => {
+    return element.map((string) => string.trim());
+  });
+  const finishedObject = {};
+
+  finishedArray.map(([key, value]) => (finishedObject[key] = value));
+
+  return finishedObject;
 }
 
 module.exports = convertToObject;

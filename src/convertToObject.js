89ed9de6586459e5291fcbbe0709f12b9ef35122
Дикун Дмитрий Николаевич <dikun.dmitriy@ankor.org>
2024-08-15 10:11:22 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,47 +6,16 @@
  * @return {object}
  */
 const convertToObject = function (sourceString) {
-  const styleObject = {};
+  return sourceString
+    .trim()
+    .split(';')
+    .filter(Boolean)
+    .map((declaration) => declaration.split(':').map((part) => part.trim()))
+    .reduce((acc, [property, value]) => {
+      acc[property] = value;
 
-  const cleanSourceString = sourceString
-    .replace(/[\t\r]+/g, '')
-    .replace(/\s*;\s*/g, ';')
-    .replace(/\s*:\s*/g, ':')
-    .replace(/;\n/g, ';');
-
-  const declarationsArray = cleanSourceString.trim().split(';').filter(Boolean);
-
-  let buffer = '';
-  let insideMultilineValue = false;
-
-  declarationsArray.forEach(function (declarationItem) {
-    if (insideMultilineValue) {
-      buffer += declarationItem.trim();
-
-      if (!/\\$/.test(buffer)) {
-        const parts = buffer.split(':');
-
-        if (parts.length === 2) {
-          styleObject[parts[0].trim()] = parts[1].trim();
-        }
-        insideMultilineValue = false;
-        buffer = '';
-      }
-    } else {
-      const parts = declarationItem.split(':');
-
-      if (parts.length === 2) {
-        if (/\\$/.test(parts[1].trim())) {
-          buffer = declarationItem.trim();
-          insideMultilineValue = true;
-        } else {
-          styleObject[parts[0].trim()] = parts[1].trim();
-        }
-      }
-    }
-  });
-
-  return styleObject;
+      return acc;
+    }, {});
 };
 
 module.exports = convertToObject;

48b33f146ed676184f4d0e859b0d1d4158f75916
Jakub Bajkowski <k.bajkowski7@wp.pl>
2024-08-15 15:44:42 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString.trim()) {
+    return {};
+  }
+
+  return sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .filter((style) => style.length > 0)
+    .reduce((stylesObject, style) => {
+      const [property, value] = style.split(':').map((part) => part.trim());
+
+      if (property) {
+        stylesObject[property] = value || '';
+      }
+
+      return stylesObject;
+    }, {});
 }
 
 module.exports = convertToObject;

095fc1f5f501053ed9a189843d15410cfdbfb506
joaoguilherme <joaoguilhermehig@gmail.com>
2024-08-25 18:55:16 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssPropertiesDotComa = sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0);
+
+  const cssPropertiesDoubleDot = cssPropertiesDotComa
+    .map((item) => item.split(':'))
+    .map((splittedCssProperty) =>
+      // eslint-disable-next-line prettier/prettier
+      splittedCssProperty.map((propertyPart) => propertyPart.trim()));
+
+  const cssPropertiesObject = cssPropertiesDoubleDot.reduce(
+    (acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    },
+    {},
+  );
+
+  return cssPropertiesObject;
 }
 
 module.exports = convertToObject;

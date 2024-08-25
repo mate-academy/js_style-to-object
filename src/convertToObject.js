379e8ba3d4d331cea30882a27f54988dcf617bea
Artem-Brui <artembryj@gmail.com>
2024-08-25 16:38:00 +0200

    solved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,29 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const regex = /\n+|\t+/g;
+
+  sourceString
+    .replaceAll(regex, ' ')
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item !== '')
+    .map((item) => {
+      return item.split(':').map((x) => {
+        return x
+          .split(',')
+          .map((y, index) => {
+            return index === 0 ? y.trim() : `          ${y.trim()}`;
+          })
+          .join(',\n');
+      });
+    })
+    .forEach((element) => {
+      result[`${element[0]}`] = element[1];
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

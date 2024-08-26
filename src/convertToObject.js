fb9455fe7bef9f7e500b317c04dc6508c087ad9d
Ann Maksay <annaorlova.dn@gmail.com>
2024-08-26 16:23:41 +0300

    feat: implement style formatting

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,23 @@
  *
  * @return {object}
  */
+const SEMICOLON = ';';
+const COLON = ':';
+
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArray = sourceString.split(SEMICOLON);
+
+  const styleObject = stylesArray.reduce((temp, style) => {
+    const separateStyle = style.split(COLON).map((part) => part.trim());
+
+    if (separateStyle.length === 2) {
+      temp[separateStyle[0]] = separateStyle[1];
+    }
+
+    return temp;
+  }, {});
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

dac8fa325f780b71fb70b246d987b3a13dc201a3
Dmytro Ivasenko <dmtivasenko@gmail.com>
2024-08-27 21:55:42 +0700

    Convert to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,12 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .filter((item) => item.includes(':'))
+    .map((item) => item.split(':'))
+    .map(([property, value]) => [property.trim(), value.trim()])
+    .reduce((acc, [property, value]) => ({ ...acc, [property]: value }), {});
 }
 
 module.exports = convertToObject;

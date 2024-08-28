ae654db636b5da973f010765b1939a0da91d7ceb
Sasha Popovych <sashapopovych98@gmail.com>
2024-08-28 19:59:48 +0200

    test

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const valuesList = sourceString
+    .split(';')
+    .filter((el) => el.trim())
+    .map((e) => e.split(':').map((s) => s.trim()))
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    }, {});
+
+  return valuesList;
 }
 
 module.exports = convertToObject;

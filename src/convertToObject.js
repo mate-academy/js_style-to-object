a38ed28fca335a68063d017521f5dfaf0672a0b9
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-09-01 23:24:23 +0300

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const rules = {};
+
+  sourceString
+    .trim()
+    .split(';')
+    .forEach((rule) => {
+      const [key, value] = rule
+        .trim()
+        .split(':')
+        .map((part) => part.trim());
+
+      rules[key] = value;
+    });
+
+  return rules;
 }
 
 module.exports = convertToObject;

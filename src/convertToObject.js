624e5572c548a1934e98781966adaec3a16190ac
Kamil <kamilbykowski99@gmail.com>
2024-09-02 16:07:37 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,25 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const FORMAT_SOURCE = sourceString.split('\n').join('');
-  const STYLE_ARRAY = FORMAT_SOURCE.split(';');
+  const styleDeclarations = sourceString.trim().split(/\s*;\s*/);
 
-  const STYLE_OBJECT = {};
+  const styleObject = {};
 
-  const PAIRS_OF_STYLE = STYLE_ARRAY.map((item) => item.split(':'));
+  const styleArray = styleDeclarations.map((item) => item.split(':'));
 
-  PAIRS_OF_STYLE.forEach((item) => {
+  styleArray.map((item) => {
     if (item.length < 2) {
       return 0;
     }
 
-    const KEY = item[0].trim();
-    const VALUE = item[1].split(',').join(`,\n`).trim();
+    const key = item[0].trim();
+    const value = item[1].trim();
 
-    STYLE_OBJECT[KEY] = VALUE;
+    styleObject[key] = value;
   });
 
-  return STYLE_OBJECT;
+  return styleObject;
 }
 
 module.exports = convertToObject;

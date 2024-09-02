620390807ddab899a83df5dec175886a4719138e
Kamil <kamilbykowski99@gmail.com>
2024-09-02 11:48:22 +0200

    Convert Style to Object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const FORMAT_SOURCE = sourceString.split('\n').join('');
+  const STYLE_ARRAY = FORMAT_SOURCE.split(';');
+
+  const STYLE_OBJECT = {};
+
+  const PAIRS_OF_STYLE = STYLE_ARRAY.map((item) => item.split(':'));
+
+  PAIRS_OF_STYLE.forEach((item) => {
+    if (item.length < 2) {
+      return 0;
+    }
+
+    const KEY = item[0].trim();
+    const VALUE = item[1].split(',').join(`,\n`).trim();
+
+    STYLE_OBJECT[KEY] = VALUE;
+  });
+
+  return STYLE_OBJECT;
 }
 
 module.exports = convertToObject;

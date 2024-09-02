e33a50510c4c3fe14791c6d8ed501dade01e7267
Inesa Shtepa <khinessa@gmail.com>
2024-09-02 13:37:40 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,30 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const delEmptyLine = sourceString.replace(/\n/g, '');
+
+  const commandsArr = delEmptyLine.split(';');
+
+  const commandsArrFinal = commandsArr.filter((str) => str.trim());
+
+  const resultObject = {};
+
+  for (let i = 0; i < commandsArrFinal.length; i++) {
+    const commandArr = commandsArrFinal[i].split(':');
+
+    const key = commandArr[0].trim();
+    const value = commandArr[1].trim();
+
+    resultObject[key] = value.replace(/,/g, ',\n');
+  }
+
+  if (sourceString === '') {
+    return {};
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

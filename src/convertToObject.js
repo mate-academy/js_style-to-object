6c4c7f03b8c101f4b6cd58cbc64396d71b8938ac
JogXFJ4uDlJi3OBH <y45587844@gmail.com>
2024-09-04 10:57:13 +0300

    Update convertToObject.js

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+ const cleanedUpString = sourceString
+    .split('\n')
+    .join('')
+    .trim()
+    .split(';')
+    .map(el => el.split(':'));
+
+  const result = {};
+
+  cleanedUpString.forEach(element => {
+    if (element.length > 1) {
+      const [cssProperty, cssValue] = element;
+
+      const objectKey = cssProperty.trim();
+      const objectValue = cssValue.trim();
+
+      result[objectKey] = objectValue;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

8bc69dec027628c60e76d9d5e21011b1d7a3a297
Dmitry <kononenko.dima2001@gmail.com>
2024-09-05 15:14:36 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const trimmedStyles = sourceString
+    .replace(/\t/g, ' ')
+    .split(';')
+    .map((e) => e.trim());
+
+  const clearedStyles = trimmedStyles
+    .filter((e) => e)
+    .reduce((acc, item) => {
+      const splitted = item.split(':');
+
+      acc[splitted[0].trim()] = splitted[1].trim().replaceAll(', ', ',\n\t\t');
+
+      return acc;
+    }, {});
+
+  return clearedStyles;
 }
 
 module.exports = convertToObject;

379154690829fb6001fecc5ddbd71cdec15469dc
vikshiyan <miss.shiyan@gmail.com>
2024-09-08 23:16:54 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const sourcesArr = sourceString.split(';');
 
+  const resultObj = sourcesArr.reduce((acumulator, item) => {
+    const element = item.split(':');
+    const elementName = element[0]?.trim();
+    const elementValue = element[1]?.trim();
+
+    if (elementName && elementValue) {
+      acumulator[elementName] = elementValue;
+    }
+
+    return acumulator;
+  }, {});
+
+  return resultObj;
+}
 module.exports = convertToObject;

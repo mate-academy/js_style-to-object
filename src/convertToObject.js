dee376fbf30adec7bc09d17460e22cd9534e05a6
missk <rostock.vasiljev@gmail.com>
2024-09-10 00:03:21 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const styleLines = sourceString.split(';').filter(line => line.trim());
+
+  styleLines.forEach(styleLine => {
+    const [property, value] = styleLine.split(':').map(part => part.trim());
+
+    if(property, value){
+      result[property] = value;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

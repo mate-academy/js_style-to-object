0f94c415a5399ecf6bb6f281a35803ca02c57441
Mykola Zavalnitskyi <zavalnitskiyk@gmaail.com>
2024-09-10 00:06:46 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,22 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  const cleanedStr = stylesString.trim();
+
+  const styleRules = cleanedStr.split(';').filter((item) => item.trim() !== '');
+
+  const styleObj = {};
+
+  styleRules.forEach((rule) => {
+    const [key, value] = rule.split(':');
+
+    if (key && value) {
+      styleObj[key.trim()] = value.trim();
+    }
+  });
+
+  return styleObj;
 }
 
 module.exports = convertToObject;

59c898bc6bc6caed4336704d78292124cc0a893b
Alina Luchytska <aluchitska@gmail.com>
2024-09-13 18:29:59 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,15 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+    .split(';')
+    .map((arrKeyAndValue) => arrKeyAndValue.split(':'))
+    .filter((item) => item.length > 1)
+    .reduce((objectRes, arrTwoWords) => {
+      objectRes[arrTwoWords[0].trim()] = arrTwoWords[1].trim();
+
+      return objectRes;
+    }, {});
 }
 
 module.exports = convertToObject;

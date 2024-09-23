b47416ea92f0a327ea2bbcf355b2c1bb8de2e706
an-marryKyslenko <olih.a@icloud.com>
2024-09-23 09:33:48 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,14 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString
+  .split(';')
+  .map((element) => {
+    return element
+      .split(':')
+      .map(el => el.trim())
+  })
+  .reduce((prev, current) => ({...prev, [current[0]] : current[1]}),{})
 }
 
 module.exports = convertToObject;

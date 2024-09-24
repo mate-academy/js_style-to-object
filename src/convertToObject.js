ee04ccefa50ac678e2c65d03f4d27dd99cde79c2
Lina Do <dykaya0916@gmail.com>
2024-09-24 15:47:16 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssProperties = {};
+  const arrayOfItems = sourceString
+    .split(';') // ['ключ : свойство','ключ:свойство']
+    .filter((item) => item.includes(':')) // убираем  пустые элементы
+    .map((elem) => elem.split(':')); //[[ключ, свойство], [ключ, свойство]]
+
+  arrayOfItems.forEach((unit) => {
+    const key = unit[0].trim();
+    const value = unit[1].trim();
+
+    cssProperties[key] = value;
+  });
+  return cssProperties;
 }
 
 module.exports = convertToObject;

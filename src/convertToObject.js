8b3d335a2ee8d7ed0deeb95456274b76cdef7330
Liz <yelyzavetakumber@gmail.com>
2024-09-25 11:41:08 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,22 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const splitString = sourceString
+    .split(';')
+    .filter((string) => string.includes(':'));
+  const result = {};
+
+  const callback = (line) => {
+    if (line) {
+      const splitLine = line.split(':');
+
+      result[splitLine[0].trim()] = splitLine[1].trim();
+    }
+  };
+
+  splitString.forEach(callback);
+
+  return result;
 }
 
 module.exports = convertToObject;

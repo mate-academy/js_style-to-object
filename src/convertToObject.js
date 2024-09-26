144334d2b10c3845aa680acff88cbf59f7d6d1b8
vkrasnovyd <v.krasnovyd@gmail.com>
2024-09-26 19:04:22 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,35 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  // Transform string into array of lines
+  const propertiesArray = sourceString.split(';');
+  const styles = {};
+
+  // Transform separate lines of the array of properties into key-value pairs
+  propertiesArray.forEach((property) => {
+    const parts = property.split(':');
+
+    const key = dataCleanup(parts[0]);
+    const value = dataCleanup(parts[1]);
+
+    // Add to the resulting object only the lines with both key and value
+    if (key.length && value.length) {
+      styles[key] = value;
+    }
+  });
+
+  return styles;
+}
+
+/**
+ * @param {any} item
+ *
+ * @returns {string}
+ *
+ * Remove whitespaces around the parts of the property
+ */
+function dataCleanup(item) {
+  return String(item).trim();
 }
 
 module.exports = convertToObject;

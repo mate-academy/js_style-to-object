9cbd9609a6ef2b2e71c88e78de0cbf07c55bb0d6
Ivan <ivan.kartavy@icloud.com>
2024-09-28 19:09:47 +0300

    task done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,16 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return sourceString.split(';').reduce((styles, styleString) => {
+    const updatedStyles = { ...styles };
+    const styleKeyValue = styleString.split(':');
+
+    if (styleKeyValue[0] && styleKeyValue[1]) {
+      updatedStyles[styleKeyValue[0].trim()] = styleKeyValue[1].trim();
+    }
+
+    return updatedStyles;
+  }, {});
 }
 
 module.exports = convertToObject;

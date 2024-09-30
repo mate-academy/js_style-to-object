42f382ab0d47841e4e77c18bbdc8aac8e6852d58
Edgers <a.b.23042000@gmail.com>
2024-09-30 15:39:21 +0300

    Saint Pipeline, please give me the green light

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splittedString = sourceString.split(';');
+
+  const trimmedArray = splittedString.map((item) => {
+    return item.trim();
+  });
+
+  for (let i = 0; i < trimmedArray.length; i++) {
+    if (trimmedArray[i] === '' || trimmedArray[i] === ' ') {
+      trimmedArray.splice(i, 1);
+      i--;
+    }
+  }
+
+  return trimmedArray.reduce((acc, item) => {
+    const [key, value] = item.split(':');
+    acc[key.trim()] = value.trim();
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;

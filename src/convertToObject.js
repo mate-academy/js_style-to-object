0e56e4cdbeb7964b609d6373f105cae7cb16a143
Halyna <minimal005@gmail.com>
2024-10-01 13:43:44 +0300

    my solution to upgrade slyles

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/**
+/*
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleUpgrated = sourceString
+    .split(';')
+    .map((el) => el.split(':'))
+    .reduce((prev, oneStyle) => {
+      if (oneStyle[0] && oneStyle[1]) {
+        return { ...prev, [oneStyle[0].trim()]: oneStyle[1].trim() };
+      }
+
+      return prev;
+    }, {});
+
+  return styleUpgrated;
 }
 
 module.exports = convertToObject;

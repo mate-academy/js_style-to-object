e429948d520af4c7b76c884f1e0c61da0f6bc01f
Jonathan Hora <jonathan.s.hora@gmail.com>
2024-10-02 21:56:07 -0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const array = sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .filter((style) => style.length > 0);
+
+  const styleObject = {};
+
+  for (let i = 0; i < array.length; i++) {
+    const style = array[i];
+
+    // Separar a chave (propriedade) e o valor da declaração
+    const [key, value] = style.split(':');
+
+    if (key && value) {
+      // Remover espaços extras da chave e do valor
+      const cleanedKey = key.trim();
+      const cleanedValue = value.trim();
+
+      // Adicionar o par chave-valor ao objeto
+      styleObject[cleanedKey] = cleanedValue;
+    }
+  }
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

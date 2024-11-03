c80ea1d1c620f37bc2b7ad39eac8e58f0545a2ff
Pavlo <pavlokotsan421@gmail.com>
2024-11-03 15:25:13 -0500

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssProperties = {};
+
+  // Access "backcround-color:    #fff"
+  const splittedString = sourceString.split(';');
+
+  for (const currentElement of splittedString) {
+    // Example: "background-color", "   #fff"
+    const splitElements = currentElement.split(':');
+
+    if (splitElements.length === 2) {
+      // Example: "background-color": "#fff", all spaces from sides are deleted
+      cssProperties[splitElements[0].trim()] = splitElements[1].trim();
+    }
+  }
+
+  return cssProperties;
 }
 
 module.exports = convertToObject;

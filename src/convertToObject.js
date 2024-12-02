6c9c111de41f1c63ad8faa03b010bb2b4581acc9
Varvara00 <varvara.galy171203@gmail.com>
2024-12-02 14:24:34 +0100

    finished task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const styleArray = sourceString.split(';').filter(Boolean);
+
+  const styleObject = {};
+
+  styleArray.forEach((style) => {
+    const [name, value] = style.split(':').map((item) => item.trim());
+
+    if (name && value) {
+      styleObject[name] = value;
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

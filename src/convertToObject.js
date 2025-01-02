7e6f23b8a73f58beadb7edbe1ababbf99974f3e9
Diana Khalaim <khalaimdiana@gmail.com>
2025-01-02 13:29:20 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+
+  const regex = /([a-zA-Z-]+)\s*:\s*([^;]+);?/g;
+  let match;
+
+  while ((match = regex.exec(sourceString)) !== null) {
+    const property = match[1].trim(); // CSS property
+    const value = match[2].trim(); // CSS value, including multiline content
+
+    if (property && value) {
+      stylesObject[property] = value;
+    }
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

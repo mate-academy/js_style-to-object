651f52bbd1a5b4b965a686b42ce2492c96bf34da
troleksii <aliosha123418@gmail.com>
2025-01-07 21:06:23 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,27 @@
  *
  * @return {object}
  */
+
+// const complexStylesString = `
+//   background-color: #fff;
+//   border: 1px solid #e8e8e8;
+// `;
+
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const strings = sourceString
+    .split(';')
+    .map((str) => str.trim())
+    .filter((str) => str.includes(':'));
+
+  strings.forEach((str) => {
+    const [key, value] = str.split(':').map((part) => part.trim());
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

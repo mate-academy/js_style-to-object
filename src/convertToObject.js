44f47ce7fe61480444085ebf90779eabd3d8e69c
Nadiia Bulmak <nadgaval@gmail.com>
2025-01-17 19:41:29 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayOfCss = sourceString.split(';');
+  const trimingAndFiltering = arrayOfCss
+    .filter((item) => item.trim() !== '')
+    .map((item) => item.trim());
+
+  const dividing = trimingAndFiltering.map((item) => {
+    const parts = item.split(':');
+    const trimmedParts = parts.map((part) => part.trim());
+
+    return trimmedParts;
+  });
+
+  const objecting = Object.fromEntries(dividing);
+
+  return objecting;
 }
 
 module.exports = convertToObject;

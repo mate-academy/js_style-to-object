be5e76585c0f5538b0ac3e58d87d360e3369a9fc
Nataliia Kozyriatska <kozyriatskanataliia@gmail.com>
2025-01-19 17:14:11 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,26 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+const convertToObject = (sourceString) => {
+  const rules = sourceString.replace(/;+/g, ';');
+
+  const result = rules.split(';');
+
+  return result.reduce((prev, rule) => {
+    const [key, value] = rule.split(':');
+
+    if (key && value) {
+      const trimmedKey = key.trim();
+      const trimmedValue = value.trim();
+
+      return {
+        ...prev,
+        [trimmedKey]: trimmedValue,
+      };
+    }
+
+    return prev;
+  }, {});
+};
 
 module.exports = convertToObject;

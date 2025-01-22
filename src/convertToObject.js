4c01a19f263d0b07a2323df9f6b03eecf412cc50
marushchak <marushchak099@gmail.com>
2025-01-22 19:32:04 +0200

    add splution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,42 +8,20 @@
 
 function convertToObject(sourceString) {
   const toArr = sourceString
-    .replace(/\n/g, ' ')
     .trim()
-    .split('; ')
+    .split(';')
     .map((el) => el.trim());
   const clineArr = toArr.filter((element) => element.length > 1);
   const result = {};
 
   clineArr.forEach((rules) => {
-    const rulesToArr = rules.split(': ');
+    const rulesToArr = rules.split(':');
     const [key, value] = rulesToArr;
-    let trimmedKey = key.trim();
-    let trimmedValue = value;
 
-    if (trimmedKey.split('').shift() === ';') {
-      trimmedKey = key.slice(1);
-    }
-
-    if (value.split('').pop() === ';') {
-      trimmedValue = value.slice(0, -1);
-    }
-
-    result[trimmedKey] = myTrim(trimmedValue, ',', ',\n          ');
+    result[key.trim()] = value.trim();
   });
 
   return result;
-}
-
-function myTrim(string, getSymbol, setSymbol) {
-  const toArr = string.split(getSymbol);
-  const result = [];
-
-  for (let i = 0; i < toArr.length; i++) {
-    result.push(toArr[i].trim());
-  }
-
-  return result.join(setSymbol);
 }
 
 module.exports = convertToObject;

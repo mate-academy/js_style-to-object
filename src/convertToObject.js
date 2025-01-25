3fabe8c9af9047a188c0db2eef7a27df7fbf9afc
AndreaBoiko <andriy.boyko35@gmail.com>
2025-01-25 23:53:25 +0100

    result

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -4,9 +4,24 @@
  * @param {string} sourceString
  *
  * @return {object}
+ *
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+
+  sourceString
+    .split(/;\s*/)
+    .filter((line) => line.trim())
+    .forEach((line) => {
+      const [property, value] = line.split(/:\s*/);
+
+      if (property && value) {
+        styleObject[property.trim()] = value.trim();
+      }
+    });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

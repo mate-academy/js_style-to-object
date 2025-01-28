a3e4f15f9c23e5ac50f19fc57bb2f2b1e5141b47
Andrii Filipenko <andreyfil1337@gmail.com>
2025-01-28 18:52:14 +0200

    2 tests failed, 15 passed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,35 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString === '') {
+    return {};
+  }
+
+  let temporaryWordsArray = [];
+  let temporaryValuesString = '';
+  const stylesObject = {};
+  const splitedSource = sourceString.split(';');
+  const allowedSymbols = /[^\s:;\n\t ]+/gm;
+
+  for (const style of splitedSource) {
+    if (allowedSymbols.test(style) === false) {
+      continue;
+    }
+
+    temporaryWordsArray = style.match(allowedSymbols);
+    temporaryValuesString = temporaryWordsArray[1];
+
+    for (let i = 2; i < temporaryWordsArray.length; i++) {
+      temporaryValuesString += ' ' + temporaryWordsArray[i];
+    }
+
+    stylesObject[temporaryWordsArray[0]] = temporaryValuesString;
+
+    temporaryWordsArray = [];
+    temporaryValuesString = '';
+  }
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

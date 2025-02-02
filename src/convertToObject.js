a939d0717d0e50ac7e1e1c906d8fdd9ddb28b1d8
Ivan Herhi <ionherhi@gmail.com>
2025-02-02 10:09:13 +0100

    sortText

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const box01 = sourceString.split(';').filter((item) => item !== '');
+
+  const arr01 = box01.map((item) => {
+    return item.split(':');
+  });
+
+  const arr02 = arr01.map((item) => item.map((part) => part.trim()));
+
+  const objsct = {};
+
+  arr02.forEach(([arr01It, arr02It]) => (objsct[arr01It] = arr02It));
+
+  return objsct;
 }
 
 module.exports = convertToObject;

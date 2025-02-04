49361f43727da7b829978ad2dec895d35f881413
Nazar Shpylovyi <shpiloviy.nazar@ukr.net>
2025-02-04 16:51:26 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(/;\s*/)
+
+    .map((rule) => rule.trim())
+    .filter((rule) => rule.length)
+    .reduce((acc, rule) => {
+      const [property, value] = rule.split(/:\s*/);
+
+      if (property && value !== undefined) {
+        acc[property.trim()] = value.trim();
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

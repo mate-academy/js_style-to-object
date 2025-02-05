8e7d308c3ac04181b9063bc0191b522123c607c8
tiblazy <hauntedapply@gmail.com>
2025-02-05 10:53:26 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((string) => string.trim())
+    .filter((string) => string !== '')
+    .map((string) => string.split(':'))
+    .map((string) => {
+      return { [string[0].trim()]: string[1].trim() };
+    })
+    .reduce((acc, obj) => {
+      return { ...acc, ...obj };
+    }, {});
 }
 
 module.exports = convertToObject;

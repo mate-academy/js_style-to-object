638b81c8f9ded27beceeb70c8517dc3a7795e607
Yuriy Postrygan <petrikeich@gmail.com>
2025-02-07 13:34:58 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,26 @@
 'use strict';
 
 /**
- * @param {string} sourceString
- *
- * @return {object}
- */
+//  * @param {string} sourceString
+//  *
+//  * @return {object}
+//  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const splitArray = sourceString.split(';');
+
+  const cleanedArray = splitArray.map((item) => item.replace(';', ''));
+  const filteredArray = cleanedArray.filter((item) => item);
+
+  const resultObject = filteredArray.reduce((acc, pair) => {
+    const [key, value] = pair.split(':').map((str) => str.trim());
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

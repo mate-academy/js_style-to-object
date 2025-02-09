23d06e84a5a657fc87e3f71a3d0983a7f57889f1
olehdemkivqa <oleg.demkiv45@gmail.com>
2025-02-09 19:49:52 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const propArr = sourceString;
+
+  propArr
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line !== '' && line.includes(':'))
+    .forEach((line) => {
+      const [property, value] = line.split(/:\s*/);
+
+      result[property.trim()] = value.trim();
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

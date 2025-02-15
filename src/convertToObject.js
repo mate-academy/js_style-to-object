c85afd68bb89eb924d77386368828e4f60034f7b
Yaroslava <semydayaroslava@icloud.com>
2025-02-15 15:46:00 -0500

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,29 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+
+  const cleanedString = sourceString
+    .replace(/\r\n|\r/g, '\n')
+    .replace(/\s*;\s*/g, ';')
+    .trim();
+  const styleLines = cleanedString.split(';');
+
+  styleLines.forEach((style) => {
+    const trimmedStyle = style.trim();
+
+    if (trimmedStyle) {
+      const [property, value] = trimmedStyle
+        .split(':')
+        .map((item) => item.trim());
+
+      if (property && value) {
+        styleObject[property] = value;
+      }
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

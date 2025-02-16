07eb673aa9cdcce0f13a58a7486b64fd54b98262
Illia <hetsko.ilya@gmail.com>
2025-02-17 01:42:54 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,18 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const pairs = sourceString.split(';');
+
+  pairs.forEach((pair) => {
+    const [key, value] = pair.split(':').map((str) => str.trim());
+
+    if (key) {
+      result[key] = value;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

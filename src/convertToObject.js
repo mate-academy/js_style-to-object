50807044abd71e1ad617496d69097b687aff71a0
Masha Ponomarenko <maria@Maria.local>
2025-02-19 12:42:09 +0200

    task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesArray = sourceString
+    .split(';')
+    .filter((styleItem) => styleItem.length > 3)
+    .map((indStyle) => indStyle.split(':').map((objEntry) => objEntry.trim()));
+
+  const stylesObject = {};
+
+  stylesArray.forEach(
+    (styleItem) => (stylesObject[styleItem[0]] = styleItem[1]),
+  );
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

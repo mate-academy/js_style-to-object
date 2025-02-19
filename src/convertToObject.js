7bc70ac2144215c5bbcdc020a0f73d0784ac6edf
Andrew Siemakin <andrewsiemakin@gmail.com>
2025-02-19 18:04:09 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const rules = sourceString.split(';');
+
+  const filterdRules = rules
+    .map((rule) => rule.trim())
+    .filter((rule) => rule !== '');
+
+  const formattedRules = filterdRules
+    .map((rule) => {
+      const parts = rule.split(':').map((part) => part.trim());
+
+      return parts.length === 2 ? parts : null;
+    })
+    .filter(Boolean);
+
+  return Object.fromEntries(formattedRules);
 }
 
 module.exports = convertToObject;

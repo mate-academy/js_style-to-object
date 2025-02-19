e1ef10c2045b894587fd751e8f1feab60dd36411
reb0rned <affmarket77@gmail.com>
2025-02-19 18:13:00 +0200

    converter solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,21 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .trim()
+    .split(';')
+    .filter((el) => el.trim() !== '')
+    .reduce((acc, el) => {
+      const [key, value] = el.split(':');
+      const keyResult = key.trim();
+      const valueResult = value.trim();
+
+      acc[keyResult] = valueResult;
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

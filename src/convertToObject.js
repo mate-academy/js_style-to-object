cf3c5ba2f905b45e43ed171330963e32d7e92c64
Sergio <srvalle@gmail.com>
2025-02-19 19:41:35 -0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString === '') {
+    return {};
+  }
+
+  const styleObject = {};
+
+  // Divide a string em linhas
+  const lines = sourceString.split(';');
+
+  // Itera sobre cada linha
+  lines.forEach((line) => {
+    // Verifica se a linha não está vazia e remove espaços se tiver espaços.
+    if (line.trim()) {
+      // Divide a linha em propriedade e valor com desestruturação
+      // map com trim limpa property e value de possíveis espaços em branco
+      const [property, value] = line.split(':').map((item) => item.trim());
+
+      // Adiciona ao objeto se a propriedade e o valor forem válidos
+      if (property && value) {
+        styleObject[property] = value;
+      }
+    }
+  });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

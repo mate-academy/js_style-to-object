7cd918657fff418b036d0e39e3f41e47dcb419a4
Vladyslav <vlad19943103@gmail.com>
2025-02-22 23:54:48 +0100

    My solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const style = {};
+  const declarations = sourceString.split(';');
+
+  declarations.forEach((declaration) => {
+    const trimerDeclaration = declaration.trim();
+
+    if (trimerDeclaration) {
+      const parts = trimerDeclaration.split(':');
+
+      if (parts.length === 2) {
+        const property = parts[0].trim();
+        const value = parts[1].trim();
+
+        style[property] = value;
+      }
+    }
+  });
+
+  return style;
 }
 
 module.exports = convertToObject;

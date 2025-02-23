6f10ea99fb410d98f5521d3853cf679bddc8b722
Anton Polishchuk <hardixxxxx@gmail.com>
2025-02-23 14:52:25 +0100

    complete task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const propsValues = sourceString.split(';');
+  const cssMap = {};
+
+  propsValues.forEach((propValue) => {
+    const [prop, value] = propValue.split(':');
+
+    if (propValue.trim()) {
+      cssMap[prop.trim()] = value.trim();
+    }
+  });
+
+  return cssMap;
 }
 
 module.exports = convertToObject;

ffa939fdd166c05f76081643231d1bffbb809f9c
Your_Name Your_Surname <your@email>
2025-02-26 19:57:10 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const letters = sourceString.split(';');
+  const result = letters.reduce((accumulator, value) => {
+    const params = value.split(':').map((element) => element.trim());
+
+    if (params.length === 2) {
+      const [key, val] = params;
+
+      accumulator[key] = val;
+    }
+
+    return accumulator;
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

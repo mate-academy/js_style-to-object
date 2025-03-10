f3a43ebe45134f74573885816943dd208310e5ac
Viktor Pastushenko <viktor.pastushenko@udelphi.com>
2025-03-10 18:27:31 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const regEx = /\n/g;
+
+  return sourceString
+    .replace(regEx, '')
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter((rule) => rule !== '')
+    .reduce((cssObject, rule) => {
+      const tmp = rule.split(':');
+      const value = tmp[1]
+        .split(',')
+        .map((item) => item.trim())
+        .join(',\n          ');
+
+      return {
+        ...cssObject,
+        [tmp[0].trim()]: value,
+      };
+    }, {});
 }
 
 module.exports = convertToObject;

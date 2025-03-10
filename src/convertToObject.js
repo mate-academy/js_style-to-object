6c487ceb2ca4fb06feb516c954cc6ef586bf707f
Vasyl <xiromant93@gmail.com>
2025-03-10 15:51:23 +0200

    Sollution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,20 @@
-'use strict';
+function convertToObject(stylesString) {
+  const stylesObject = {};
 
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
-function convertToObject(sourceString) {
-  // write your code here
+  const declarations = stylesString
+    .split(';')
+    .map((s) => s.trim())
+    .filter((s) => s.length > 0);
+
+  declarations.forEach((declaration) => {
+    const [property, value] = declaration.split(':').map((s) => s.trim());
+
+    if (property && value !== undefined) {
+      stylesObject[property] = value;
+    }
+  });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

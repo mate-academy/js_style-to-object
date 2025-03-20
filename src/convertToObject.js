3a7757151632d2a602779bb977029134094f632f
Katarzyna Milewska <milewska30@gmail.com>
2025-03-20 15:00:26 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,21 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
-
   const cssString = sourceString.trim().replace(/;$/, '');
 
-  const rules = cssString.split(';').map((rule) => rule.trim());
+  const rules = cssString
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter(Boolean);
 
-  rules.forEach((rule) => {
+  return rules.reduce((result, rule) => {
     const [property, value] = rule.split(':').map((part) => part.trim());
 
     if (property && value) {
       result[property] = value;
     }
-  });
 
-  return result;
+    return result;
+  }, {});
 }
 
 module.exports = convertToObject;

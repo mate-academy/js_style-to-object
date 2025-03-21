af241b4aa88d63c2f6bd81ef716f7ad92170c1ae
ILLIA Stasiuk <bobicp01@gmail.com>
2025-03-21 18:24:56 +0100

    Advence 2.0

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,26 +5,18 @@
  *
  * @return {object}
  */
-function convertToObject(keys) {
-  const styleArry = keys.split(';');
+function convertToObject(stylesString) {
+  return Object.fromEntries(
+    stylesString
+      .split(';')
+      .map((line) => line.trim())
+      .filter((line) => line) // убираем пустые строки
+      .map((style) => {
+        const [key, value] = style.split(':');
 
-  const resultArr = styleArry
-    .map((item) => item.trim())
-    .filter((item) => item.length > 0);
-
-  const stylesEntries = resultArr.map((style) => {
-    const [key, value] = style.split(':');
-
-    return [key.trim(), value.trim()];
-  });
-
-  const total = stylesEntries.reduce((acc, [key, value]) => {
-    acc[key] = value;
-
-    return acc;
-  }, {});
-
-  return total;
+        return [key.trim(), value.trim()];
+      }),
+  );
 }
 
 module.exports = convertToObject;

c9505dfd4329ffeb43950023e94f084be75c506c
ILLIA Stasiuk <bobicp01@gmail.com>
2025-03-21 18:19:41 +0100

    JS Advanced

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,26 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(keys) {
+  const styleArry = keys.split(';');
+
+  const resultArr = styleArry
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0);
+
+  const stylesEntries = resultArr.map((style) => {
+    const [key, value] = style.split(':');
+
+    return [key.trim(), value.trim()];
+  });
+
+  const total = stylesEntries.reduce((acc, [key, value]) => {
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return total;
 }
 
 module.exports = convertToObject;

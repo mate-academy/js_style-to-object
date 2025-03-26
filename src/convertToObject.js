9df30868f3e3473dc1b42556823707b3bc8e4e0c
AnoriFrell <anori.fjord@gmail.com>
2025-03-26 17:22:14 +0200

    objected

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,22 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const convertToArray = sourceString.split(';');
+  const normalizedArray = convertToArray.map((item) => {
+    return item.trim();
+  });
+
+  const createObject = normalizedArray.reduce((acc, item) => {
+    const [key, value] = item.split(':').map((part) => part.trim());
+
+    if (key && value) {
+      acc[key] = value;
+    }
+
+    return acc;
+  }, {});
+
+  return createObject;
 }
 
 module.exports = convertToObject;

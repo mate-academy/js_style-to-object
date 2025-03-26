87e8c5a2153dbaaef74183132c025b71ee9780bb
NazarMykhailv <mihailiv1011@gmail.com>
2025-03-26 15:49:26 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,28 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const stylesObject = {};
+
+  return sourceString
+    .split(';')
+    .map((styleRule) => styleRule.trim())
+    .filter((styleRule) => styleRule.length > 0)
+    .reduce((accumulator, styleRule) => {
+      const colonPosition = styleRule.indexOf(':');
+
+      if (colonPosition === -1) {
+        return accumulator;
+      }
+
+      const property = styleRule.slice(0, colonPosition).trim();
+      const value = styleRule.slice(colonPosition + 1).trim();
+
+      if (property.length > 0) {
+        accumulator[property] = value;
+      }
+
+      return accumulator;
+    }, stylesObject);
 }
 
 module.exports = convertToObject;

4ad64782797fbba8a4708d279185a0d4a2322ecf
Diana <dinamitka2369@gmail.com>
2025-03-28 13:39:51 +0100

    result1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,20 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(styles) {
+  return styles
+    .split(';')
+    .map((style) => style.trim())
+    .filter(Boolean)
+    .reduce((acc, style) => {
+      const [key, value] = style.split(':').map((part) => part.trim());
+
+      if (key && value) {
+        acc[key] = value;
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

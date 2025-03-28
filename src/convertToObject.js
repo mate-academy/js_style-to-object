78eb6e33d4031045ad14e1d7c89440e5240fcb31
PArtCreate <143039892+partcreate@users.noreply.github.com>
2025-03-28 08:54:04 +0000

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,24 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const strArray = sourceString.split(';');
+
+  for (let i = 0; i < strArray.length; i++) {
+    strArray[i] = strArray[i]
+      .trim()
+      .split(':')
+      .map((el) => el.trim());
+  }
+
+  const filtArray = strArray.filter((prop) => prop.length > 1);
+
+  const resObject = {};
+
+  for (const pr of filtArray) {
+    resObject[pr[0]] = pr[1];
+  }
+
+  return resObject;
 }
 
 module.exports = convertToObject;

4321447babaf1e2720a732cf0250fdc4d43dc9d1
Vladyslav <vladchunkaluk@gmail.com>
2025-04-22 13:30:57 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const rules = sourceString.split(';');
+  const trimmedRules = rules.map((rule) => rule.trim());
+  const propertyValuePairs = trimmedRules
+    .filter((rule) => rule !== '') // Фільтруємо порожні правила
+    .map((rule) => rule.split(':'));
+  const styleObjects = propertyValuePairs
+    .filter((pair) => pair.length === 2) // Фільтруємо некоректні правила
+    .map((pair) => ({ [pair[0].trim()]: pair[1].trim() }));
+
+  // Об'єднуємо масив об'єктів в один об'єкт
+  return styleObjects.reduce((acc, obj) => ({ ...acc, ...obj }), {});
 }
 
 module.exports = convertToObject;

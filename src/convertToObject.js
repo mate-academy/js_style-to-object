d64ebd60eafc80b826ab0c18928322af3e9fb2d4
Yakiv Salaban <yasha0@icloud.com>
2025-04-24 12:31:21 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const propsDividedToStrings = sourceString.split(';');
+  const correctCssRules = propsDividedToStrings.reduce(
+    (accum, propertyPair) => {
+      if (propertyPair.includes(':')) {
+        const propAndValue = propertyPair
+          .split(':')
+          .map((proper) => proper.trim());
+
+        accum[propAndValue[0]] = propAndValue[1];
+      }
+
+      return accum;
+    },
+    {},
+  );
+
+  return correctCssRules;
 }
 
 module.exports = convertToObject;

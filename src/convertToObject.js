d9e3f7d40c2bfba73d0e15ee8d5ca7eb883bc4fb
Viktoria Zabolotnia <vika.zab4@gmail.com>
2025-04-29 20:46:53 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,24 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(styles) {
+  const arrayOfRules = styles
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter(Boolean);
+
+  const objectOfRules = {};
+
+  for (const rule of arrayOfRules) {
+    const colonIndex = rule.indexOf(':');
+
+    const key = rule.slice(0, colonIndex).trim();
+    const value = rule.slice(colonIndex + 1).trim();
+
+    objectOfRules[key] = value;
+  }
+
+  return objectOfRules;
 }
 
 module.exports = convertToObject;

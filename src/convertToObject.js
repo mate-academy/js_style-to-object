e4ce62ec1f0985d1839d319b16ab9e512d07bfdb
Veronika Okhten <veronika.oxt12@gmail.com>
2025-05-02 19:49:09 +0300

    add solutionn

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,28 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString.trim()) {
+    return {};
+  }
+
+  const linesFilter = sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line !== '');
+
+  const stylesObj = {};
+
+  for (let i = 0; i < linesFilter.length; i++) {
+    const [key, value] = linesFilter[i].split(':');
+
+    if (key && value) {
+      stylesObj[key.trim()] = value.trim();
+    }
+  }
+
+  return stylesObj;
 }
 
 module.exports = convertToObject;

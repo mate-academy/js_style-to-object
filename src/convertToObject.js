4bff895974557b155fceeb36f33f8e283c28ea5d
Vadym-Kit <superovyq@gmai.com>
2025-05-02 19:43:26 +0300

    result

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = sourceString
+    .split(';')
+    .map((a) => a.trim())
+    .filter((a) => a.includes(':'))
+    .reduce((acc, a) => {
+      const [key, value] = a.split(':');
+
+      if (key && value) {
+        acc[key.trim()] = value.trim();
+      }
+
+      return acc;
+    }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

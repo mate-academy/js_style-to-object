343fbb75741175a1401dd7645fb0a104c9db69d8
Andrew Pavlenko <andrew.pawllenko@gmail.com>
2025-05-02 16:53:22 +0300

    did the JS task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const rules = sourceString.split(';');
+
+  rules.forEach((rule) => {
+    const trimmedRule = rule.trim();
+
+    if (trimmedRule) {
+      const [property, value] = trimmedRule
+        .split(':')
+        .map((item) => item.trim());
+
+      result[property] = value;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

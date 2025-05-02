a02c02af7d9b3cd993d6d8cc0bf265f9e0ebdfb0
TetianaHodis1308 <hodistetianajob@ukr.net>
2025-05-02 17:18:09 +0300

    added fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,11 +6,13 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  return sourceString.split(';').reduce((acc, elem, index, arr) => {
-    const [key, value] = elem.split(':');
+  return sourceString.split(';').reduce((acc, elem) => {
+    if (elem !== ' ') {
+      const [key, value] = elem.split(':');
 
-    if (key !== undefined && value !== undefined) {
-      acc[key.trim()] = value.trim();
+      if (key !== undefined && value !== undefined) {
+        acc[key.trim()] = value.trim();
+      }
     }
 
     return acc;

843971a89df437e0f7fe6f4dd1c7375d94f97944
Oleksii Myronenko <oleksiimrnko@gmail.com>
2025-05-04 22:36:29 +0300

    add test solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const declarations = sourceString.split(';');
+
+  for (let declaration of declarations) {
+    declaration = declaration.trim();
+
+    if (!declaration) {
+      continue;
+    }
+
+    const colonIndex = declaration.indexOf(':');
+
+    if (colonIndex === -1) {
+      continue;
+    }
+
+    const property = declaration.slice(0, colonIndex).trim();
+    const value = declaration.slice(colonIndex + 1).trim();
+
+    result[property] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

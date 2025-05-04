482119f437d67721527da9b002aa479778f353a2
Eleonora Stasyuk <1990applesweet@gmail.com>
2025-05-04 15:13:59 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,26 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const splittedArr = sourceString.split(';');
+
+  for (let i = 0; i < splittedArr.length; i++) {
+    const style = splittedArr[i];
+
+    if (style) {
+      const colonIndex = style.indexOf(':');
+
+      if (colonIndex > -1) {
+        const prop = style.slice(0, colonIndex).trim();
+        const value = style.slice(colonIndex + 1).trim();
+
+        result[prop] = value;
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

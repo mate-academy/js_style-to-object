6d5335c9ef1afc90043df1904f0b2497d5b22750
Yvhenii <mtr1289@gmail.com>
2025-05-04 14:23:08 +0300

    new file

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = {};
+  const declarations = sourceString.split(';');
+
+  for (const declaration of declarations) {
+    const parts = declaration.trim().split(':');
+
+    if (parts.length === 2) {
+      const key = parts[0].trim();
+      const value = parts[1].trim();
+
+      if (key !== '' && value !== '') {
+        styles[key] = value;
+      }
+    }
+  }
+
+  return styles;
 }
 
 module.exports = convertToObject;

be55b34ba48c57bc60e48d218ed30641448aa997
Dmitriy <dmitriyhaiduk00@gmail.com>
2025-05-06 14:20:53 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -3,7 +3,6 @@
 function convertToObject(styleString) {
   const styleObject = {};
   const declarations = styleString.split(';');
-  
 
   for (const declaration of declarations) {
     const trimmedDeclaration = declaration.trim();
@@ -20,9 +19,34 @@ function convertToObject(styleString) {
   return styleObject;
 }
 
-const complexStylesString = `  background-color:      #fff;-webkit-border-radius: 5px;  border-radius     : 5px;  border: 1px solid #e8e8e8;  -webkit-box-sizing: border-box;  box-sizing: border-box;  clear   : both       ;  cursor: pointer;  float: left;  font-family: inherit;      font-size: 14px;  font-weight: 400;  height: 42px;  line-height:    40px;  outline: 0;  padding-left    : 18px;  padding-right: 30px;  ;  ;  position: relative;  text-align: left !important;  -webkit-transition: all .2s ease-in-out;  transition: all .2s ease-in-out;  -moz-user-select: none;  -ms-user-select: none;  user-select: none;  white-space: nowrap;  width: auto;`;
-const result = convertToObject(complexStylesString);
-console.log(result);
+const complexStylesString = `
+  background-color: #fff;
+  -webkit-border-radius: 5px;
+  border-radius: 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear: both;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+  font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height: 40px;
+  outline: 0;
+  padding-left: 18px;
+  padding-right: 30px;
+  position: relative;
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  white-space: nowrap;
+  width: auto;
+`;
 
 console.log(result == {
   'background-color': '#fff',

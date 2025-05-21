5594f5c7f5f164e19a0d4e508306dfd27968cc74
Your Namesimbochka <kinasvika@gmail.com>
2025-05-21 16:30:22 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,34 @@
 'use strict';
 
+/* eslint-disable no-console */
+
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const rules = sourceString
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter((rule) => rule.length > 0);
+
+  for (const rule of rules) {
+    const [property, ...valueParts] = rule.split(':');
+
+    if (!property || valueParts.length === 0) {
+      continue;
+    }
+
+    const key = property.trim();
+    const value = valueParts.join(':').trim();
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

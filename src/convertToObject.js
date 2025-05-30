1296370fada239ffa4fc04710efe40e5fd6e976d
Yurii <j.zherebilov15@gmail.com>
2025-05-31 00:47:02 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,37 @@
  *
  * @return {object}
  */
+// function convertToObject(sourceString) {
+//   const filtered = sourceString.split(';').filter((elem) => elem !== '');
+
+//   const obj = {};
+
+//   for (let i = 0; i < filtered.length; i++) {
+//     const splited = filtered[i]
+//       .split(':')
+//       .filter((elem) => elem !== ' ' && elem !== '');
+//     const trimmedKey = splited[0].trim();
+//     const trimmedValue = splited[1].trim();
+
+//     obj[trimmedKey] = trimmedValue;
+//   }
+
+//   return obj;
+// }
+
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+
+  sourceString
+    .split(';')
+    .filter((el) => el.trim() !== '')
+    .forEach((pair) => {
+      const [key, value] = pair.split(':');
+
+      obj[key.trim()] = value.trim();
+    });
+
+  return obj;
 }
 
 module.exports = convertToObject;

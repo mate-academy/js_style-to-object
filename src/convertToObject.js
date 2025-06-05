6a41f3c189a5b112eb034498dc09787d0ee8bb1d
Daniel-Huudek <139517145+Daniel-Huudek@users.noreply.github.com>
2025-06-05 11:45:00 -0300

    tesk solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  const rules = sourceString.split(';');
+
+  for (const rule of rules) {
+    const [property, value] = rule.split(':').map((item) => item.trim());
+
+    if (property && value) {
+      result[property] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

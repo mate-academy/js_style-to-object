9f10944e0236cef24f722a5df82d273b43331d17
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-06-16 10:01:17 +0300

    final solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,29 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const res = {};
+  const sourceStringSplitted = sourceString.split('\n');
+
+  const keyAndValues = sourceStringSplitted.map((el) => {
+    let [key, value] = el.split(':');
+
+    if ([key, value].length < 2) {
+      return null;
+    }
+
+    key = key.trim();
+    value = value.trim().replace(/;$/, '');
+
+    return [key, value];
+  });
+
+  for (let i = 0; i < keyAndValues.length; i++) {
+    if (keyAndValues[i][0] && keyAndValues[i][1]) {
+      res[keyAndValues[i][0]] = keyAndValues[i][1];
+    }
+  }
+
+  return res;
 }
 
 module.exports = convertToObject;

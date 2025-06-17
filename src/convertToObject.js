fa87d202346cd0074ea4fa580a6742983d6b6388
Dinu Litun <dinulitun@macbookair.home>
2025-06-17 12:27:03 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const arr = sourceString.split(';');
+
+  const obj1 = {};
+
+  arr.map((item) => {
+    const [key, value] = item.split(':').map((part) => part.trim());
+
+    if (key && value !== undefined) {
+      obj1[key] = value;
+    }
+  });
+
+  return obj1;
 }
 
 module.exports = convertToObject;

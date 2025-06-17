27db849b13a4718987d1dcbd1fea7ed44b7320f1
Tamila Kochereshchenko <tamila272005@icloud.com>
2025-06-17 13:24:06 +0300

    first try

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+  const lines = sourceString.split('/n');
+
+  for (let line of lines) {
+    line = line.trim();
+
+    if (!line || !line.includes(':')) {
+      continue;
+    }
+
+    const colonIndex = line.lastIndexOf(':');
+    const key = line.slice(0, colonIndex).trim();
+    let value = line.slice(colonIndex + 1).trim();
+
+    if (value.endsWith(';')) {
+      value = value.slice(0, -1).trim();
+    }
+    obj[key] = value;
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

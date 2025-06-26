daadbd85497b5fbd3a9c25403c8ac63de56978ee
michelam-pseudo <michelammilian@outlook.com>
2025-06-26 15:49:21 -0300

    Codificação do módulo de conversão de uma folha de estilo CSS em template literal para um objeto chave e valor

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,36 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
+function convertToObject(styleString) {
   // write your code here
+  const styleStringSplited = styleString.split(';');
+
+  styleStringSplited.forEach((item, index) => {
+    styleStringSplited[index] = item.trim();
+  })
+
+
+  styleStringSplited.forEach((item, index) => {
+    if (item === '') {
+      delete styleStringSplited[index];
+      return;
+    }
+    styleStringSplited[index] = item.split(':');
+  })
+
+  const object = {};
+
+  styleStringSplited.forEach((role) => {
+    if (role === undefined) {
+      return;
+    }
+
+    object[role[0].trim()] = role[1].trim();
+  })
+
+  return object;
+
 }
+
 
 module.exports = convertToObject;

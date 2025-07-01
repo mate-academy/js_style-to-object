bf1c86fa74be5c7d85084dc6c0dc191ffcc69d01
Ivan Berkut <necrofairywork@gmail.com>
2025-07-01 13:37:28 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayStylesWithoutSemicolones = sourceString
+    .replaceAll(';', ':')
+    .split(':');
+  const arrayTrim = arrayStylesWithoutSemicolones.map((x) => x.trim());
+  const arrayFilter = arrayTrim.filter((x) => x !== '');
+  const arrayKeys = arrayFilter.filter((x, i) => i % 2 === 0);
+  const arrayValues = arrayFilter.filter((x, i) => i % 2 !== 0);
+
+  return createObject(arrayKeys, arrayValues);
+}
+
+function createObject(keys, values) {
+  const result = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    result[keys[i]] = values[i];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

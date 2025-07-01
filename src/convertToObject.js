4981162fe3102d35578a9250497d9beaf68f9505
Julio Vergueiro <89145136+Juliovergueiro@users.noreply.github.com>
2025-06-30 21:50:35 -0300

    First Commit

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString.split(';').forEach((newObj) => {
+    const objIndex = newObj.indexOf(':');
+
+    if (objIndex !== -1) {
+      const objKey = newObj.slice(0, objIndex).trim();
+      const objValue = newObj.slice(objIndex + 1).trim();
+
+      result[objKey] = objValue; // <-- this creates the object key/value
+    }
+  });
+
+  return result;
 }
+
+const stringo = 'background-color: red; font-size: 16px; margin-top: 10px';
+
+convertToObject(stringo);
 
 module.exports = convertToObject;

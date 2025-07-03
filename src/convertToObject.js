fa1274b647863db9208b65a628fc542f42470c59
Svyatoslav Kozelchuk-Panulin <svyatoslavkozelchuk@gmail.com>
2025-07-03 18:27:08 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,23 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(complexStylesString) {
+  const splitString = complexStylesString.split(';');
+
+  const trimmedArray = splitString
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0);
+
+  const result = {};
+
+  trimmedArray.map((item) => {
+    const [key, ...rest] = item.split(':');
+    const value = rest.join(':').trim();
+
+    result[key.trim()] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

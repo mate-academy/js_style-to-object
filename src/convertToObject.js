98bb9819bbb00aca218bb72d5b4e5d758c116a8c
CwaR228 <sosok2gt@gmail.com>
2025-07-04 14:52:01 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,27 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const converted = {};
+  const cuttedString = sourceString.split(';');
+
+  const filtered = cuttedString.filter((line) => {
+    const trimmed = line.trim();
+
+    return trimmed !== '' && trimmed.includes(':');
+  });
+
+  filtered.map((item) => {
+    let [property, value] = item.split(':');
+
+    property = property.trim();
+    value = value.trim();
+
+    return (converted[property] = value);
+  });
+
+  return converted;
 }
 
 module.exports = convertToObject;

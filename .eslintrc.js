87247ef91daae5fdd5488fb2fc63231060760d43
vtyacodes <vitiyah95@gmail.com>
2025-07-08 00:23:44 +0800

    solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   extends: '@mate-academy/eslint-config',
   env: {
-    jest: true
+    jest: true,
   },
   rules: {
-    'no-proto': 0
+    'no-proto': 0,
+    'no-console': 'off',
   },
-  plugins: ['jest']
+  plugins: ['jest'],
 };

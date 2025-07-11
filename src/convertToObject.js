c0072ced7aa76c969e0012ba773ba8213be1a862
Dmytro Petrov <sidodji228007@gmail.com>
2025-07-11 19:58:10 +0300

    addSolution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splittedString = sourceString.split(';');
+
+  splittedString.map(
+    (element, index, array) => (array[index] = element.split(':')),
+  );
+
+  splittedString.map((element) => {
+    for (let i = 0; i < element.length; i++) {
+      element[i] = element[i].trim();
+    }
+  });
+
+  return splittedString.reduce((accumulator = {}, string) => {
+    const [key, value] = string;
+
+    accumulator[key] = value;
+
+    return accumulator;
+  }, {});
 }
 
 module.exports = convertToObject;

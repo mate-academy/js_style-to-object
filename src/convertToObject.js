90fe76dfbe5a40d01c0bf99a5a90b88e54cf3d39
Nikolay Tyshchenko <nikolay.tyshchenko04@gmail.com>
2025-07-17 15:08:34 +0300

    solution for convertStylesToObject task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesList = sourceString
+    .split(';')
+    .reduce((styleAccumulator, styleProperty) => {
+      const [propertyName, propertyValue] = styleProperty.split(':');
+
+      if (!propertyName || !propertyValue) {
+        return styleAccumulator;
+      }
+
+      const trimmedName = propertyName.trim();
+      const trimmedValue = propertyValue.trim();
+
+      if (trimmedName && trimmedValue) {
+        styleAccumulator[trimmedName] = trimmedValue;
+      }
+
+      return styleAccumulator;
+    }, {});
+
+  return stylesList;
 }
 
 module.exports = convertToObject;

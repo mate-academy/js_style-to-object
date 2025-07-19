66d227d9d718d9e055eb0bb22ef5b6cd2093a188
Anna Polonnikova <anna.polonnikova@namecheap.com>
2025-07-19 17:58:42 +0000

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,17 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return Object.fromEntries(
+    sourceString
+      .split(';')
+      .map((style) => style.trim())
+      .filter((style) => style)
+      .map((style) => {
+        const [key, value] = style.split(':');
+
+        return [key.trim(), value.trim()];
+      }),
+  );
 }
 
 module.exports = convertToObject;

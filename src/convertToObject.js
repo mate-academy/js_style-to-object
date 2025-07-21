242cdd3628650e584a1ebcd39e67e8dc0ce74e26
Max Tovstopiatyi <maxrori228@gmail.com>
2025-07-21 16:08:22 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const allMeaning = sourceString.split(';');
+  const obj = {};
+
+  for (const meaning of allMeaning) {
+    const [key, value] = meaning.split(':').map((str) => str.trim());
+
+    obj[key] = value;
+  }
+
+  return obj;
 }
 
 module.exports = convertToObject;

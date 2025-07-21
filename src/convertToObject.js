9c5291261c1ccef9d78eb09f680ca57f0b071f57
lvlashnin <lvlashnin@gmail.com>
2025-07-21 18:29:17 +0300

    added convert logic

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultStyleObject = {};
+  const removedSpacesLines = sourceString
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line !== '');
+
+  removedSpacesLines.forEach((line) => {
+    const [rule, key] = line.split(':');
+    const trimmedRule = rule.trim();
+    const trimmedKey = key.trim();
+
+    resultStyleObject[trimmedRule] = trimmedKey;
+  });
+
+  return resultStyleObject;
 }
 
 module.exports = convertToObject;

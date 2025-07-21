d00e79d8b4319cac5d44ad787a8fe6caef51c40f
Max Tovstopiatyi <maxrori228@gmail.com>
2025-07-21 16:14:12 +0300

    fix issues

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,6 +10,10 @@ function convertToObject(sourceString) {
   const obj = {};
 
   for (const meaning of allMeaning) {
+    if (!meaning.trim()) {
+      continue;
+    }
+
     const [key, value] = meaning.split(':').map((str) => str.trim());
 
     obj[key] = value;

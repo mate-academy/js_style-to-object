057d70dd717b77fad4eae3f7be8741eb8dac38fa
danie010830 <danielgukillwar008@gmail.com>
2025-07-24 15:10:05 -0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,6 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
+  const result = {};
+
+  const declarations = sourceString.split(';');
+
+  for (const ch of declarations) {
+    const [prop, value] = ch.split(':');
+
+    if (prop && value) {
+      result[prop.trim()] = value.trim();
+    }
+  }
+
+  return result;
   // write your code here
 }
 

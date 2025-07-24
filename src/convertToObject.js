47c0839a105a4e6993f718b10fbffe85c0c2717b
Guilherme Gouvea <guilhermepontogouvea@gmail.com>
2025-07-24 15:08:01 -0300

    fixed filter declaration

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,7 +9,7 @@ function convertToObject(sourceString) {
   const finalObject = sourceString
     .split(';')
     .map((declaration) => declaration.trim())
-    .filter((declaration) => declaration !== '' && ';')
+    .filter((declaration) => declaration !== '')
     .reduce((acc, declaration) => {
       const parts = declaration.split(':');
 

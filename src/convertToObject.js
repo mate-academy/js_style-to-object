180d7e83fee2fff70a108625715706d4d532a617
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-07-26 14:05:30 +0300

    first version validator

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const lines = sourceString
+    .replace(/\t/g, '')
+    .split(';')
+    .map((item) => item.trim())
+    .filter((item) => item.includes(':'));
+
+  lines.forEach((string) => {
+    const sliceIndex = string.indexOf(':');
+
+    const key = string.slice(0, sliceIndex).trim();
+    const value = string.slice(sliceIndex + 1).trim();
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

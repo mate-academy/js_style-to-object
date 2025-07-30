92f57ab29f93e07706bab7f5ca25d497b7c099f3
Nikita Chupahin <nik123chupahin@gmail.com>
2025-07-30 15:48:56 +0300

    added task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .filter((style) => style && style.includes(':'))
+    .map((style) => {
+      const [property, ...valueParts] = style.split(':');
+      const key = property.trim();
+      const value = valueParts.join(':').replace(/^\s+|\s+$/g, '');
+
+      return [key, value];
+    })
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

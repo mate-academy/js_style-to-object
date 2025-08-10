293e92d8c866c4f34e5cbacc0607c151774f20f8
kirky359 <lol783176@gmail.com>
2025-08-10 13:16:00 +0300

    fix

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,30 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const objResult = {};
-
-  const array = sourceString
+  const stylePairs = sourceString
     .split(';')
     .map((el) => el.trim())
     .filter(Boolean)
+    .filter((el) => el.includes(':'))
     .map((el) => {
       const index = el.indexOf(':');
-
-      if (index === -1) {
-        return false;
-      }
-
       const key = el.slice(0, index).trim();
       const val = el.slice(index + 1).trim();
 
       return [key, val];
     });
 
-  for (let i = 0; i < array.length; i++) {
-    objResult[array[i][0]] = array[i][1];
-  }
-
-  return objResult;
+  return Object.fromEntries(stylePairs);
 }
 
 module.exports = convertToObject;

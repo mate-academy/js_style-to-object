583c8628c8fa59d245910ee528c20cef7f4f0f99
kirky359 <lol783176@gmail.com>
2025-08-10 13:08:56 +0300

    task completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const objResult = {};
+
+  const array = sourceString
+    .split(';')
+    .map((el) => el.trim())
+    .filter(Boolean)
+    .map((el) => {
+      const index = el.indexOf(':');
+
+      if (index === -1) {
+        return false;
+      }
+
+      const key = el.slice(0, index).trim();
+      const val = el.slice(index + 1).trim();
+
+      return [key, val];
+    });
+
+  for (let i = 0; i < array.length; i++) {
+    objResult[array[i][0]] = array[i][1];
+  }
+
+  return objResult;
 }
 
 module.exports = convertToObject;

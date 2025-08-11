b32699d55b79a257c286ce5b19c0d1979536b6ef
Oleh Prokopenko <olehprokopenko1@gmail.com>
2025-08-11 23:44:19 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,34 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const values = [];
+  let temp = '';
+
+  for (const char of sourceString) {
+    if (char === ';') {
+      if (temp.trim() !== '') {
+        values.push(temp.trim());
+        temp = '';
+      }
+    } else {
+      temp += char;
+    }
+  }
+
+  if (temp.trim()) {
+    values.push(temp.trim());
+  }
+
+  const keyAndVal = values.map((value) => {
+    const indexPosition = value.indexOf(':');
+
+    return [
+      value.slice(0, indexPosition).trim(),
+      value.slice(indexPosition + 1).trim(),
+    ];
+  });
+
+  return Object.fromEntries(keyAndVal);
 }
 
 module.exports = convertToObject;

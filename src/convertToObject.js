cb4b7816eaf39cca19a50ab7cce5d3c56d7b22c5
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-08-13 15:38:53 +0300

    second solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,16 +10,11 @@ function convertToObject(sourceString) {
 
   sourceString
     .split(';')
-    .map(function (style) {
-      return style.trim();
-    })
-    .filter(function (style) {
-      return style !== '' && style.includes(':');
-    })
+    .filter((style) => style.trim() !== '' && style.includes(':'))
     .forEach(function (style) {
       const parts = style.split(':');
       const key = parts[0].trim();
-      const value = parts.slice(1).join(':').trim();
+      const value = parts[1].trim();
 
       if (key && value) {
         styleObject[key] = value;

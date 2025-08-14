9e96b8d843f28aa8faa3a5f73c2662cc7d80dc88
sr.veremienko@gmail.com <sr.veremienko@gmail.com>
2025-08-14 22:24:45 +0300

    Added script to form css

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  return Object.fromEntries(
+    sourceString
+      .split(';')
+      .map((el) => el.trim())
+      .filter((noEmpty) => noEmpty)
+      .map((item) => {
+        const keyValue = item.split(':');
+        const key = keyValue[0].trim();
+        const value = keyValue[1].trim();
+
+        return [key, value];
+      }),
+  );
 }
 
 module.exports = convertToObject;

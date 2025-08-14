abdf367cc2a71f4e8b7120b2fa881eb36467b139
sr.veremienko@gmail.com <sr.veremienko@gmail.com>
2025-08-14 22:36:39 +0300

    Added script to form css - fix comments

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,14 +11,15 @@ function convertToObject(sourceString) {
     sourceString
       .split(';')
       .map((el) => el.trim())
-      .filter((noEmpty) => noEmpty)
+      .filter((noEmpty) => noEmpty && noEmpty.includes(':'))
       .map((item) => {
         const keyValue = item.split(':');
         const key = keyValue[0].trim();
         const value = keyValue[1].trim();
 
         return [key, value];
-      }),
+      })
+      .filter(([key, value]) => key && value),
   );
 }
 

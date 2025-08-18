d533866d788ade9e5387d7fecdaf85bf833602ab
diego69-foda <diego.bkuhn@gmail.com>
2025-08-18 11:22:04 -0300

    agora acho que passa

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -10,21 +10,21 @@ function convertToObject(sourceString) {
     .split(';') // separa pelas declarações
     .map((line) => line.trim()) // remove espaços extras
     .filter((line) => line.length > 0) // ignora vazios
-    .reduce((acc, declaration) => {
+    .reduce((style, declaration) => {
       const [property, ...valueParts] = declaration.split(':');
 
       if (!property || valueParts.length === 0) {
-        return acc;
+        return style;
       } // ignora inválidos
 
       const key = property.trim();
       const value = valueParts.join(':').trim(); // junta caso valor tenha ":"
 
       if (key && value) {
-        acc[key] = value;
+        style[key] = value;
       }
 
-      return acc;
+      return style;
     }, {});
 }
 

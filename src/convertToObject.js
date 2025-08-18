33047c6c13bc397c67fb7dff7f51463049e01271
diego69-foda <diego.bkuhn@gmail.com>
2025-08-18 11:16:12 -0300

    FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFUUUUUUUUUUUUUUUUUUUUUUUUUUUUUCCCCCCCCCCCCCCCCCCCCCCCCCCCCKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,19 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-return sourceString
+  return sourceString
     .split(';') // separa pelas declarações
-    .map(line => line.trim()) // remove espaços extras
-    .filter(line => line.length > 0) // ignora vazios
+    .map((line) => line.trim()) // remove espaços extras
+    .filter((line) => line.length > 0) // ignora vazios
     .reduce((acc, declaration) => {
       const [property, ...valueParts] = declaration.split(':');
-      if (!property || valueParts.length === 0) return acc; // ignora inválidos
+
+      if (!property || valueParts.length === 0) {
+        return acc;
+      } // ignora inválidos
 
       const key = property.trim();
       const value = valueParts.join(':').trim(); // junta caso valor tenha ":"
 
-      if (key && value) acc[key] = value;
+      if (key && value) {
+        acc[key] = value;
+      }
+
       return acc;
-    }, {});}
+    }, {});
+}
 
 module.exports = convertToObject;

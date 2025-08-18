3328f2ed46b67dfe9995b3cdd5fe2b569c6f2a57
Yuriy Gumenskyi <gumenskiy.yuriy@gmail.com>
2025-08-18 21:37:46 +0300

    Solution_2 of convertToObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -11,13 +11,17 @@ function convertToObject(sourceString) {
     .filter((pair) => pair.trim() !== '');
 
   function objCreator(objectAccumulator, cssDeclaration = '') {
-    const objKey = cssDeclaration.split(':')[0].trim();
-    const objValue = cssDeclaration.split(':')[1].trim();
+    if (!cssDeclaration.includes(':')) {
+      return objectAccumulator;
+    } else {
+      const objKey = cssDeclaration.split(':')[0].trim();
+      const objValue = cssDeclaration.split(':')[1].trim();
 
-    return {
-      ...objectAccumulator,
-      [objKey]: objValue,
-    };
+      return {
+        ...objectAccumulator,
+        [objKey]: objValue,
+      };
+    }
   }
 
   return cssDeclarations.reduce(objCreator, {});

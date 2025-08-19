406f34c506d1c7c935b7a2d384cd9a6ac82b7970
DropPredator <ionchiccc@gmail.com>
2025-08-19 17:32:28 +0300

    completed task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  if (sourceString === '') {
+    return {};
+  }
+
+  const properties = sourceString.split(';');
+  const finalObj = {};
+
+  for (const property of properties) {
+    const parts = property.split(':');
+    const propertyName = String(parts[0]).trim();
+    const propertyValue = String(parts[1]).trim();
+
+    if (propertyName === '') {
+      continue;
+    }
+
+    finalObj[propertyName] = propertyValue;
+  }
+
+  return finalObj;
 }
 
 module.exports = convertToObject;

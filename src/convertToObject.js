5cf31f600d21024951273f79ded45fd3815c5be5
Your Name <you@example.com>
2025-08-21 02:11:54 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,14 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  let styleObject = {};
+  const styleArray = sourceString.trim().split(';');
+
+  styleObject = Object.fromEntries(
+    styleArray.map((couple) => couple.split(':').map((item) => item.trim())),
+  );
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

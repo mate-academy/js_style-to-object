fe047f14aaccfb869a85ade0c0b2953383fe45d5
Naomi <naomidie1311@gmail.com>
2025-08-20 19:18:17 +0200

    styleToObject_nd_001

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';') // Split by semicolon
+    .map((decl) => decl.trim()) // Trim whitespace
+    .filter((decl) => decl && decl.includes(':'))
+    .map((decl) => {
+      const [property, ...rest] = decl.split(':');
+      const value = rest.join(':'); // In case value contains colons
+
+      return [property.trim(), value.trim()];
+    })
+    .reduce((acc, [key, val]) => {
+      if (key && val) {
+        acc[key] = val;
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

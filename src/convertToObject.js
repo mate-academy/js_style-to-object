546134905108b97033598070646ee43edbaabaec
FRANCISCOPELEGRINI <franciscoprcunha@gmail.com>
2025-08-21 16:17:03 -0300

    Mate Academy ConvertToObject

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let arr = sourceString
+    .split(';')
+    .map(sla => sla.trim())
+    .filter(sla => sla.length > 0);
+
+  let suporte = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    let [key, value] = arr[i].split(':');
+    if (key && value) {
+      let prop = key.trim();
+      let val = value.trim();
+      suporte[prop] = val;
+    }
+  }
+
+  return suporte;
 }
 
 module.exports = convertToObject;
+

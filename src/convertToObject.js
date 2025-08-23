4daa6d33eb778ea95a8af4f6f86de27f90c648cc
Yelyzaveta Bendiuh <lizabendug@gmail.com>
2025-08-24 00:16:32 +0200

    add solution 2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,22 +5,26 @@
  *
  * @return {object}
  */
-const result = convertToObject(complexStylesString);
+
 function convertToObject(convertableString) {
   const objectToReturn = {};
   const normalizedArray = convertableString
-  .split(';')
-  .map((part) => part.trim())
-  .filter((element) => element.length > 0);
+    .split(';')
+    .map((part) => part.trim())
+    .filter((element) => element.length > 0);
+
   fixAndMerge(normalizedArray, objectToReturn);
+
   return objectToReturn;
 }
+
 function fixAndMerge(array, object) {
   for (const element of array) {
     let key = element.split(':')[0].trim();
-    let valOfKey = element.split(':')[1].trim();
+    const valOfKey = element.split(':')[1].trim();
+
     if (key.includes('-')) {
-      key = `'${key}'`
+      key = `'${key}'`;
     }
     object[key] = valOfKey;
   }

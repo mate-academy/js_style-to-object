e6d2a5911dc560bdd5b124f51331ee3f2549e22b
Afengi <gorodny.ivan4@gmail.com>
2025-09-09 15:59:38 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styleObject = {};
+
+  sourceString
+    .split(';')
+    .map((s) => s.trim())
+    .filter(Boolean)
+    .forEach((declaration) => {
+      const [property, value] = declaration.split(':').map((s) => s.trim());
+
+      if (property && value) {
+        const camelCaseProperty = property;
+
+        styleObject[camelCaseProperty] = value;
+      }
+    });
+
+  return styleObject;
 }
 
 module.exports = convertToObject;

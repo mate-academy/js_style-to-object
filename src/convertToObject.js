68d98b680f390225b9da682a1f7407fd6652597d
VM93 <vovamuzyka93@gmail.com>
2025-09-11 22:19:53 +0300

    3rd Soolution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,29 +8,18 @@
 function convertToObject(sourceString) {
   // write your code here
 
-  // create an empty object
-  const result = {};
+  const stylesObject = sourceString
+    .split(';')
+    .filter((declaration) => declaration.includes(':'))
+    .reduce((acc, declaration) => {
+      const [property, value] = declaration.split(':');
 
-  // split the string
-  const splitedString = sourceString.split(';'); // has crated an array
+      acc[property.trim()] = value.trim();
 
-  // iterate the array
-  splitedString.forEach((stringOfArray) /* name of keyword is up to you */ => {
-    // check if the word exists before colon and skip if doesnt exist
-    if (stringOfArray.indexOf(':') > 0) {
-      // cut the word before colon with .slice()
-      // and skip any spaces before and after the word with .trim()
-      const key = stringOfArray.slice(0, stringOfArray.indexOf(':')).trim();
-      // cut the word after colon with .slice()
-      // and the same way with spaces by .trim()
-      const value = stringOfArray.slice(stringOfArray.indexOf(':') + 1).trim();
+      return acc;
+    }, {});
 
-      // add property with value to the object wich was created before
-      result[key] = value;
-    }
-  });
-
-  return result;
+  return stylesObject;
 }
 
 module.exports = convertToObject;

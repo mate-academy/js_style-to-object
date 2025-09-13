abcf1426b86792b7eb74882f1244d1317dda742f
Dima <dima2003strik@gmail.com>
2025-09-13 14:17:06 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,17 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  let arr = sourceString.split(';');
+
+  arr = arr.map((str) => str.trim());
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i].split(':').map((str) => str.trim());
+  }
+
+  return arr.reduce((accumulator, currentValue) => {
+    return { ...accumulator, [currentValue[0]]: currentValue[1] };
+  }, {});
 }
 
 module.exports = convertToObject;

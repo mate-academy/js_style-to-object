ba04ce691b7b2574e89c1c2a5466c0094bc9baff
Vladyslav Hartman <vladyslav.gartman@gmail.com>
2025-09-18 23:05:50 +0300

    added tast solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,27 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const strings = sourceString.split(';');
+
+  const filteredStrings = strings.filter(
+    (str) => str.trim() !== '' && str.includes(':'),
+  );
+
+  const pairs = filteredStrings.map((str) => {
+    const index = str.indexOf(':');
+    const key = str.slice(0, index).trim();
+    const value = str.slice(index + 1).trim();
+
+    return [key, value];
+  });
+
+  const result = pairs.reduce((style, [key, value]) => {
+    style[key] = value.replace(/;$/, '').trim();
+
+    return style;
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

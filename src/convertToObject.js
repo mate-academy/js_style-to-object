47875aae8562a2c6f6eace8466954532930a3d3d
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-09-25 17:27:50 +0300

    Solution before Q&A

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,18 +7,21 @@
  */
 function convertToObject(sourceString) {
   const properties = sourceString.split(';');
-  const normalize = properties.map((property) => ({
-    [property
+  const normalize = properties.map((property) => {
+    const keyValue = property
       .split('')
       .filter((char) => char !== '\n' || char !== ' ')
       .slice(0, property.indexOf(':'))
       .join('')
-      .trim()]: property
+      .trim();
+    const value = property
       .split('')
       .slice(property.indexOf(':') + 1)
       .join('')
-      .trim(),
-  }));
+      .trim();
+
+    return { [keyValue]: value };
+  });
 
   const convertedToObject = normalize.reduce((accummulator, currentObject) => {
     const key = Object.keys(currentObject)[0];

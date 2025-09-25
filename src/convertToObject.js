fcb5ce633bcd3e294181cdb04631bca25405ae7b
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-09-25 17:47:30 +0300

    Final Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,35 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const properties = sourceString.split(';');
-  const normalize = properties.map((property) => {
-    const keyValue = property
-      .split('')
-      .filter((char) => char !== '\n' || char !== ' ')
-      .slice(0, property.indexOf(':'))
-      .join('')
-      .trim();
-    const value = property
-      .split('')
-      .slice(property.indexOf(':') + 1)
-      .join('')
-      .trim();
+  const properties = sourceString
+    .split(';')
+    .map((item) => item.split(':'))
+    .filter((item) => item.length === 2);
 
-    return { [keyValue]: value };
+  const formatedObject = {};
+
+  properties.map((item) => {
+    const propKey = item[0].trim();
+    const propValue = item[1].trim();
+
+    formatedObject[propKey] = propValue;
   });
 
-  const convertedToObject = normalize.reduce((accummulator, currentObject) => {
-    const key = Object.keys(currentObject)[0];
-    const value = currentObject[key];
-
-    if (key.length > 0 && value.length > 0) {
-      accummulator[key] = value;
-    }
-
-    return accummulator;
-  }, {});
-
-  return convertedToObject;
+  return formatedObject;
 }
 
 module.exports = convertToObject;

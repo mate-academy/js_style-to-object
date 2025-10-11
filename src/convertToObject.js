2f49b9390e9ba21042a07a04605c32cb0f6eb491
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-11 08:43:25 -0300

    development of style to object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,58 @@
  *
  * @return {object}
  */
+
+const complexStylesString = `
+  background-color:      #fff;
+-webkit-border-radius: 5px;
+  border-radius     : 5px;
+  border: 1px solid #e8e8e8;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  clear   : both       ;
+  cursor: pointer;
+  float: left;
+  font-family: inherit;
+      font-size: 14px;
+  font-weight: 400;
+  height: 42px;
+  line-height:    40px;
+  outline: 0;
+  padding-left    : 18px;
+  padding-right: 30px;
+  ;
+
+  ;
+  position: relative;
+
+
+  text-align: left !important;
+  -webkit-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+
+  white-space: nowrap;
+  width: auto;
+`;
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+  .split(';')
+  .map(decl => decl.trim())
+  .filter(decl => decl.includes(':'))
+  .reduce((accumulator, decl) => {
+    const [property, value] = decl.split(':').map(part => part.trim());
+    accumulator[property] = value;
+    return accumulator;
+  }, {});
+
+
 }
+
+const result = convertToObject(complexStylesString);
+
+console.log(result);
 
 module.exports = convertToObject;

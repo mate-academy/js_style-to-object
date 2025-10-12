bff6a2c64edf1fcfd8c4efeb3305f8f23aa82f9b
Andrey <inokatre08@gmail.com>
2025-10-12 22:23:23 +0300

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,13 +8,20 @@
 function convertToObject(sourceString) {
   return sourceString
     .split(';')
+    .map((line) => line.trim())
     .filter((line) => line.includes(':'))
-    .map((line) => line.split(':'))
-    .map(([key, value]) => [key.trim(), value.trim()])
-    .reduce((acc, [key, value]) => {
-      acc[key] = value;
+    .map((line) => {
+      const colonIndex = line.indexOf(':');
+      const property = line.slice(0, colonIndex).trim();
+      const value = line.slice(colonIndex + 1).trim();
 
-      return acc;
+      return [property, value];
+    })
+    .filter(([property, value]) => property !== '' && value !== '')
+    .reduce((stylesObject, [property, value]) => {
+      stylesObject[property] = value;
+
+      return stylesObject;
     }, {});
 }
 

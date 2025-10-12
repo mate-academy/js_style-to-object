499aa22d908afe9a18070caf113069a024f8bb67
Elber-Kevenny <vieirabeb@gmail.com>
2025-10-11 23:03:58 -0300

    new solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,24 +7,83 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-  const convert = {};
+  const parts = sourceString
+    .split(';')
+    .map((x) => x.trim())
+    .filter((x) => x.length)
+    .map((x) => {
+      const i = x.indexOf(':');
 
-  const cssStyles = sourceString.trim().split(';');
+      if (i === -1) {
+        return null;
+      } else {
+        const key = x.slice(0, i).trim();
+        const value = x.slice(i + 1).trim();
 
-  for (const item of cssStyles) {
-    const idx = item.indexOf(':');
+        return [key, value];
+      }
+    })
+    .filter((x) => Array.isArray(x) && x.length === 2);
 
-    if (idx === -1) {
-      continue;
-    }
+  const result = parts.reduce((acc, [key, value]) => {
+    acc[key] = value;
 
-    const key = item.slice(0, idx).trim();
-    const value = item.slice(idx + 1).trim();
+    return acc;
+  }, {});
 
-    convert[key] = value;
-  }
-
-  return convert;
+  return result;
 }
 
 module.exports = convertToObject;
+
+// parts com Split =
+/* ['background-color:      #fff;',
+'-webkit-border-radius: 5px;',
+'  border-radius     : 5px;',
+'  border: 1px solid #e8e8e8   ';'] */
+
+/* parts com map e trim
+  Map vai iterar por cada elemento e aplicar trim
+  em cada (trim vai remover espaços no começo e no fin)
+
+  parts com map e trim = [
+'background-color:      #fff;',
+'-webkit-border-radius: 5px;',
+'border-radius     : 5px;',
+'border: 1px solid #e8e8e8;'] */
+
+/* parts com filter
+Esse filtro remove elementos vazios — ou seja,
+qualquer item com .length === 0 será excluído.
+
+*/
+
+/* parts com map. Nesse passo o map vai iterar por cada elemento e retornar o
+indice de ':'
+[
+'background-color:      #fff;',
+'-webkit-border-radius: 5px;',
+'border-radius     : 5px;',
+'border: 1px solid #e8e8e8;']
+
+se a posição do ':' for -1 significa que ':' nao existe no elemento
+se não, crie a variavel key e atribua o elemento que fica antes de
+':' usando o slice.
+fica assim: key = ['background-color:'], ['-webkit-border-radius:']
+mesma coisa para value, só que agora pegamos o elemento depois de ':',
+fica assim: value = ['#fff'], ['5px;']. lembrando que o trim remove os espaços
+desses elementos.
+
+return [key, value] em 1 só array.
+fica assim:
+[
+['background-color:', '#fff'],
+['-webkit-border-radius', '5px;']
+]
+filter.length elimina elementos vazios
+slice(0, 2) cria arrays com start em 0 e end em 2
+- Extrai os dois primeiros elementos do array filtrado.
+- slice(início, fim) retorna uma cópia parcial do array.
+- Não altera o array original(ele não inclui o elemento numero 2, end)
+
+*/

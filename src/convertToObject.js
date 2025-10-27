b184cb0a5a90281d03a2406cd65f346b8b2c2b13
Horoshko Vitaliy <horoshko29@gmail.com>
2025-10-27 23:30:05 +0100

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,24 @@
-'use strict';
-
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const list = sourceString.split(';');
+  const result = {};
+  let name = '';
+  let value = '';
+
+  for (let i of list) {
+    if (i.includes(':')) {
+      i = i.split(':');
+      name = i[0].trim();
+      value = i[1].trim();
+      result[name] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

b392a72488ee0a916c2dbb2a6aa582157ca9d447
Mukola <vmv.vynnychuk98@gmail.com>
2025-11-01 10:03:50 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map(Item => Item.trim())
+    .filter(Boolean)
+    .reduce((newObj, Item) => {
+      const colonIndex = Item.indexOf(':');
+      if (colonIndex === -1) return newObj;
+
+      const key = Item.slice(0, colonIndex).trim();
+      const value = Item.slice(colonIndex + 1).trim();
+
+      newObj[key] = value;
+      return newObj;
+    }, {});
 }
 
 module.exports = convertToObject;
+

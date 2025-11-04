5cbbff8bb1f184164bb169063d34de894cac46a4
unknown <iogorrochamiranda2@gmail.com>
2025-11-04 03:20:58 -0300

    Solução para string desordenada usando métodos JS

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const secoes = sourceString.split(';'); // divide pelos espaços ' '
+  const tratamentoDeLimpeza = secoes
+    .map((partes) => partes.trim()) // tira espaços vazios de cada parte
+    .filter((index) => index !== ''); // tira os espaços do arry
+
+  const objetoFormatado = {};
+
+  tratamentoDeLimpeza.forEach((elemento) => {
+    // pega cada elemento do arry e divide nos ':'
+    const partes = elemento.split(':');
+    // tira todos os espaços antes e depois de cada elemento do arry
+    const chave = partes[0].trim();
+    const valor = partes[1].trim();
+    // adiciona ao obj a chave e valor já formatados
+
+    objetoFormatado[chave] = valor;
+  });
+
+  return objetoFormatado;
 }
 
 module.exports = convertToObject;

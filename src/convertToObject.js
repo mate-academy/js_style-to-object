e1502b550bd9541660adac91af544be620cebecb
Alex <1inboxna@gmail.com>
2025-11-14 21:06:57 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,30 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  if (!sourceString.trim()) {
+    return {};
+  }
+
+  const parts = sourceString.split(';');
+  const cleaned = parts.map((el) => el.trim()).filter((el) => el !== '');
+  const result = {};
+
+  cleaned.forEach((rule) => {
+    const colonIndex = rule.indexOf(':');
+
+    if (colonIndex === -1) {
+      return;
+    }
+
+    const key = rule.slice(0, colonIndex).trim();
+    const value = rule.slice(colonIndex + 1).trim();
+
+    result[key] = value;
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

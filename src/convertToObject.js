591a43227b1db8a28731657ccf31ea0b10367e5a
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-11-18 18:29:35 +0200

    Add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const lines = sourceString.split(';');
+  const declarations = lines
+    .map((line) => line.trim())
+    .filter((line) => line.length);
+  const result = declarations.reduce((acc, declaration) => {
+    const [key, value] = declaration.split(':').map((part) => part.trim());
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

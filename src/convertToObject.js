42e43af9e978418e5dfbff7f4c679be48c47d7a5
Taras Yelahin <inflades@gmail.com>
2025-11-18 23:34:53 +0200

    style to object task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,17 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const declarations = sourceString.split(';');
+  const trimmedDeclarations = declarations.map((element) => element.trim());
+  const cssArray = trimmedDeclarations.reduce((status, element) => {
+    const parts = element.split(':').map((part) => part.trim());
+
+    return { ...status, [parts[0]]: parts[1] };
+  }, {});
+
+  return cssArray;
 }
 
 module.exports = convertToObject;

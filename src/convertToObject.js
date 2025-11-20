2c8a8f6946b38560c916f6f443de61496eec44ef
maksbtw <maks.20050817@gmail.com>
2025-11-20 20:53:58 +0100

    fixed checklist mistakes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,19 +7,16 @@
  */
 function convertToObject(sourceString) {
   const stylesList = sourceString.split(';').map((line) => line.trim());
-  const result = {};
 
-  for (const style of stylesList) {
-    const separatedLine = style.split(':');
+  return stylesList.reduce((prev, style) => {
+    const [prop, value] = style.split(':');
 
-    if (separatedLine.length === 2) {
-      const [prop, value] = separatedLine;
-
-      result[prop.trim()] = value.trim();
+    if (prop && value) {
+      return { ...prev, [prop.trim()]: value.trim() };
     }
-  }
 
-  return result;
+    return prev;
+  }, {});
 }
 
 module.exports = convertToObject;

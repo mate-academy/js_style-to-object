0b985adc215efac4998866527bf6b3f04501c445
unknown <dkuz6323@gmail.com>
2025-12-01 18:27:33 +0200

    completed tasks

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,19 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const sourceSplit = sourceString.split(';');
+
+  const result = {};
+
+  sourceSplit.forEach((element) => {
+    const objectValues = element.split(':');
+
+    if (objectValues[1]) {
+      result[objectValues[0].trim(' ')] = objectValues[1].trim(' ');
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;

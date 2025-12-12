5271a27a107c93ed2a5edb63e458683aba874588
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2025-12-12 23:31:31 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,27 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+
+  if (sourceString.length === 0) {
+    return result;
+  }
+
+  sourceString
+    .split(';')
+    .map((el) =>
+      el
+        .trim()
+        .split(':')
+        // eslint-disable-next-line prettier/prettier
+        .map((item) => item.trim()))
+    .forEach((el) => {
+      if (el.length !== 1) {
+        result[el[0]] = el[1];
+      }
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

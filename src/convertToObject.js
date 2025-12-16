f7260392e7f51750d53f43181a4b4b55abc9fbe0
Dmytro Krylov <dimononk@gmail.com>
2025-12-16 16:34:08 +0200

    fix var naming

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,16 +8,19 @@
 function convertToObject(sourceString) {
   const properties = sourceString.split(';');
 
-  return properties.reduce((obj, prop) => {
+  return properties.reduce((styleObject, prop) => {
     if (prop.trim() !== '') {
       const parts = prop.split(':');
-      const key = parts[0].trim();
-      const value = parts[1].replace(';', '').trim();
 
-      obj[key] = value;
+      if (parts[0] !== undefined) {
+        const key = parts[0].trim();
+        const value = parts[1].replace(';', '').trim();
+
+        styleObject[key] = value;
+      }
     }
 
-    return obj;
+    return styleObject;
   }, {});
 }
 

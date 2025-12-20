637694eea543a9922175640a3d9ade287a3e5880
Stanislav Mosakov <ayfonsky@gmail.com>
2025-12-20 17:38:04 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const transformObject = {};
+  const stringToArr = sourceString.split(';');
+
+  for (let i = 0; i < stringToArr.length; i++) {
+    const lineTrim = stringToArr[i].trim();
+
+    if (lineTrim.length !== 0) {
+      const partLine = lineTrim.split(':');
+      let key;
+      let value;
+
+      for (let j = 0; j < partLine.length; j++) {
+        const valueTrim = partLine[j].trim();
+
+        if (j === 0) {
+          key = valueTrim;
+        }
+
+        if (j === 1) {
+          value = valueTrim;
+        }
+      }
+      transformObject[key] = value;
+    }
+  }
+
+  return transformObject;
 }
 
 module.exports = convertToObject;

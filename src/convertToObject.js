9fcead6077874c7fd1b43aef25b1a4d8f552463c
vladahvala <80634246+vladahvala@users.noreply.github.com>
2025-12-21 23:00:11 +0200

    solved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const obj = {};
+
+  // Розбиваємо весь рядок по ";", бо кожне правило CSS закінчується ;
+  sourceString.split(';').forEach((rule) => {
+    let string = rule;
+
+    string = rule.trim();
+
+    if (!string) {
+      return;
+    }
+
+    const [key, ...valueParts] = rule.split(':');
+
+    if (!valueParts.length) {
+      return;
+    }
+
+    // eslint-disable-next-line max-len
+    const value = valueParts.join(':').trim();
+
+    obj[key.trim()] = value;
+  });
+
+  return obj;
 }
 
 module.exports = convertToObject;

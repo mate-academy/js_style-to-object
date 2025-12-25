8a9a004dbbbb8b212dd9362db9177f9d534b5115
Nastia Mazur <anastasiamazur2006@gmail.com>
2025-12-25 16:01:16 +0200

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .trim()
+    .split(';')
+    .reduce((mas, el) => {
+      const cleanLine = el.trim();
+
+      if (cleanLine.length > 4) {
+        const [key, value] = cleanLine.split(':');
+
+        if (key && value) {
+          mas[key.trim()] = value.trim();
+        }
+      }
+
+      return mas;
+    }, {});
 }
 
 module.exports = convertToObject;

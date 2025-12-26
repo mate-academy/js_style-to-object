64bc208f60374d7fe7a6d694b7367d7cd8f7c19c
Maksym Kolomiiets <maksymkolomiiets2@gmail.com>
2025-12-26 15:31:51 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,22 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
-}
+function convertToObject(stylesString) {
+  return stylesString
+    .split(';')
+    .map((styleRule) => styleRule.trim())
+    .filter((styleRule) => styleRule !== '')
+    .reduce((stylesAccumulator, styleRule) => {
+      const colonIndex = styleRule.indexOf(':');
 
+      const propertyName = styleRule.slice(0, colonIndex).trim();
+      const propertyValue = styleRule.slice(colonIndex + 1).trim();
+
+      // Використовуємо spread-оператор, щоб не мутувати акумулятор
+      return {
+        ...stylesAccumulator,
+        [propertyName]: propertyValue,
+      };
+    }, {});
+}
 module.exports = convertToObject;

3a2b6497545090404655d067857ffaab2917dc5b
Ariane <bittencourt98ari@gmail.com>
2025-12-29 22:05:45 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((part) => part.trim())
+    .filter((part) => part !== '' && part.includes(':'))
+    .map((declaration) => {
+      const [prop, ...valueParts] = declaration.split(':');
+
+      return [prop.trim(), valueParts.join(':').trim()];
+    })
+    .filter(([key, value]) => key !== '' && value !== '')
+    .reduce((acc, [key, value]) => {
+      acc[key] = value;
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

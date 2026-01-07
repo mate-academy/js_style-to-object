bbfb6e3f0d360784ad65246edd0decd5945bb410
Nazar <morhunnaza200516@gmail.com>
2026-01-07 21:20:53 +0200

    complate

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,27 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+
+function convertToObject(stylesString) {
+  const obj = {};
+
+  stylesString
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter((rule) => rule)
+    .forEach((rule) => {
+      const [key, ...rest] = rule.split(':');
+
+      if (!key || rest.length === 0) {
+        return;
+      }
+
+      const value = rest.join(':').trim();
+
+      obj[key.trim()] = value;
+    });
+
+  return obj;
 }
 
 module.exports = convertToObject;

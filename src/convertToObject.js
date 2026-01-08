c403ca291cdb5e59107a390bc89b0db988758918
Mykyta <f1xggofficial@gmail.com>
2026-01-08 09:21:31 +0100

    solved

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString
+    .split(';') // Розбиваю рядок по ;
+    .map((line) => line.trim()) // Прибираю зайві пробіли в рядку
+    .filter(Boolean) // Прибираю порожні рядки
+    .forEach((line) => {
+      const [key, ...rest] = line.split(':');
+      // Розбиття рядка на ключ і решту (значення)
+
+      if (!key || rest.length === 0) {
+        return;
+      } // Якщо немає ключа, або значення то пропуск
+
+      const value = rest.join(':').trim();
+
+      result[key.trim()] = value;
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

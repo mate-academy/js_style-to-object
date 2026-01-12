08bf4482d6c4dceb621d5496d074b7a50dcd810e
Victoria Neustroeva <neustrvk@gmail.com>
2026-01-12 22:52:18 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const clean = sourceString
+    .trim()
+    .replace(/\s*:\s*/g, ':')
+    .replace(/\s*;\s*/g, ';');
+
+  const sourceArr = clean
+    .split(';')
+    .filter(Boolean)
+    .map((element) => element.split(':'));
+
+  const object = Object.fromEntries(sourceArr);
+
+  return object;
 }
 
 module.exports = convertToObject;

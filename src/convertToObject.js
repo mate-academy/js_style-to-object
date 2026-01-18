f637b68bd4ff9f38cf3b42db9afa34da8d442dd2
Yaroslav Synytsia <yaroslavsynytsia@gmail.com>
2026-01-18 17:27:47 +0200

    created solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((s) => s.trim())
+    .filter(Boolean)
+    .reduce((acc, item) => {
+      const colonIndex = item.indexOf(':');
+
+      if (colonIndex !== -1) {
+        const key = item.slice(0, colonIndex).trim();
+        const value = item.slice(colonIndex + 1).trim();
+
+        acc[key] = value;
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

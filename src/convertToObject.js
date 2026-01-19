ad7785514ad14c192d204024ef3420406b8c66b6
Adrian Holka <goholka1997@gmail.com>
2026-01-19 08:59:48 +0100

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const regex = /([^:;\s]+)[ \t\r\n]*:[ \t\r\n]*([\s\S]*?)[ \t\r\n]*(?:;|$)/g;
+
+  let match;
+
+  while ((match = regex.exec(sourceString)) !== null) {
+    const property = match[1].replace(/^[ \t]+|[ \t]+$/g, '');
+    const value = match[2].replace(/^[ \t]+|[ \t]+$/g, '');
+
+    if (property) {
+      result[property] = value;
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

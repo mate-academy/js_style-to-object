b48b25ffae794fae8c8abcb43832db1beacb9aea
konradtech2 <konradgithub@gmail.com>
2026-01-20 20:10:24 +0100

    Solving taks

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  if(sourceString.length === 0) {
+    return {};
+  }
+ let newObject = {};
+ let splittedArray = sourceString.split(';');
 
+  for(let i = 0; i < splittedArray.length; i++) {
+    let newSplittedArray = splittedArray[i].split(':');
+    
+
+    if(newSplittedArray.length === 2) {
+      newObject[newSplittedArray[0].trim()] = newSplittedArray[1].trim();
+    }
+   
+  }
+
+  return newObject;
+}
 module.exports = convertToObject;

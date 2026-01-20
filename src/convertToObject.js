f652faf44aa2695e5bb717cfcfe92512d50fb2fd
konradtech2 <konradgithub@gmail.com>
2026-01-20 20:30:33 +0100

    Solving task 2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,19 +9,29 @@ function convertToObject(sourceString) {
   if(sourceString.length === 0) {
     return {};
   }
- let newObject = {};
- let splittedArray = sourceString.split(';');
 
-  for(let i = 0; i < splittedArray.length; i++) {
-    let newSplittedArray = splittedArray[i].split(':');
-    
+ let styleProperties = {};
+ let declarations = sourceString.split(';');
 
-    if(newSplittedArray.length === 2) {
-      newObject[newSplittedArray[0].trim()] = newSplittedArray[1].trim();
+  declarations.forEach(declaration => {
+
+    const parts = declaration.split(':');
+
+    if(parts.length === 2) {
+
+      styleProperties[parts[0].trim()] = parts[1].trim();
+
     }
-   
-  }
+  })
 
-  return newObject;
+  return styleProperties;
 }
 module.exports = convertToObject;
+
+
+// let newSplittedArray.forEach((el) => el.split(';'))
+//declarations.forEach(declaration => {
+  // 1. Podziel 'declaration' za pomocą ':'
+  // 2. Sprawdź, czy po podziale masz 2 elementy
+  // 3. Jeśli tak, wyczyść je (trim) i dodaj do 'styles'
+//});

77e111a41b3de24c0535f4aea270f35f0fc9dfb3
DenisNosov22 <dennosov2210@gmail.com>
2026-01-27 15:43:57 +0200

    Sol-1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .split(';')
+    .map((rule) => rule.trim())
+    .filter((rule) => rule && rule.includes(':'))
+    .reduce((result, rule) => {
+      const [property, ...valueParts] = rule.split(':');
+      const value = valueParts.join(':').trim();
+
+      result[property.trim()] = value;
+
+      return result;
+    }, {});
 }
 
 module.exports = convertToObject;

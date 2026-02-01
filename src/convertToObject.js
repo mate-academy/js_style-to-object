4456b6fe6e099e83119f875cbc58b620201427fc
Serhii Shimko <shimkosergij2010@gmail.com>
2026-02-01 17:42:21 +0200

    completed the task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const splitString = sourceString.split(';');
+  const result = splitString
+    .filter((element) => {
+      if (element.trim() === '') {
+        return false;
+      }
+
+      return true;
+    })
+    .map((string) => {
+      const splitOneString = string.split(':');
+      const [key, value] = splitOneString;
+
+      return [key.trim(), value.trim()];
+    })
+    .reduce((sum, element) => {
+      const [key, value] = element;
+
+      sum[key] = value;
+
+      return sum;
+    }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

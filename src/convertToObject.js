1871b546b509089d558709e2ebd878120aabf1fa
Dmytro Omelchuk <swat04123@gmail.com>
2026-02-14 10:42:29 +0200

    Add convertToObject implementation

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const parts = sourceString.split(';');
+  const line = parts.map((item) => item.trim()).filter((item) => item);
+  const pairs = line.map((r) => r.split(':'));
+  const parsedData = {};
+
+  pairs.forEach((pair) => {
+    const key = pair[0].trim();
+    const value = pair[1].trim();
+
+    parsedData[key] = value;
+  });
+
+  return parsedData;
 }
 
 module.exports = convertToObject;

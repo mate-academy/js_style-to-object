d030d35374dae0363eb30431d0a9b93521806cf0
Marcos Vinicius <marcos_araujo7@hotmail.com>
2026-02-16 22:51:09 -0300

    develop

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,23 @@
 'use strict';
 
-/**
- * @param {string} sourceString
- *
- * @return {object}
- */
 function convertToObject(sourceString) {
-  // write your code here
+  let sourceArr = sourceString;
+
+  sourceArr = sourceArr.split(';');
+  sourceArr = sourceArr.filter((item) => item.trim() !== '');
+
+  const sourceToObject = sourceArr.reduce((acc, source) => {
+    const sourceEle = source.split(':');
+
+    const key = sourceEle[0].trim();
+    const value = sourceEle[1].trim();
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
+
+  return sourceToObject;
 }
 
 module.exports = convertToObject;

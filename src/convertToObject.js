e6aa40bd09789fbe1f1a626e9efd68f21f66d904
eduferrs <edu.ferr96@gmail.com>
2026-02-23 14:09:15 -0300

    done

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let parts = sourceString
+    .split(/[:;]/)
+    .map((p) => p.trim())
+    .filter((p) => p.length > 0);
+
+  parts = parts.reduce((prev, part, i, self) => {
+    if (i % 2 === 0) {
+      const chave = part;
+      const valor = self[i + 1];
+
+      prev[chave] = valor;
+    }
+
+    return prev;
+  }, {});
+
+  return parts;
 }
 
 module.exports = convertToObject;

1997b5ef9a9bd9d674642695ed08632ce2c7d94f
Katrysya <katrysyar@gmail.com>
2026-03-02 11:25:32 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const newStyleString = {};
+  const eachWord = sourceString.split(/[;:]/).map((el) => el.trim());
+
+  const temp = eachWord.filter((el) => el !== '');
+
+  for (let i = 0; i < temp.length; i += 2) {
+    newStyleString[temp[i]] = temp[i + 1];
+  }
+
+  return newStyleString;
 }
 
 module.exports = convertToObject;

1c487b616946b67633942e0dbe1d0ad03b774394
Stanislav <parustas@gmail.com>
2026-03-03 11:24:32 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString.split(';').reduce((acc, currentLine) => {
+    const trimmedLine = currentLine.trim();
+
+    if (trimmedLine) {
+      const [key, ...valueParts] = trimmedLine.split(':');
+      const propName = key.trim();
+      const propValue = valueParts.join(':').trim();
+
+      acc[propName] = propValue;
+    }
+
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;

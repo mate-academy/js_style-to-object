cc773b1a851f11d6b0efddd51b6da41824ea8d17
kateryna Habchak <kvizovapidtrimka@gmail.com>
2026-03-15 18:38:14 +0100

    add changes

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,7 +8,7 @@
 function convertToObject(sourceString) {
   return sourceString
     .split(';')
-    .filter((styleObject) => styleObject.trim())
+    .filter((stylesObject) => stylesObject.trim())
     .reduce((obj, style) => {
       const [key, value] = style.split(':');
 

78bddff3d3ef6dc8a7075d0ec46555a0133872bd
Thiago Moro do Carmo <thiago.moro77@gmail.com>
2026-03-20 13:03:24 -0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,31 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString
+    .replaceAll('\n', '')
+    .split(';')
+    .filter((string) => {
+      return string.trim() !== '';
+    })
+    .map((objString) => {
+      let [key, value] = objString.split(':');
+
+      key = key.trim();
+
+      if (value.includes(',')) {
+        value = value.replaceAll(',', ',\n').trim();
+      } else {
+        value = value.trim();
+      }
+
+      return { [key]: value };
+    })
+    .reduce((agregate, keyValue) => {
+      return {
+        ...agregate,
+        ...keyValue,
+      };
+    }, {});
 }
 
 module.exports = convertToObject;

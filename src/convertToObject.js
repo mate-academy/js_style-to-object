41affcf05131e80407eb8a7849594da50eb72151
linska <linskatanya@gmail.com>
2026-04-07 18:09:53 +0300

    feat: add convertToObject function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,19 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  return sourceString.split(';').reduce((acc, item) => {
+    const [rawKey, rawValue] = item.split(':');
+    const key = rawKey && rawKey.replace(/\s/g, '');
+    const value = rawValue && rawValue.trim();
+
+    if (key && value) {
+      acc[key] = value;
+    }
+
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;

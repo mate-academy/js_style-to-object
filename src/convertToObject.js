a7fb08bb2e1e42e0c3e1e23748e112cef442bc1d
Roman Lysunets <rlysunets@gmail.com>
2026-04-09 18:10:21 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const arr = sourceString.split(';').map((el) => el.trim());
+  const arrOfArrs = arr.map((el) => el.split(':'));
+
+  const res = {};
+
+  for (let i = 0; i < arrOfArrs.length; i++) {
+    if (arrOfArrs[i].length > 1) {
+      const key = arrOfArrs[i][0].trim();
+      const val = arrOfArrs[i][1].trim();
+
+      res[key] = val;
+    }
+  }
+
+  return res;
 }
 
 module.exports = convertToObject;

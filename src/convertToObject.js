fa5cfa9da293b1ea92943c3890683a61c6c2e295
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2026-04-10 16:30:23 +0300

    implement reduce instead loop

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,24 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const converted = {};
   const trimmed = sourceString
     .trim()
     .split(';')
     .map((style) => style.trim())
     .filter((style) => style && style.indexOf(':') !== -1);
 
-  trimmed.forEach((style) => {
+  const convertedStyles = trimmed.reduce((acc, style) => {
     const colonIndex = style.indexOf(':');
     const key = style.slice(0, colonIndex).trim();
     const value = style.slice(colonIndex + 1).trim();
 
     if (key) {
-      converted[key] = value;
+      acc[key] = value;
     }
-  });
 
-  return converted;
+    return acc;
+  }, {});
+
+  return convertedStyles;
 }
 
 module.exports = convertToObject;

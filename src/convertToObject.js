8c9a272bd712cd50591072b1a4724bc90ecbddbc
Михайло <mykhailo@MacBook-Air-Mihajlo.local>
2026-04-10 16:11:30 +0300

    solved after review

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,25 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const convertedObject = {};
-  const trimmedArr = sourceString
+  const converted = {};
+  const trimmed = sourceString
     .trim()
     .split(';')
     .map((style) => style.trim())
     .filter((style) => style && style.indexOf(':') !== -1);
 
-  for (const style of trimmedArr) {
+  trimmed.forEach((style) => {
     const colonIndex = style.indexOf(':');
     const key = style.slice(0, colonIndex).trim();
-
     const value = style.slice(colonIndex + 1).trim();
 
     if (key) {
-      convertedObject[key] = value;
+      converted[key] = value;
     }
-  }
+  });
 
-  return convertedObject;
+  return converted;
 }
 
 module.exports = convertToObject;

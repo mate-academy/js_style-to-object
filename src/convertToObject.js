4cde0359c503481423d09158228619435fd0846f
NazariiKaban <fedak_3efirka@ukr.net>
2026-04-29 12:47:23 +0200

    firstSolution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,18 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const sourceStringSplitted = sourceString
+    .split(';')
+    .map((item) => item.trim())
+    .map((item) => item.split(':').map((part) => part.trim()));
+
+  return sourceStringSplitted.reduce((accumulator, [key, value]) => {
+    accumulator[key] = value;
+
+    return accumulator;
+  }, {});
 }
 
 module.exports = convertToObject;

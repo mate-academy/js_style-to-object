ef408f68ade82fc6f52351f01c2ffd775714256f
Vitalii Hoshuliak <goshulyakvitalik@gmail.com>
2026-05-06 19:54:51 +0100

    19/54

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,20 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  return stylesString
+    .split(';')
+    .map((s) => s.trim())
+    .filter(Boolean)
+    .reduce((acc, decl) => {
+      const [prop, value] = decl.split(':');
+
+      if (prop && value) {
+        acc[prop.trim()] = value.trim();
+      }
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

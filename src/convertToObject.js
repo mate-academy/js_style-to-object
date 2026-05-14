d371e5060d4ae4a4753f7b22e2fb84335790e3d7
interpunktus <240490702+interpunktus@users.noreply.github.com>
2026-05-14 22:25:15 +0300

    try1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,18 @@
  *
  * @return {object}
  */
-function convertToObject(sourceString) {
-  // write your code here
+function convertToObject(stylesString) {
+  return stylesString
+    .split(';')
+    .map((pair) => pair.trim())
+    .filter((pair) => pair.includes(':'))
+    .reduce((acc, pair) => {
+      const [property, value] = pair.split(':').map((s) => s.trim());
+
+      acc[property] = value;
+
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

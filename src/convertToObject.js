48c88942c12c40191bec608912bdbd5c00eb0738
VoloGor <goralvv@gmail.com>
2026-05-14 15:36:56 +0200

    Convert Style String to Object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,18 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const declarations = sourceString
+    .split(';')
+    .filter((item) => item.trim() !== '');
+  const result = {};
+
+  for (const declaration of declarations) {
+    const [key, value] = declaration.split(':');
+
+    result[key.trim()] = value.trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

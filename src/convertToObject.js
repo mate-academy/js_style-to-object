1fc667bfeaa2a45cba418b0b4786e4a666e55837
Iryna Kaida <kaidairyna@gmail.com>
2026-05-16 21:08:13 +0300

    add convertToObject function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
-}
+  const parts = sourceString.split(`;`);
+  const trimmed = parts.map((x) => x.trim());
+  const filtered = trimmed.filter((x) => x.length > 0);
+  const pair = filtered.map((x) => x.split(`:`));
+  const result = pair.reduce((acc, x) => {
+    acc[x[0].trim()] = x[1].trim();
 
+    return acc;
+  }, {});
+
+  return result;
+}
 module.exports = convertToObject;

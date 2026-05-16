9beced6eb11505bebb8aa3f30221db1c1562c6fa
Mariana <zh.yu.st22@gmail.com>
2026-05-16 13:38:50 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,6 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
+  const result = sourceString.split(';');
+  const realResult = {};
+
+  for (const item of result) {
+    const triM = item.trim();
+
+    if (!triM) {
+      continue;
+    }
+
+    const index = triM.indexOf(':');
+    const key = triM.slice(0, index).trim();
+    const value = triM.slice(index + 1).trim();
+
+    realResult[key] = value;
+  }
+
+  return realResult;
   // write your code here
 }
 

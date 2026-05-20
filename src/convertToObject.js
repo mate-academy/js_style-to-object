3eab173bc1bf7bb348383c1a52452e880b49a361
Danyliuk Yaroslav <unityismylove@gmail.com>
2026-05-20 15:16:17 +0300

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,27 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let dividedString = sourceString.split(';');
+
+  dividedString = dividedString
+    .map((item) => item.trim())
+    .filter((item) => item !== '');
+
+  const styles = Object.fromEntries(
+    dividedString.map((item) => {
+      const [key, value] = item.split(':');
+
+      return [key.trim(), value.trim()];
+    }),
+  );
+
+  // for (let i = 0; i < dividedString.length; i++) {
+  //   const [key, value] = dividedString[i].split(':');
+
+  //   styles[key.trim()] = value.trim();
+  // }
+
+  return styles;
 }
 
 module.exports = convertToObject;

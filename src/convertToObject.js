cbed895b0faa5d71ccd2f30608c441ecab228828
Raphael Reis <raphael.reis1320@gmail.com>
2026-06-20 13:38:48 -0300

    resolvido2

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,25 +5,45 @@
  *
  * @return {object}
  */
+// function convertToObject(sourceString) {
+//   const trimInit = sourceString.trim();
+
+//   const parts = trimInit.split(';');
+
+//   const result = {};
+
+//   for (const item of parts) {
+//     if (!item.trim()) {
+//       continue;
+//     }
+
+//     const [propried, ...valueParts] = item.split(':');
+//     const value = valueParts.join(':').trim();
+
+//     result[propried.trim()] = value;
+//   }
+
+//   return result;
+// }
+
 function convertToObject(sourceString) {
   const trimInit = sourceString.trim();
-
   const parts = trimInit.split(';');
 
-  const result = {};
-
-  for (const item of parts) {
+  const stylesObject = parts.reduce((acc, item) => {
     if (!item.trim()) {
-      continue;
+      return acc;
     }
 
     const [propried, ...valueParts] = item.split(':');
     const value = valueParts.join(':').trim();
 
-    result[propried.trim()] = value;
-  }
+    acc[propried.trim()] = value;
 
-  return result;
+    return acc;
+  }, {});
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

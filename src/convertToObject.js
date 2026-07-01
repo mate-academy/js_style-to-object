79a02bc34bd1e0d6dfdc1f384b3b2d245e6da6c5
Petrenko Oleksii <petrenko1694@gmail.com>
2026-07-01 19:44:13 +0300

    String-to-Object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,22 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const interimArray = sourceString
+    .split(';')
+
+    .map((el) => el.trim())
+
+    .filter((el) => el !== '');
+
+  const result = {};
+
+  const call = (string) => {
+    result[string.split(':')[0].trim()] = string.split(':')[1].trim();
+  };
+
+  interimArray.forEach(call);
+
+  return result;
 }
 
 module.exports = convertToObject;

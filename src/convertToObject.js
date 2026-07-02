67dd00ecd4fcadbf992da1a1837afab79734af4e
Vladyslav Vyshnivskyi <vladyslav.vyshnivskyi@avanquest.com>
2026-07-02 12:36:35 +0300

    Add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,26 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  const styles = sourceString.split(';');
+
+  for (const style of styles) {
+    const trimmed = style.trim();
+
+    if (!trimmed) {
+      continue;
+    }
+
+    const separatorIndex = trimmed.indexOf(':');
+
+    const property = trimmed.slice(0, separatorIndex).trim();
+    const value = trimmed.slice(separatorIndex + 1).trim();
+
+    result[property] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

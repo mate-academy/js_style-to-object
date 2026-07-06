ad67b0cad6816fedea931d41edc4d8bf4c383124
Anton Shaga <antonyshaga@gmail.com>
2026-07-06 17:29:24 -0300

    Implement style string converter

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,20 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const stylesObject = {};
+
+  sourceString
+    .split(';')
+    .filter((style) => style.trim() !== '')
+    .forEach((style) => {
+      const parts = style.split(':');
+      const property = parts[0].trim();
+      const value = parts.slice(1).join(':').trim();
+
+      stylesObject[property] = value;
+    });
+
+  return stylesObject;
 }
 
 module.exports = convertToObject;

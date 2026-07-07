0ee87ff7561bf7ee7495216ea2ecf668222a9473
ALex <sasha.hivuk@gmail.com>
2026-07-07 13:40:38 +0300

    convertString

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,23 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const result = {};
+  const splitString = sourceString.trim().split(';');
+
+  for (let i = 0; i < splitString.length; i++) {
+    const partsString = splitString[i].trim().split(':');
+
+    if (partsString.length < 2) {
+      continue;
+    }
+
+    const key = partsString[0].trim();
+    const value = partsString[1].trim();
+
+    result[key] = value.trim();
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

3cede8279b29c16aa085767e8776e00508822096
Khrystyna Salo <kristinasalo4@gmail.com>
2026-07-19 21:53:12 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssStyles = sourceString.split(';');
+
+  return cssStyles
+    .map((style) => style.trim())
+    .filter((style) => style.includes(':'))
+    .reduce((accumulator, style) => {
+      const colonIndex = style.indexOf(':');
+
+      const key = style.slice(0, colonIndex).trim();
+      const value = style.slice(colonIndex + 1).trim();
+
+      accumulator[key] = value;
+
+      return accumulator;
+    }, {});
 }
 
 module.exports = convertToObject;

e38231dd8fc60a53366829dde9376a6852a873b7
Olya <fialkaolya08@gmail.com>
2026-07-25 21:01:10 +0200

    Finished exercise

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,34 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const masOfSplit = sourceString.split(';');
+  const masOfKeysAndDeterm = masOfSplit.map((KeysAndDeterm) => {
+    return KeysAndDeterm.split(':');
+  });
+
+  let key = '';
+  let value = '';
+  const cssObject = {};
+
+  masOfKeysAndDeterm.forEach((oneLine) => {
+    if (oneLine === undefined || oneLine.length <= 0 || oneLine.length > 2) {
+      return;
+    }
+
+    key = oneLine[0].trim();
+
+    if (oneLine.length === 2) {
+      value = oneLine[1].trim();
+    }
+
+    if (key === '') {
+      return;
+    }
+
+    cssObject[key] = value;
+  });
+
+  return cssObject;
 }
 
 module.exports = convertToObject;

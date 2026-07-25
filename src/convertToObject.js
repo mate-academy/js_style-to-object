38df6347e45e24fadbacd618a087540ebd80b50b
Ruslan Sydorenko <letovletov111@gmail.com>
2026-07-25 07:55:20 +0200

    task array methods

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -5,8 +5,24 @@
  *
  * @return {object}
  */
+
 function convertToObject(sourceString) {
-  // write your code here
+  const stringSplitted = sourceString.split(';');
+  const stringMapped = stringSplitted.map((field) => field.trim());
+  const stringFiltered = stringMapped.filter((field) => field !== '');
+
+  const stringDivided = stringFiltered.map((field) => {
+    return field.split(':').map((part) => part.trim());
+  });
+
+  const resultObj = stringDivided.reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [key]: value,
+    };
+  }, {});
+
+  return resultObj;
 }
 
 module.exports = convertToObject;

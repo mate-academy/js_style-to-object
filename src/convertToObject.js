2f29aa9e04df387c529551ef7c8e8d862ba5725f
Radusca <grozavradustefan@gmail.com>
2026-08-03 13:39:02 +0300

    verify

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,14 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  return sourceString.split(';').filter(item => item.trim() != '').map(item => item.split(':').trim());
+  return sourceString
+    .split(';')
+    .filter(item => item.trim() !== '')
+    .map(item => item.split(':').map(i => i.trim()))
+    .reduce((acc, pereche) => {
+      acc[pereche[0]] = pereche[1];
+      return acc;
+    }, {});
 }
 
 module.exports = convertToObject;

09e88d4cbb73b821ff3a44e3ba26987544e02958
DYM <dmetedji@gmail.com>
2026-08-03 14:24:41 +0000

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,30 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arraySourceString = sourceString.split(';');
+  const filterArraySourceSpace = arraySourceString.filter((x) => trimElem(x));
+  const trimArraySourceString = [];
+
+  for (const elem of filterArraySourceSpace) {
+    trimArraySourceString.push(elem.trim());
+  }
+
+  const result = trimArraySourceString.reduce((acc, elem) => {
+    const pair = elem.split(':');
+
+    pair[0] = pair[0].trim();
+    pair[1] = pair[1].trim();
+
+    acc[pair[0]] = pair[1];
+
+    return acc;
+  }, {});
+
+  function trimElem(elem) {
+    return elem.trim() !== '';
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

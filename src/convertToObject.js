b6d23f634c2ef0e0fade61ca5cfccd48fa54e8eb
Radusca <grozavradustefan@gmail.com>
2026-08-03 13:37:16 +0300

    correct

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,7 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  return sourceString.split(';').forEach(item => item.trim().split(':').forEach(i => i.trim()));
+  return sourceString.split(';').filter(item => item.trim() != '').map(item => item.split(':').trim());
 }
 
 module.exports = convertToObject;

a45a9fa9900e954f59f406b0f75a054a5a469962
diiyavol <nikitanesterenko88@gmail.com>
2026-08-05 12:25:42 +0300

    Solution 1

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,29 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const allString = sourceString
+    .replace(/\t/g, ' ')
+    .split(';')
+    .map((line) => line.trim())
+    .filter((line) => line !== '');
+
+  const pairs = allString.map((line) => {
+    const colonIndex = line.indexOf(':');
+    const key = line.slice(0, colonIndex).trim();
+    const value = line
+      .slice(colonIndex + 1)
+      .trim()
+      .replace(';', '');
+
+    return [key, value];
+  });
+  const result = pairs.reduce((obj, pair) => {
+    obj[pair[0]] = pair[1];
+
+    return obj;
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;

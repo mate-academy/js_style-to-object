6d0db882c3d5b2dfdd252cfc2c0ce9498e350350
Mykola Averchenko <kolya.averchenko@gmail.com>
2026-08-10 22:31:46 +0300

    add solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,25 @@
  */
 function convertToObject(sourceString) {
   // write your code here
+  const convertedObject = {};
+  const sourceLines = sourceString.split(';');
+
+  if (sourceString.length === 0) {
+    return convertedObject;
+  }
+
+  sourceLines.map((line) => {
+    let [attribute, value] = line.split(':');
+
+    if (line !== '' && line.trim().length !== 0) {
+      attribute = attribute.trim();
+      value = value.trim();
+
+      convertedObject[attribute] = value;
+    }
+  });
+
+  return convertedObject;
 }
 
 module.exports = convertToObject;

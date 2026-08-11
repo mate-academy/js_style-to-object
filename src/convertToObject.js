a3e5c13bcf6494c3bfebe69fc585e283f0e490b9
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2026-08-11 15:58:42 +0300

    Completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const lines = sourceString.trim().split(';');
+  const result = {};
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i].trim();
+
+    if (line === '') {
+      continue;
+    }
+
+    const separatorIndex = line.indexOf(':');
+
+    const key = line.slice(0, separatorIndex).trim();
+    const value = line.slice(separatorIndex + 1).trim();
+
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

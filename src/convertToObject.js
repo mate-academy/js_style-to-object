a775e16d7d50123a9399db911227d98a407f3dd1
pirotskyi <pirotskyi.a@gmail.com>
2026-08-12 17:05:53 +0300

    fix: correct implementation

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,13 +6,18 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const rules = sourceString.split(';');
-  const filteredRules = rules.filter((rule) => rule.trim() !== '');
+  const lines = sourceString.split('\n');
+  const filteredLines = lines.filter((line) => line.trim() !== '');
 
-  const parsedStylesObject = filteredRules.reduce((obj, rule) => {
+  const parsedStylesObject = filteredLines.reduce((obj, line) => {
+    const rule = line.trim();
     const colonIndex = rule.indexOf(':');
     const key = rule.slice(0, colonIndex).trim();
-    const value = rule.slice(colonIndex + 1).trim();
+    let value = rule.slice(colonIndex + 1).trim();
+
+    if (value.endsWith(';')) {
+      value = value.slice(0, -1).trim();
+    }
 
     obj[key] = value;
 

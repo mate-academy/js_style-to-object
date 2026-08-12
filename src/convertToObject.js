fcc674faddf116d1b7013dbb24544acb92d9a400
sofiatsebenko <sofialtd50@gmail.com>
2026-08-12 19:42:29 +0300

    converted to obj

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  sourceString
+    .split(';')
+    .map((style) => style.trim())
+    .filter((style) => style)
+    .forEach((style) => {
+      const [key, ...value] = style.split(':');
+
+      result[key.trim()] = value.join(':').trim();
+    });
+
+  return result;
 }
 
 module.exports = convertToObject;

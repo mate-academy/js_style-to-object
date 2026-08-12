6564f4d4c2fb939eb34538fd88ed4fea4eba0301
pirotskyi <pirotskyi.a@gmail.com>
2026-08-12 16:56:24 +0300

    fix: solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -9,17 +9,17 @@ function convertToObject(sourceString) {
   const rules = sourceString.split(';');
   const filteredRules = rules.filter((rule) => rule.trim() !== '');
 
-  const result = filteredRules.reduce((obj, rule) => {
-    const parts = rule.split(':');
-    const key = parts[0].trim();
-    const value = parts[1].trim();
+  const parsedStylesObject = filteredRules.reduce((obj, rule) => {
+    const colonIndex = rule.indexOf(':');
+    const key = rule.slice(0, colonIndex).trim();
+    const value = rule.slice(colonIndex + 1).trim();
 
     obj[key] = value;
 
     return obj;
   }, {});
 
-  return result;
+  return parsedStylesObject;
 }
 
 module.exports = convertToObject;

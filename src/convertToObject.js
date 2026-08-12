6e74e2871660d98b5ded44af47d8a2d9d7ae571a
Maksym <maksimyatskulyak@meta.ua>
2026-08-12 22:50:18 +0300

    replaced for...of with reduce()

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,24 +6,23 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const result = {};
   const cleanRules = sourceString
     .split(';')
     .map((rule) => rule.trim())
     .filter((empty) => empty !== '');
 
-  for (const rule of cleanRules) {
+  return cleanRules.reduce((parsedStyles, rule) => {
     const colonIndex = rule.indexOf(':');
 
     if (colonIndex !== -1) {
       const key = rule.slice(0, colonIndex).trim();
       const value = rule.slice(colonIndex + 1).trim();
 
-      result[key] = value;
+      parsedStyles[key] = value;
     }
-  }
 
-  return result;
+    return parsedStyles;
+  }, {});
 }
 
 module.exports = convertToObject;

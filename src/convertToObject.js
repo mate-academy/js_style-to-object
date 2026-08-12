f171fa1768ef9656e8808a0c7cd1585c1e250686
Vlad CHuikov <koras4uikov@gmail.com>
2026-08-12 22:58:14 +0300

    added task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const rulesNotFormated = sourceString.split(';');
+
+  const rulesNFKV = rulesNotFormated.map((el) => el.trim().split(':'));
+
+  const rulesFKV = rulesNFKV.map((arr) => arr.map((el) => el.trim('')));
+
+  const removeSpaces = rulesFKV.filter((arr) => arr.some((el) => el !== ''));
+
+  return removeSpaces.reduce((acc, [key, value]) => {
+    acc[key] = value;
+
+    return acc;
+  }, {});
 }
 
 module.exports = convertToObject;

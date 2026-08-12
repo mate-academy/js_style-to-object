79f763f6f42a61e488df585d18fb3d3f7a29f2ae
HiljSV <hiljsv@gmail.com>
2026-08-13 00:36:22 +0300

    The task of converting the style string into an object has been completed

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,12 +1,26 @@
 'use strict';
 
+const DECLARATION_SEPARATOR = ';';
+const PROPERTY_VALUE_SEPARATOR = ':';
+
 /**
  * @param {string} sourceString
  *
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const declaretions = sourceString
+    .split(DECLARATION_SEPARATOR)
+    .filter((declaration) => declaration.includes(PROPERTY_VALUE_SEPARATOR))
+    .map((declaration) => {
+      const separatorIndex = declaration.indexOf(PROPERTY_VALUE_SEPARATOR);
+      const property = declaration.slice(0, separatorIndex).trim();
+      const value = declaration.slice(separatorIndex + 1).trim();
+
+      return [property, value];
+    });
+
+  return Object.fromEntries(declaretions);
 }
 
 module.exports = convertToObject;

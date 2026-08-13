a16886705de4ed2bd422be7dbf90e2bac27ce378
Santana <wonderday1804@gmail.com>
2026-08-13 03:41:14 +0300

    convert to object solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const styles = sourceString.split(';').filter((style) => style.trim() !== '');
+
+  const pairs = styles.map((style) => style.split(':'));
+
+  const cleanPairs = pairs.map((pair) => {
+    return [pair[0].trim(), pair[1].trim()];
+  });
+
+  const result = {};
+
+  for (const pair of cleanPairs) {
+    result[pair[0]] = pair[1];
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

a8003eb3f60499c5292b3258335b22a257b175c0
Veronika Nychyporuk <veronikanychyporuk@gmail.com>
2026-08-30 04:24:27 +0300

    fix solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,13 +7,16 @@
  */
 function convertToObject(sourceString) {
   let styles = sourceString.split(';');
-  const resultObject = {};
 
   styles = styles.map((element) => element.trim().split(':'));
   styles = styles.filter((element) => element.length === 2);
   styles = styles.map((element) => [element[0].trim(), element[1].trim()]);
 
-  styles.forEach((element) => (resultObject[element[0]] = element[1]));
+  const resultObject = styles.reduce((acc, element) => {
+    acc[element[0]] = element[1];
+
+    return acc;
+  }, {});
 
   return resultObject;
 }

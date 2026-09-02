3d6d7d292bb8ac2db5206ddb23bbfe04fcd0dd48
Hubert Piasecki <piahub@wp.pl>
2026-09-02 14:14:06 +0200

    Task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,28 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultObject = {};
+
+  const splittedString = sourceString.split(';');
+
+  if (sourceString.length <= 0) {
+    return resultObject;
+  }
+
+  for (const part of splittedString) {
+    const colonIndex = part.indexOf(':');
+    const key = part.slice(0, colonIndex).trim();
+
+    if (!key) {
+      continue;
+    }
+
+    const value = part.slice(colonIndex + 1).trim();
+
+    resultObject[key] = value;
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;

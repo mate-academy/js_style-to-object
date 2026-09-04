e3efd46f1d3f442fd9189f9d3e2155eaa1b296f5
DeeDooc203 <yarilko.k@gmail.com>
2026-09-04 15:41:50 +0300

    added convert function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 'use strict';
 
 /**
@@ -6,7 +7,32 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+
+  if (!sourceString) {
+    return result;
+  }
+
+  const declarations = sourceString.split(';');
+
+  for (const declaration of declarations) {
+    const colonIndex = declaration.indexOf(':');
+
+    if (colonIndex === -1) {
+      continue;
+    }
+
+    const property = declaration.slice(0, colonIndex).trim();
+    const value = declaration.slice(colonIndex + 1).trim();
+
+    if (property === '') {
+      continue;
+    }
+
+    result[property] = value;
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;

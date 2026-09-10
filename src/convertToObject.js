114c8f998d46cffb24d6d6a45d545e929e751302
Andrii Zhurbas <zhurbas777@gmail.com>
2026-09-10 16:18:57 +0300

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -7,6 +7,21 @@
  */
 function convertToObject(sourceString) {
   // write your code here
-}
+  const result = {};
 
+  const stringsArr = sourceString
+    .split(';')
+    .map((element) => element.trim())
+    .filter((lines) => lines !== '' && lines !== ';')
+    .map((line) => line.split(':'))
+    .map((parts) => {
+      return parts.map((part) => part.trim().replaceAll(';', ''));
+    });
+
+  for (let i = 0; i < stringsArr.length; i++) {
+    result[stringsArr[i][0]] = stringsArr[i][1];
+  }
+
+  return result;
+}
 module.exports = convertToObject;

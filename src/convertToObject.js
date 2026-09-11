b37b45e753d6063761e8c64f60e61f95af0be534
Andrzej Lipski <mustang12@gmail.com>
2026-09-11 11:23:20 +0200

    Solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,19 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  return Object.fromEntries(
+    sourceString
+      .split(';')
+      .filter((style) => style.trim())
+      .map((style) => {
+        const separatorIndex = style.indexOf(':');
+
+        return [
+          style.slice(0, separatorIndex).trim(),
+          style.slice(separatorIndex + 1).trim(),
+        ];
+      }),
+  );
 }
 
 module.exports = convertToObject;
